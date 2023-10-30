db55bce16ad3d322b54474a7f7a29e8587cd96bf
vtoper <vincent.toper@gmail.com>
2023-10-30 22:25:26 +0100

    Fix Teija

--- modules/php/Helpers/Conditions.php
+++ modules/php/Helpers/Conditions.php
@@ -21,8 +21,8 @@ abstract class Conditions
 
   public static function firstCharacterPlayed($card, $event)
   {
-    return $event['playCard'] === true &&
+    return $event['playCard'] === true && $card->getPId() == $event['pId'] && 
       $event['cardType'] == EXPLORER &&
-      ($card->getExtraDatas()['userPower'] ?? false) == false;
+      ($card->getExtraDatas()['userPower'] ?? false) == false
   }
 }
