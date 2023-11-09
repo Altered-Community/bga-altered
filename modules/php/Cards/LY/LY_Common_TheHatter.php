ca6cc88199499b1a45ce44a9b699d2bbaebc5054
vtoper <vincent.toper@gmail.com>
2023-11-09 23:08:21 +0100

    Hatter preco

---
+++ modules/php/Cards/LY/LY_Common_TheHatter.php
@@ -0,0 +1,35 @@
+<?php
+namespace ALT\Cards\LY;
+use ALT\Helpers\FT;
+
+class LY_Common_TheHatter extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+
+    $this->properties = [
+      'uid' => '85',
+      'asset' => 'LY-18-MadHatter-C',
+      'frameSize' => 1,
+
+      'faction' => FACTION_LY,
+      'name' => clienttranslate('The Hatter'),
+      'typeline' => clienttranslate('Character - Citizen'),
+      'rarity' => RARITY_COMMON,
+      'type' => CHARACTER,
+      'subtype' => 'Citizen',
+
+      'echoDesc' => clienttranslate(
+        '{D} : Target Character with hand cost {3} or less becomes [[Anchored]]. (Discard me from your Reserve to activate this effect)'
+      ),
+      'reminders' => clienttranslate('He can\'t swim.'),
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 0,
+      'costHand' => 4,
+      'costMemory' => 5,
+      'effectEcho' => FT::ACTION(TARGET, ['maxHandCost' => 3, 'effect' => FT::GAIN($this, ANCHORED)]),
+    ];
+  }
+}
