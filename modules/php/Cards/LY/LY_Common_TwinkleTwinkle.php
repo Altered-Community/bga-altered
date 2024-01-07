bb603ed4fe9da7eb936da87fb3ab5edd1003969d
vtoper <vincent.toper@gmail.com>
2024-01-07 22:31:13 +0100

    LY_Common_TwinkleTwinkle

---
+++ modules/php/Cards/LY/LY_Common_TwinkleTwinkle.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
+
+class LY_Common_TwinkleTwinkle extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_23_C',
+      'asset' => 'ALT_CORE_B_LY_23_C',
+
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Twinkle Twinkle'),
+      'typeline' => clienttranslate('Spell - Song'),
+      'type' => SPELL,
+      'subtypes' => [SONG],
+      'effectDesc' => clienttranslate('Target Character gains $[ASLEEP].'),
+      'supportDesc' => clienttranslate(
+        '{D} : The next card you play this turn costs {1} less. (Discard me from Reserve to do this.)'
+      ),
+      'costHand' => 2,
+      'costReserve' => 2,
+
+      'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, ASLEEP)]),
+      'effectSupport' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => ALL, 'reduction' => 1]],
+      ],
+
+    ];
+  }
+}
