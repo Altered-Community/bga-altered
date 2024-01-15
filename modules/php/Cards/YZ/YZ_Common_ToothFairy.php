5f2e2acc22fcdeaea908be56fe6806f9c7ecc39e
vtoper <vincent.toper@gmail.com>
2024-01-15 23:48:44 +0100

    YZ_Common_ToothFairy + YZ_Rare_ToothFairy

---
+++ modules/php/Cards/YZ/YZ_Common_ToothFairy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Common_ToothFairy extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_06_C',
+      'asset' => 'ALT_CORE_B_YZ_06_C',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Tooth Fairy'),
+      'typeline' => clienttranslate('Character - Fairy'),
+      'type' => CHARACTER,
+      'subtypes' => [FAIRY],
+      'effectDesc' => clienttranslate('{H} $[SABOTAGE].'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+    ];
+  }
+}
---
+++ modules/php/Cards/YZ/YZ_Rare_ToothFairy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_ToothFairy extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_06_R1',
+      'asset' => 'ALT_CORE_B_YZ_06_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Tooth Fairy'),
+      'typeline' => clienttranslate('Character - Fairy'),
+      'type' => CHARACTER,
+      'subtypes' => [FAIRY],
+      'effectDesc' => clienttranslate('{H} $[SABOTAGE].'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 1,
+      'changedStats' => ['costReserve'],
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+    ];
+  }
+}
