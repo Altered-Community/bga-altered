a9861bc83d54e0d253fe123354f48c597d3291e6
vtoper <vincent.toper@gmail.com>
2024-01-15 23:03:30 +0100

    MU_Rare_MeditationTraining

---
+++ modules/php/Cards/MU/MU_Rare_MeditationTraining.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_MeditationTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_25_R1',
+      'asset' => 'ALT_CORE_B_MU_25_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Meditation Training'),
+      'typeline' => clienttranslate('Spell - Boon'),
+      'type' => SPELL,
+      'subtypes' => [BOON],
+      'effectDesc' => clienttranslate('Target Character with Hand Cost {3} or less gains $[ANCHORED].'),
+      'supportDesc' => clienttranslate(
+        '#{D} : The next Character you play this turn gains 1 boost.# (Discard me from Reserve to do this.)'
+      ),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'effectPlayed' => FT::ACTION(TARGET, ['maxHandCost' => 3, 'effect' => FT::GAIN($this, ANCHORED)]),
+      'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterGains1Boost']),
+
+    ];
+  }
+}
