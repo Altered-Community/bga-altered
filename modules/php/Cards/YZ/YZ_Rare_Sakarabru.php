822682e5e02891b312e4e30980fcd49ee47395e2
vtoper <vincent.toper@gmail.com>
2024-01-15 23:56:23 +0100

    YZ_Rare_Sakarabru

---
+++ modules/php/Cards/YZ/YZ_Rare_Sakarabru.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_Sakarabru extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_18_R1',
+      'asset' => 'ALT_CORE_B_YZ_18_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Sakarabru'),
+      'typeline' => clienttranslate('Character - Deity'),
+      'type' => CHARACTER,
+      'subtypes' => [DEITY],
+      'effectDesc' => clienttranslate('{H} Your opponent\'s Expedition facing mine moves backwards one region.'),
+      'supportDesc' => clienttranslate('#{D} : Draw a card.# (Discard me from Reserve to do this.)'),
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 5,
+      'costHand' => 7,
+      'costReserve' => 4,
+      'changedStats' => ['forest', 'mountain', 'ocean'],
+      'effectHand' => FT::ACTION(MOVE_EXPEDITION, ['n' => -1, 'expedition' => EFFECT, 'pId' => OPPONENT]),
+      'effectSupport' => FT::ACTION(DRAW, ['players' => ME]),
+
+    ];
+  }
+}
