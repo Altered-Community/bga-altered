4772396abf18b55d8026b00d0e5845a294a65e29
vtoper <vincent.toper@gmail.com>
2024-01-15 22:38:30 +0100

    BR_Rare_PhysicalTraining

---
+++ modules/php/Cards/BR/BR_Rare_PhysicalTraining.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+class BR_Rare_PhysicalTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_26_R1',
+      'asset' => 'ALT_CORE_B_BR_26_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Physical Training'),
+      'typeline' => clienttranslate('Spell - Boon'),
+      'type' => SPELL,
+      'subtypes' => [BOON],
+      'effectDesc' => clienttranslate('Target Character gains 3 boosts$[BB].  #Draw a card.#'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['costHand'],
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST, 'n' => 3])]),
+        FT::ACTION(DRAW, ['players' => ME])
+      )
+
+    ];
+  }
+}
