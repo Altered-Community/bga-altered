8d2b9f30f936a659ced610ee3d605473a585dd3c
vtoper <vincent.toper@gmail.com>
2024-01-15 22:51:11 +0100

    LY_Rare_Tanuki

---
+++ modules/php/Cards/LY/LY_Rare_Tanuki.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
+
+class LY_Rare_Tanuki extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_09_R1',
+      'asset' => 'ALT_CORE_B_LY_09_R1',
+
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Tanuki'),
+      'typeline' => clienttranslate('Character - Spirit'),
+      'type' => CHARACTER,
+      'subtypes' => [SPIRIT],
+      'effectDesc' => clienttranslate('{H} $[SABOTAGE].  #{R} Roll a die. On a 4 or higher, [SABOTAGE].#'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 0,
+      'costHand' => 3,
+      'costReserve' => 3,
+
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+      'effectPlayed' => FT::ACTION(ROLL_DIE, [
+        'effect' => ['4+' => FT::ACTION(TARGET, [
+          'targetType' => [CHARACTER, SPELL, PERMANENT],
+          'targetLocation' => [RESERVE],
+          'upTo' => true,
+          'effect' => FT::ACTION(DISCARD, []),
+        ])]
+      ]),
+    ];
+  }
+}
