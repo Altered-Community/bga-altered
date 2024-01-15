80ca70cb91607b695fd0ad01591c419b2dde1063
vtoper <vincent.toper@gmail.com>
2024-01-15 23:58:39 +0100

    LY_Rare_Offyougo

---
+++ modules/php/Cards/LY/LY_Rare_Offyougo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
+
+class LY_Rare_Offyougo extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_21_R2',
+      'asset' => 'ALT_CORE_B_YZ_21_R2',
+
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Off you go!'),
+      'typeline' => clienttranslate('Spell - Disruption'),
+      'type' => SPELL,
+      'subtypes' => [DISRUPTION],
+      'effectDesc' => clienttranslate('Send to Reserve target Character with Hand Cost {3} or less.'),
+      'costHand' => 2,
+      'costReserve' => 4,
+      'effectPlayed' => FT::ACTION(TARGET, ['maxHandCost' => 3, 'effect' => FT::DISCARD_TO_RESERVE()]),
+
+    ];
+  }
+}
