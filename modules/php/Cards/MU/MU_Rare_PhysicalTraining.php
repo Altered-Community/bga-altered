01907b721850599382958c531492ff7527bc26c4
vtoper <vincent.toper@gmail.com>
2024-01-15 22:39:23 +0100

    MU_Rare_PhysicalTraining

---
+++ modules/php/Cards/MU/MU_Rare_PhysicalTraining.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_PhysicalTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_26_R2',
+      'asset' => 'ALT_CORE_B_BR_26_R2',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Physical Training'),
+      'typeline' => clienttranslate('Spell - Boon'),
+      'type' => SPELL,
+      'subtypes' => [BOON],
+      'effectDesc' => clienttranslate('Target Character gains 3 boosts$[BB].'),
+      'costHand' => 2,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST, 'n' => 3])]),
+    ];
+  }
+}
