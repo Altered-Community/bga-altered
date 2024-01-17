714671dcf72da1679088119a00b0b911c491d0b8
vtoper <vincent.toper@gmail.com>
2024-01-18 00:12:40 +0100

    AX_Rare_TheOuroborosLyraBastion

---
+++ modules/php/Cards/AX/AX_Rare_TheOuroborosLyraBastion.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_TheOuroborosLyraBastion extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_30_R2',
+      'asset' => 'ALT_CORE_B_LY_30_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('The Ouroboros, Lyra Bastion'),
+      'typeline' => clienttranslate('Permanent - Landmark'),
+      'type' => PERMANENT,
+      'subtypes' => [LANDMARK],
+      'flavorText' => clienttranslate(
+        'The Expeditionary Corps\' mobile outpost soars high, like a spinning wheel weaving the wind.'
+      ),
+      'effectDesc' => clienttranslate(
+        '#{J} [RESUPPLY].#  If you would #$[RESUPPLY], instead, reveal the top two cards of your deck. Put one in Reserve, and discard the other.#'
+      ),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(RESUPPLY, []),
+      // Passive effect managed in Resupply
+    ];
+  }
+}
