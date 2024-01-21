f21f6f9abff8fd8109892d6649617a3a36636bad
vtoper <vincent.toper@gmail.com>
2024-01-21 23:20:53 +0100

    Babayaga

---
+++ modules/php/Cards/YZ/YZ_Common_BabaYaga.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Common_BabaYaga extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_11_C',
+      'asset' => 'ALT_CORE_B_YZ_11_C',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Baba Yaga'),
+      'typeline' => clienttranslate('Character - Mage'),
+      'type' => CHARACTER,
+      'subtypes' => [MAGE],
+      'effectDesc' => clienttranslate('{H} Draw a card.'),
+      'forest' => 2,
+      'mountain' => 1,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(DRAW, ['players' => ME]),
+    ];
+  }
+}
