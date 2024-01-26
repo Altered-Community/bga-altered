4b850cfef8b5f4f2d697c01a6a0e921d10e2d2e1
vtoper <vincent.toper@gmail.com>
2024-01-26 22:56:04 +0100

    remoe old line

--- modules/php/Cards/LY/LY_Common_TheHatter.php
+++ modules/php/Cards/LY/LY_Common_TheHatter.php
@@ -18,14 +18,12 @@ class LY_Common_TheHatter extends \ALT\Models\Card
       'name' => 'The Hatter',
       'type' => CHARACTER,
       'subtypes' => [CITIZEN],
-      'effectDesc' => clienttranslate('(*He can\'t swim.*)'),
-      'supportDesc' =>
-        '{D} : Target Character with Hand Cost {3} or less gains [ANCHORED]. (Discard me from Reserve to do this.)',
+      'supportDesc' => '{D} : Target Character with Hand Cost {3} or less gains [ANCHORED]. (Discard me from Reserve to do this.)',
       'supportIcon' => 'discard',
       'typeline' => 'Character - Citizen',
       'artist' => 'Zero Wen',
       'flavorText' =>
-        "\"You can draw water out of a water-well, so I should think you could draw treacle out of a treacle-well — eh, stupid ?\"",
+      "\"You can draw water out of a water-well, so I should think you could draw treacle out of a treacle-well — eh, stupid ?\"",
 
       'forest' => 5,
       'mountain' => 5,
