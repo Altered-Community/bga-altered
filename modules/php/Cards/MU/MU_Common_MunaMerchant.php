41a4c13a4764b7e36663a2d4844674f5375f3fdd
vtoper <vincent.toper@gmail.com>
2024-02-04 00:04:15 +0100

    MU_Common_MunaMerchant

---
+++ modules/php/Cards/MU/MU_Common_MunaMerchant.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Common_MunaMerchant extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_23_C',
+      'asset' => 'ALT_CORE_B_MU_23_C',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_COMMON,
+      'name' => 'Muna Merchant',
+      'typeline' => 'Character - Citizen',
+      'type' => CHARACTER,
+      'flavorText' => '"What do you need?"',
+      'artist' => 'Ba Vo',
+      'subtypes' => [CITIZEN],
+      'effectDesc' => '{R} $<RESUPPLY>.',
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'effectReserve' => FT::ACTION(RESUPPLY, [])
+    ];
+  }
+}
