8fd8e8b4a6c7acc369bf428e47286c26b4a5d496
vtoper <vincent.toper@gmail.com>
2024-02-06 23:13:22 +0100

    OD_Common_PaperHerald

---
+++ modules/php/Cards/OD/OD_Common_PaperHerald.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+class OD_Common_PaperHerald extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_OR_04_C',
+      'asset' => 'ALT_CORE_B_OR_04_C',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_COMMON,
+      'name' => 'Paper Herald',
+      'typeline' => 'Character - Messenger',
+      'type' => CHARACTER,
+      'flavorText' => 'Words mean more than what is set down on paper.',
+      'artist' => 'Jean-Baptiste Andrier',
+      'subtypes' => [MESSENGER],
+      'supportDesc' =>
+      '{D} : Create an <ORDIS_RECRUIT> Soldier token in target Expedition. (Discard me from Reserve to do this.)',
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 1,
+      'costHand' => 1,
+      'costReserve' => 1,
+      'supportIcon' => 'discard',
+      'effectSupport' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'OD_Common_OrdisRecruit',
+      ]),
+    ];
+  }
+}
