4ef525cd0f532dd0bbb011cc6cb00e1cd29b0f62
vtoper <vincent.toper@gmail.com>
2024-02-25 00:08:45 +0100

    YZ_Common_MagicalTraining

---
+++ modules/php/Cards/YZ/YZ_Common_MagicalTraining.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Common_MagicalTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_19_C',
+      'asset' => 'ALT_CORE_B_YZ_19_C',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_COMMON,
+      'name' => 'Magical Training',
+      'typeline' => 'Spell - Conjuration',
+      'type' => SPELL,
+      'flavorText' => 'Without practice, the gift of Alteration will come to nothing.',
+      'artist' => 'Zero Wen',
+      'subtypes' => [CONJURATION],
+      'effectDesc' => 'Draw a card.',
+      'costHand' => 1,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME])
+    ];
+  }
+}
