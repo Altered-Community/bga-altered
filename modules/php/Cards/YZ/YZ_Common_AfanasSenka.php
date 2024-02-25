5f06578b63719f4934674e5901f5abf821db816f
vtoper <vincent.toper@gmail.com>
2024-02-25 23:18:51 +0100

    YZ_Common_AfanasSenka

---
+++ modules/php/Cards/YZ/YZ_Common_AfanasSenka.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Common_AfanasSenka extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_03_C',
+      'asset' => 'ALT_CORE_B_YZ_03_C',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_COMMON,
+      'name' => 'Afanas & Senka',
+      'typeline' => 'Yzmir Hero',
+      'type' => HERO,
+      'flavorText' => 'The world feeds on magic, and withers in its absence.',
+      'artist' => 'Edward Cheekokseang',
+      'effectDesc' =>
+      'When you play a Spell — Target Character you control gains 1 boost$<BB>. (Do this after the spell resolves.)',
+
+      'reserveSlots' => 2,
+      'landmarkSlots' => 2,
+
+      'effectPassive' => [
+        'ChooseAssignment' => [
+          'condition' => 'isSpellPlayed',
+          'output' => FT::ACTION(
+            TARGET,
+            [
+              'targetPlayer' => ME,
+              'targetType' => [CHARACTER, TOKEN],
+              'effect' => FT::GAIN(EFFECT, BOOST)
+            ]
+          ),
+        ]
+      ]
+    ];
+  }
+}
