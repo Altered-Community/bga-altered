cbe93e915542eb1a85c690131b845a91e97fa574
vtoper <vincent.toper@gmail.com>
2024-02-25 14:15:17 +0100

    YZ_Common_BabasIsba

---
+++ modules/php/Cards/YZ/YZ_Common_BabasIsba.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Common_BabasIsba extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_28_C',
+      'asset' => 'ALT_CORE_B_YZ_28_C',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_COMMON,
+      'name' => "Baba's Isba",
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' => 'Boney Legs, she\'s called, and her hut is one reason why.',
+      'artist' => 'Taras Susak',
+      'subtypes' => [LANDMARK],
+      'effectDesc' => '{J} Draw a card.  {T}, Sacrifice a Character: $<AFTER_YOU>.',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME]),
+      'effectTap' => FT::SEQ(
+        FT::ACTION(
+          TARGET,
+          [
+            'targetPlayer' => ME,
+            'targetType' => [CHARACTER, TOKEN],
+            'effect' => FT::ACTION(DISCARD, ['desc' => 'sacrifice'])
+          ]
+        ),
+        FT::ACTION(AFTER_YOU, [])
+      )
+    ];
+  }
+}
