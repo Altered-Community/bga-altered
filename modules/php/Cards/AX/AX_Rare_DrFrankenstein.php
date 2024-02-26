66f27b348712d92bbb3a74dbb067c3a04bdee966
vtoper <vincent.toper@gmail.com>
2024-02-26 23:01:43 +0100

    Rare_DrFrankenstein

---
+++ modules/php/Cards/AX/AX_Rare_DrFrankenstein.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_DrFrankenstein extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_17_R1',
+      'asset' => 'ALT_CORE_B_AX_17_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Dr. Frankenstein',
+      'typeline' => 'Character - Engineer',
+      'type' => CHARACTER,
+      'flavorText' => 'We sometimes seek companionship in the strangest of life forms.',
+      'artist' => 'Taras Susak',
+      'subtypes' => [ENGINEER],
+      'effectDesc' => '{R} You may activate the {j} triggers of target Permanent you control.',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 4,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'effectReserve' => FT::ACTION(TARGET, [
+        'targetType' => [PERMANENT],
+        'targetPlayer' => ME,
+        'hasEffects' => ['Played'],
+        'effect' => FT::ACTION(ACTIVATE_EFFECT, []),
+      ]),
+    ];
+  }
+}
