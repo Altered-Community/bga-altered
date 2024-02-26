91c4a9ff4371070da8b0819014d94e9bd730d381
vtoper <vincent.toper@gmail.com>
2024-02-26 23:27:42 +0100

    Rare_KelonBurst

---
+++ modules/php/Cards/AX/AX_Rare_KelonBurst.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+
+class AX_Rare_KelonBurst extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_23_R1',
+      'asset' => 'ALT_CORE_B_AX_23_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Kelon Burst',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'There\'s an enduring legend in the Suspira quarries: the existence of another type of Kelon.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' =>
+      '<FLEETING>.  Choose one:  • Send to Reserve target Character with Hand Cost {4} or less.  • Discard target Permanent with Hand Cost {4} or less.  #If you control two or more Landmarks, create a <BRASSBUG> Robot token in target Expedition.#',
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN($this, FLEETING),
+        [
+          'optional' => true,
+          'type' => NODE_XOR,
+          'childs' => [
+            FT::ACTION(TARGET, ['maxHandCost' => 4, 'effect' => FT::DISCARD_TO_RESERVE()]),
+            FT::ACTION(TARGET, ['maxHandCost' => 4, 'targetType' => [PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+          ],
+        ],
+        FT::ACTION(CHECK_CONDITION, [
+          'condition' => 'control2Landmarks',
+          'effect' => FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'AX_Common_Brassbug',
+            'targetLocation' => STORMS,
+          ]),
+        ])
+      ),
+    ];
+  }
+}
---
+++ modules/php/Cards/OD/OD_Rare_KelonBurst.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+
+class OD_Rare_KelonBurst extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_23_R2',
+      'asset' => 'ALT_CORE_B_AX_23_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Kelon Burst',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'There\'s an enduring legend in the Suspira quarries: the existence of another type of Kelon.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' =>
+      '$<FLEETING>.  Choose one:  • Send to Reserve target Character with Hand Cost {4} or less.  • Discard target Permanent with Hand Cost {4} or less.',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::SEQ(FT::GAIN($this, FLEETING), [
+        'optional' => true,
+        'type' => NODE_XOR,
+        'childs' => [
+          FT::ACTION(TARGET, ['maxHandCost' => 4, 'effect' => FT::DISCARD_TO_RESERVE()]),
+          FT::ACTION(TARGET, ['maxHandCost' => 4, 'targetType' => [PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+        ],
+      ]),
+    ];
+  }
+}
