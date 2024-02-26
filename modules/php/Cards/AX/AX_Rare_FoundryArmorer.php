b2cd359779fc8b8e1e4ee9d6bb043b450f81b22a
vtoper <vincent.toper@gmail.com>
2024-02-26 22:58:44 +0100

    Rare_FoundryArmorer

--- modules/php/Cards/AX/AX_Rare_FoundryArmorer.php
+++ modules/php/Cards/AX/AX_Rare_FoundryArmorer.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace ALT\Cards\BR;
+namespace ALT\Cards\AX;
 
 use ALT\Helpers\FT;
 
 
-class BR_Rare_FoundryArmorer extends \ALT\Models\Card
+class AX_Rare_FoundryArmorer extends \ALT\Models\Card
 {
   public function __construct($row)
   {
     parent::__construct($row);
     $this->properties = [
-      'uid' => 'ALT_CORE_B_AX_16_R2',
+      'uid' => 'ALT_CORE_B_AX_16_R1',
       'asset' => 'ALT_CORE_B_AX_16_R1',
 
-      'faction' => FACTION_BR,
+      'faction' => FACTION_AX,
       'rarity' => RARITY_RARE,
       'name' => 'Foundry Armorer',
       'typeline' => 'Character - Engineer',
@@ -22,14 +22,13 @@ class BR_Rare_FoundryArmorer extends \ALT\Models\Card
       'flavorText' => 'No Brassbug would survive in the Tumult without armor.',
       'artist' => 'Anh Tung',
       'subtypes' => [ENGINEER],
-      'effectDesc' => '{R} Create a <BRASSBUG> Robot token in target Expedition.',
+      'effectDesc' => '#{J}# Create a <BRASSBUG> Robot token in target Expedition.',
       'forest' => 2,
       'mountain' => 2,
       'ocean' => 2,
-      'costHand' => 2,
+      'costHand' => 3,
       'costReserve' => 3,
-      'changedStats' => ['costHand'],
-      'effectReserve' => FT::ACTION(INVOKE_TOKEN, [
+      'effectPlayed' => FT::ACTION(INVOKE_TOKEN, [
         'pId' => 'source',
         'tokenType' => 'AX_Common_Brassbug',
         'targetLocation' => STORMS,
