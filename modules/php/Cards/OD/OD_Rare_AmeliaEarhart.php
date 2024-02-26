7af710420138e99320790002559dbb9dfe6c58bc
vtoper <vincent.toper@gmail.com>
2024-02-26 22:36:19 +0100

    Rare_AmeliaEarhart

---
+++ modules/php/Cards/OD/OD_Rare_AmeliaEarhart.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+
+class OD_Rare_AmeliaEarhart extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_11_R2',
+      'asset' => 'ALT_CORE_B_AX_11_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Amelia Earhart',
+      'typeline' => 'Character - Adventurer',
+      'type' => CHARACTER,
+      'flavorText' => '"The most effective way to do it, is to do it."',
+      'artist' => 'Taras Susak',
+      'subtypes' => [ADVENTURER],
+      'effectDesc' => '#{H} I gain 1 boost.#',
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 1,
+      'effectHand' => FT::GAIN($this, BOOST)
+    ];
+  }
+}
