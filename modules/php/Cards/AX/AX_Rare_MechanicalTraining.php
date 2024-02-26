c6e0e1600311c3d2ab25880bc7810a74097f6181
vtoper <vincent.toper@gmail.com>
2024-02-26 23:21:29 +0100

    RAre MechanicalTraining

---
+++ modules/php/Cards/AX/AX_Rare_MechanicalTraining.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_MechanicalTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_22_R1',
+      'asset' => 'ALT_CORE_B_AX_22_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Mechanical Training',
+      'typeline' => 'Spell - Boon',
+      'type' => SPELL,
+      'flavorText' => 'To learn to create, first learn to fix.',
+      'artist' => 'Damian Audino',
+      'subtypes' => [BOON],
+      'effectDesc' => 'Activate the {j} triggers of #up to two# target Permanents you control.',
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costHand'],
+      'effectPlayed' => FT::ACTION(TARGET, [
+        'targetType' => [PERMANENT],
+        'upTo' => true,
+        'n' => 2,
+        'targetPlayer' => ME,
+        'hasEffects' => ['Played'],
+        'effect' => FT::ACTION(ACTIVATE_EFFECT, []),
+      ]),
+    ];
+  }
+}
