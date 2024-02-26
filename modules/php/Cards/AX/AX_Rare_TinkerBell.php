243ff501c99d6725ae38a6e665199f1326adce22
vtoper <vincent.toper@gmail.com>
2024-02-26 22:29:21 +0100

    Rare tinkerbell

---
+++ modules/php/Cards/AX/AX_Rare_TinkerBell.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+
+class AX_Rare_TinkerBell extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_09_R1',
+      'asset' => 'ALT_CORE_B_AX_09_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Tinker Bell',
+      'typeline' => 'Character - Fairy',
+      'type' => CHARACTER,
+      'flavorText' => 'Ting! Ting-a-ling! There\'s something magical in mischievous tinkering...',
+      'artist' => 'Anh Tung',
+      'subtypes' => [FAIRY],
+      'effectDesc' => '{R} $<SABOTAGE>.',
+      'forest' => 1,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'changedStats' => ['costHand'],
+      'effectReserve' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+    ];
+  }
+}
