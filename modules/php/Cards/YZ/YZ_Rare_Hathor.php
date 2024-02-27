6f673d5585e171ae17137c6ebbfc09777a441ef0
vtoper <vincent.toper@gmail.com>
2024-02-27 23:27:49 +0100

    Rare_Hathor

---
+++ modules/php/Cards/YZ/YZ_Rare_Hathor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+
+class YZ_Rare_Hathor extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_07_R2',
+      'asset' => 'ALT_CORE_B_LY_07_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Hathor',
+      'typeline' => 'Character - Deity Artist',
+      'type' => CHARACTER,
+      'flavorText' => 'Dance is the language of the soul.',
+      'artist' => 'Taras Susak',
+      'subtypes' => [DEITY, ARTIST],
+      'supportDesc' => '{D} : Return another card from your Reserve to your hand. (Discard me from Reserve to do this.)',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'supportIcon' => 'discard',
+      'effectSupport' => FT::ACTION(TARGET, [
+        'targetPlayer' => ME,
+        'targetLocation' => [RESERVE],
+        'targetType' => [CHARACTER, SPELL, PERMANENT],
+        'effect' => FT::RETURN_TO_HAND(),
+      ]),
+    ];
+  }
+}
