1442764090c5be55557e8fc5582986c340faebe2
vtoper <vincent.toper@gmail.com>
2024-02-27 23:09:40 +0100

    Rare_ManaEruption

---
+++ modules/php/Cards/MU/MU_Rare_ManaEruption.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_ManaEruption extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_29_R2',
+      'asset' => 'ALT_CORE_B_BR_29_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Mana Eruption',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'Flames fly from his hand as he falls towards the monster, leaving a trail of glowing fire behind him...',
+      'artist' => 'Zero Wen',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' => '$<FLEETING>.  Discard one of your Mana Orbs. If you do, discard target Character or Permanent.',
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN($this, FLEETING),
+        FT::ACTION(TARGET, [
+          'targetPlayer' => ME,
+          'targetLocation' => [MANA],
+          'targetType' => [CHARACTER, TOKEN, SPELL, PERMANENT],
+          'effect' => FT::ACTION(DISCARD, []),
+        ]),
+        FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN, PERMANENT], 'effect' => FT::ACTION(DISCARD, [])])
+      ),
+    ];
+  }
+}
