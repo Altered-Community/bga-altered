ec6c53322fc42ee7e5cddea6e44588c56390a221
vtoper <vincent.toper@gmail.com>
2024-02-27 13:35:55 +0100

    Rare foundry bastion

---
+++ modules/php/Cards/AX/AX_Rare_TheFoundryAxiomBastion.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+
+class AX_Rare_TheFoundryAxiomBastion extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_29_R1',
+      'asset' => 'ALT_CORE_B_AX_29_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'The Foundry, Axiom Bastion',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' =>
+      'Embedded in the cliff wall, it overlooks the city and reminds everyone who looks up at it that nothing is unreachable if you put your mind to it.',
+      'artist' => 'Jean-Baptiste Andrier',
+      'subtypes' => [LANDMARK],
+      'effectDesc' =>
+      '#{J} Draw a card.#  {T} : Activate the {r} triggers of the next Character you play from your hand this turn.',
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME]),
+      'effectTap' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'triggerEffectOfNextCharacter', 'args' => ['type' => CHARACTER, 'from' => HAND, 'effect' => RESERVE]]),
+
+    ];
+  }
+}
---
+++ modules/php/Cards/BR/BR_Rare_TheFoundryAxiomBastion.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+
+class BR_Rare_TheFoundryAxiomBastion extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_29_R2',
+      'asset' => 'ALT_CORE_B_AX_29_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'The Foundry, Axiom Bastion',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' =>
+      'Embedded in the cliff wall, it overlooks the city and reminds everyone who looks up at it that nothing is unreachable if you put your mind to it.',
+      'artist' => 'Jean-Baptiste Andrier',
+      'subtypes' => [LANDMARK],
+      'effectDesc' => '{T} : Activate the {r} triggers of the next Character you play from your hand this turn.',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectTap' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'triggerEffectOfNextCharacter', 'args' => ['type' => CHARACTER, 'from' => HAND, 'effect' => RESERVE]]),
+
+    ];
+  }
+}
