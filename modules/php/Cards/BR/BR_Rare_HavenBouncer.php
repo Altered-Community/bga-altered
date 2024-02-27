19b645b2a4647e482cd0fdc545425411da81e7ec
vtoper <vincent.toper@gmail.com>
2024-02-27 22:13:50 +0100

    Rare HavenBouncer

---
+++ modules/php/Cards/BR/BR_Rare_HavenBouncer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+
+class BR_Rare_HavenBouncer extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_15_R1',
+      'asset' => 'ALT_CORE_B_BR_15_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'Haven Bouncer',
+      'typeline' => 'Character - Soldier',
+      'type' => CHARACTER,
+      'flavorText' => 'Only the bravest can enter Haven.',
+      'artist' => 'Edward Cheekokseang',
+      'subtypes' => [SOLDIER],
+      'effectDesc' => '{H} $<SABOTAGE>.  {R} I gain #2 boosts#.',
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+      'effectReserve' => FT::GAIN($this, BOOST, 2),
+    ];
+  }
+}
