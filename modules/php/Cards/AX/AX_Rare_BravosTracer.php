6a950b71f56d075df4ed4032a051e04435583425
vtoper <vincent.toper@gmail.com>
2024-02-27 13:43:45 +0100

    AX_Rare_BravosTracer

---
+++ modules/php/Cards/AX/AX_Rare_BravosTracer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+
+class AX_Rare_BravosTracer extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_07_R2',
+      'asset' => 'ALT_CORE_B_BR_07_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Bravos Tracer',
+      'typeline' => 'Character - Adventurer',
+      'type' => CHARACTER,
+      'flavorText' => '"I only feel alive when I hear the wind whistling in my ears."',
+      'artist' => 'Justice Wong',
+      'subtypes' => [ADVENTURER],
+      'effectDesc' => '{J} I gain <FLEETING_CHAR>. (If I would be sent to Reserve, discard me instead.)',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::GAIN($this, FLEETING),
+    ];
+  }
+}
