d71c26dbca1cdef8198b7e7c145c941f2840a16c
vtoper <vincent.toper@gmail.com>
2024-02-27 13:37:39 +0100

    YZ_Rare_BrassbugHive

---
+++ modules/php/Cards/YZ/YZ_Rare_BrassbugHive.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+
+class YZ_Rare_BrassbugHive extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_AX_30_R2',
+      'asset' => 'ALT_CORE_B_AX_30_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Brassbug Hive',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' => 'What could possibly go wrong with an adorable little self-replicating autonomous robot?',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [LANDMARK],
+      'effectDesc' =>
+      '{J} Create a <BRASSBUG> Robot token in target Expedition.  At Noon — Create a <BRASSBUG> Robot token in target Expedition.',
+      'costHand' => 5,
+      'costReserve' => 5,
+      'effectPlayed' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'AX_Common_Brassbug',
+        'targetLocation' => STORMS,
+      ]),
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'myTurn',
+          'output' => FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'AX_Common_Brassbug',
+            'targetLocation' => STORMS,
+          ]),
+        ],
+      ],
+    ];
+  }
+}
