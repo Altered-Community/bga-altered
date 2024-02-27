4d56efa8f7c0a7a285dd3c133d373f15e2efc9ba
vtoper <vincent.toper@gmail.com>
2024-02-27 13:40:26 +0100

    Rare Issun Boshi

---
+++ modules/php/Cards/MU/MU_Rare_Issunboshi.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+
+class MU_Rare_Issunboshi extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_05_R2',
+      'asset' => 'ALT_CORE_B_BR_05_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Issun-bōshi',
+      'typeline' => 'Character - Adventurer',
+      'type' => CHARACTER,
+      'flavorText' => 'Small stature, big heart, immense adventures.',
+      'artist' => 'Anh Tung',
+      'subtypes' => [ADVENTURER],
+      'effectDesc' => '#{R} Target Character gains 1 boost.#',
+      'supportDesc' => '{D} : The next Character you play this turn gains 1 boost. (Discard me from Reserve to do this.)',
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 2,
+      'costHand' => 1,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'supportIcon' => 'discard',
+      'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterGains1Boost']),
+      'effectReserve' => FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST])]),
+
+    ];
+  }
+}
