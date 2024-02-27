85ec0f1c92b7b6d0dd8b0895aa540776f34031c6
vtoper <vincent.toper@gmail.com>
2024-02-27 22:03:15 +0100

    Rare Kappa

---
+++ modules/php/Cards/BR/BR_Rare_Kappa.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+class BR_Rare_Kappa extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_13_R1',
+      'asset' => 'ALT_CORE_B_BR_13_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'Kappa',
+      'typeline' => 'Character - Trainer Spirit',
+      'type' => CHARACTER,
+      'flavorText' => '"Push through the pain. Giving up would hurt far more."',
+      'artist' => 'Matteo Spirito',
+      'subtypes' => [TRAINER, SPIRIT],
+      'effectDesc' => '#$<SEASONED>.#',
+      'supportDesc' => '#{D} : The next Character you play this turn gains 1 boost.# (Discard me from Reserve to do this.)',
+      'forest' => 4,
+      'mountain' => 4,
+      'ocean' => 5,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'supportIcon' => 'discard',
+      'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterGains1Boost']),
+      'seasoned' => true,
+    ];
+  }
+}
