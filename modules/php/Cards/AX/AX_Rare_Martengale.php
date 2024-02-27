bed0ffd92dbea380d30cd534b8a1afab41af1149
vtoper <vincent.toper@gmail.com>
2024-02-27 23:16:37 +0100

    Rare MArtengale

---
+++ modules/php/Cards/AX/AX_Rare_Martengale.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+class AX_Rare_Martengale extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_04_R2',
+      'asset' => 'ALT_CORE_B_LY_04_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Martengale',
+      'typeline' => 'Character - Spirit Animal',
+      'type' => CHARACTER,
+      'flavorText' => 'Spotting a martengale is always a good omen.',
+      'artist' => 'Nestor Papatriantafyllou',
+      'subtypes' => [SPIRIT, ANIMAL],
+      'supportDesc' => '{D} : The next card you play this turn costs {1} less. (Discard me from Reserve to do this.)',
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 0,
+      'costHand' => 1,
+      'costReserve' => 1,
+      'supportIcon' => 'discard',
+      'effectSupport' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => ALL, 'reduction' => 1]],
+      ],
+    ];
+  }
+}
---
+++ modules/php/Cards/LY/LY_Rare_Martengale.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ALT\Cards\LY;
+
+class LY_Rare_Martengale extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_04_R1',
+      'asset' => 'ALT_CORE_B_LY_04_R1',
+
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_RARE,
+      'name' => 'Martengale',
+      'typeline' => 'Character - Spirit Animal',
+      'type' => CHARACTER,
+      'flavorText' => 'Spotting a martengale is always a good omen.',
+      'artist' => 'Nestor Papatriantafyllou',
+      'subtypes' => [SPIRIT, ANIMAL],
+      'effectDesc' => '#If you would roll a die, you may add 1 to its result.# (Choose after you see the result.)',
+      'supportDesc' => '{D} : The next card you play this turn costs {1} less. (Discard me from Reserve to do this.)',
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 0,
+      'costHand' => 1,
+      'costReserve' => 1,
+    ];
+  }
+}
