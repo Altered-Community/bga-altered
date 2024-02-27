56b086287ee0e59eec3645c7e9989823f3c501e5
vtoper <vincent.toper@gmail.com>
2024-02-27 21:46:10 +0100

    Rare HavenTrainee

---
+++ modules/php/Cards/OD/OD_Rare_HavenTrainee.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+
+class OD_Rare_HavenTrainee extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_BR_09_R2',
+      'asset' => 'ALT_CORE_B_BR_09_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Haven Trainee',
+      'typeline' => 'Character - Apprentice',
+      'type' => CHARACTER,
+      'flavorText' => '"All right, lad, show me what you’ve learned."',
+      'artist' => 'Atanas Lozanski',
+      'subtypes' => [APPRENTICE],
+      'effectDesc' => '{R} #Create an <ORDIS_RECRUIT> Soldier token in each of your Expeditions.#',
+      'forest' => 3,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 4,
+      'effectReserve' => FT::SEQ(
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'OD_Common_OrdisRecruit',
+          'targetLocation' => [STORM_RIGHT],
+        ]),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'OD_Common_OrdisRecruit',
+          'targetLocation' => [STORM_LEFT],
+        ])
+      ),
+    ];
+  }
+}
