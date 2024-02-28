4645000c3cc269c4640402c29146bd550549a42d
vtoper <vincent.toper@gmail.com>
2024-02-28 17:31:19 +0100

    Rare_OuroborosCroupier

---
+++ modules/php/Cards/AX/AX_Rare_OuroborosCroupier.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_OuroborosCroupier extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_17_R2',
+      'asset' => 'ALT_CORE_B_LY_17_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Ouroboros Croupier',
+      'typeline' => 'Character - Citizen',
+      'type' => CHARACTER,
+      'flavorText' => 'The house never loses.',
+      'artist' => 'Anh Tung',
+      'subtypes' => [CITIZEN],
+      'effectDesc' => '{H} Roll a die. On a 4+, draw a card. On a 1-3, $<RESUPPLY_LOW>.',
+      'supportDesc' => '#{D} : The next card you play this turn costs {1} less.# (Discard me from Reserve to do this.)',
+      'forest' => 0,
+      'mountain' => 5,
+      'ocean' => 5,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['mountain', 'ocean'],
+      'effectHand' => FT::ACTION(ROLL_DIE, [
+        'effect' => [
+          '1-3' => FT::ACTION(RESUPPLY, []),
+          '4+' => FT::ACTION(DRAW, ['players' => ME]),
+        ],
+      ]),
+      'supportIcon' => 'discard',
+      'effectSupport' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => ALL, 'reduction' => 1]],
+      ],
+    ];
+  }
+}
