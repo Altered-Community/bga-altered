0459b12cd1c215b245df56f25ce43aa711a83f07
vtoper <vincent.toper@gmail.com>
2024-02-28 18:02:06 +0100

    BR_Rare_AllIn

---
+++ modules/php/Cards/BR/BR_Rare_AllIn.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+class BR_Rare_AllIn extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_LY_25_R2',
+      'asset' => 'ALT_CORE_B_LY_25_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'All In!',
+      'typeline' => 'Spell - Boon',
+      'type' => SPELL,
+      'flavorText' => 'There\'s a time to be cautious, and a time to bet it all!',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [BOON],
+      'effectDesc' => 'Roll a die. Target Character gains X boosts, where X is the result.',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(ROLL_DIE, [
+        'effect' => ['1+' => FT::ACTION(TARGET, ['effect' => FT::GAIN('effect', BOOST, 'die')])],
+      ]),
+    ];
+  }
+}
