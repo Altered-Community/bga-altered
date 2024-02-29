93dd2450db2a9842c689922f3014eaf3fd81a997
vtoper <vincent.toper@gmail.com>
2024-02-29 22:04:03 +0100

    Rare_Mowgli

---
+++ modules/php/Cards/MU/MU_Rare_Mowgli.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_Mowgli extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_07_R1',
+      'asset' => 'ALT_CORE_B_MU_07_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Mowgli',
+      'typeline' => 'Character - Druid',
+      'type' => CHARACTER,
+      'flavorText' => '"Wake up, you lazy furball!"',
+      'artist' => 'Ba Vo',
+      'subtypes' => [DRUID],
+      'supportDesc' =>
+      '#{D} : Target Character with Hand Cost {3} or less gains <ANCHORED>.# (Discard me from Reserve to do this.)',
+      'forest' => 3,
+      'mountain' => 0,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 1,
+      'supportIcon' => 'discard',
+      'effectSupport' => FT::ACTION(TARGET, ['maxHandCost' => 3, 'effect' => FT::GAIN($this, ANCHORED)]),
+
+    ];
+  }
+}
