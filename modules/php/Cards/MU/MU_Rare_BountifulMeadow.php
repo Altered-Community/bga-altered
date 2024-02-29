d7330da646146c1310837b98902dfff95fc061f2
vtoper <vincent.toper@gmail.com>
2024-02-29 22:58:20 +0100

    Rare_BountifulMeadow

---
+++ modules/php/Cards/MU/MU_Rare_BountifulMeadow.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_BountifulMeadow extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_24_R1',
+      'asset' => 'ALT_CORE_B_MU_24_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Bountiful Meadow',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' => 'There\'s no greater joy than seeing all that is green thrive and grow.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [LANDMARK],
+      'effectDesc' => '#{J} You may pay {1} to $<RESUPPLY_INF>.#  {T} : The next Plant you play this turn costs {1} less.',
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectTap' =>  [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => PLANT, 'reduction' => 1]],
+      ],
+      'effectPlayed' => FT::SEQ_OPTIONAL(
+        FT::ACTION(PAY, ['pay' => 1]),
+        FT::ACTION(RESUPPLY, [])
+      )
+    ];
+  }
+}
---
+++ modules/php/Cards/OD/OD_Rare_BountifulMeadow.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+class OD_Rare_BountifulMeadow extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_24_R2',
+      'asset' => 'ALT_CORE_B_MU_24_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Bountiful Meadow',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' => 'There\'s no greater joy than seeing all that is green thrive and grow.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [LANDMARK],
+      'effectDesc' => '{T} : The next #Bureaucrat# you play this turn costs {1} less.',
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectTap' =>  [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => BUREAUCRAT, 'reduction' => 1]],
+      ]
+    ];
+  }
+}
