28bb64a42cdef445a0d46237fbc8a942c0f25326
vtoper <vincent.toper@gmail.com>
2024-02-29 23:43:48 +0100

    AX_Rare_JeannedArc

---
+++ modules/php/Cards/AX/AX_Rare_JeannedArc.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_JeannedArc extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_OR_17_R2',
+      'asset' => 'ALT_CORE_B_OR_17_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => "Jeanne d'Arc",
+      'typeline' => 'Character - Soldier',
+      'type' => CHARACTER,
+      'flavorText' => 'She\'ll be followed long after she\'s gone.',
+      'artist' => 'Jean-Baptiste Andrier',
+      'subtypes' => [SOLDIER],
+      'effectDesc' => 'When I leave the Expedition zone — Create a #<BRASSBUG> Robot# token in each of your Expeditions.',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 5,
+      'costReserve' => 5,
+      'changedStats' => ['forest', 'mountain', 'ocean', 'costHand', 'costReserve'],
+      'effectPassive' => [
+        'LeaveExpedition' => [
+          'output' => FT::SEQ(
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'AX_Common_Brassbug',
+              'targetLocation' => [STORM_RIGHT],
+            ]),
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'AX_Common_Brassbug',
+              'targetLocation' => [STORM_LEFT],
+            ])
+          ),
+        ],
+      ],
+    ];
+  }
+}
