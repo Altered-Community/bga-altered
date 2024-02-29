dffc065c567c385e3b6a7ac6f52eac3e5ee6fb7f
vtoper <vincent.toper@gmail.com>
2024-02-29 22:16:49 +0100

    Rare_DaughterofYggdrasil

---
+++ modules/php/Cards/MU/MU_Rare_DaughterofYggdrasil.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_DaughterofYggdrasil extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_12_R1',
+      'asset' => 'ALT_CORE_B_MU_12_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Daughter of Yggdrasil',
+      'typeline' => 'Character - Plant',
+      'type' => CHARACTER,
+      'flavorText' => 'The children of Yggdrasil now take care of the world trees.',
+      'artist' => 'Nestor Papatriantafyllou',
+      'subtypes' => [PLANT],
+      'effectDesc' => '{H} #Each player# draws a card.',
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(DRAW, ['players' => ALL]),
+
+    ];
+  }
+}
