f3e97e03dec617de9737e6e1c25c200b1d4d5f26
vtoper <vincent.toper@gmail.com>
2024-02-29 23:47:58 +0100

    Rare_Anubis

---
+++ modules/php/Cards/OD/OD_Rare_Anubis.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+class OD_Rare_Anubis extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_OR_20_R1',
+      'asset' => 'ALT_CORE_B_OR_20_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Anubis',
+      'typeline' => 'Character - Deity',
+      'type' => CHARACTER,
+      'flavorText' => 'Only a heart as light as a feather can stand up to the judgment of Anubis.',
+      'artist' => 'Romain Kurdi',
+      'subtypes' => [DEITY],
+      'effectDesc' => '{J} Each player sacrifices a Character.',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['mountain', 'costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'AllPlayersSacrifice1'])
+
+    ];
+  }
+}
