24a73a391448097b84a1db2bfda7bf9a87bd61e7
vtoper <vincent.toper@gmail.com>
2024-02-29 23:01:48 +0100

    Rare_ManaReaping

---
+++ modules/php/Cards/BR/BR_Rare_ManaReaping.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+class BR_Rare_ManaReaping extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_26_R2',
+      'asset' => 'ALT_CORE_B_MU_26_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'Mana Reaping',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'Everything is energy and that\'s all there is to it.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' => '$<FLEETING>.  Put target Character or Permanent in its owner\'s Mana zone (as an exhausted Mana Orb).',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN($this, FLEETING),
+        FT::ACTION(TARGET, [
+          'targetType' => [PERMANENT, CHARACTER],
+          'effect' => FT::ACTION(DISCARD, ['destination' => MANA, 'tapped' => true]),
+        ])
+      )
+    ];
+  }
+}
---
+++ modules/php/Cards/MU/MU_Rare_ManaReaping.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
+
+class MU_Rare_ManaReaping extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_MU_26_R1',
+      'asset' => 'ALT_CORE_B_MU_26_R1',
+
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => 'Mana Reaping',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'Everything is energy and that\'s all there is to it.',
+      'artist' => 'HuoMiao Studio',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' => 'Put target Character or Permanent in its owner\'s Mana zone (as an exhausted Mana Orb).',
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(TARGET, [
+        'targetType' => [PERMANENT, CHARACTER],
+        'effect' => FT::ACTION(DISCARD, ['destination' => MANA, 'tapped' => true]),
+      ])
+
+    ];
+  }
+}
