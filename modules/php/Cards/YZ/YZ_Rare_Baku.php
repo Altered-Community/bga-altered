8694cf5992603d9fd7ad86bc384efa1caab0ef45
vtoper <vincent.toper@gmail.com>
2024-03-02 00:01:33 +0100

    Rare baku

---
+++ modules/php/Cards/YZ/YZ_Rare_Baku.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_Baku extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_10_R1',
+      'asset' => 'ALT_CORE_B_YZ_10_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Baku',
+      'typeline' => 'Character - Spirit',
+      'type' => CHARACTER,
+      'flavorText' => '"I’ve seen it myself! \'Baku! Baku!\', it says, eating up ghosts as though they were candies."',
+      'artist' => 'Zero Wen',
+      'subtypes' => [SPIRIT],
+      'effectDesc' => '#{J}# Target opponent discards a card from their hand.',
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['costReserve'],
+      'effectPlayed' => FT::ACTION(TARGET_PLAYER, ['effect' =>  FT::ACTION(
+        TARGET,
+        [
+          'targetPlayer' => ME,
+          'targetLocation' => [HAND],
+          'targetType' => [CHARACTER, SPELL, PERMANENT],
+          'effect' => FT::ACTION(DISCARD, []),
+        ]
+      )])
+    ];
+  }
+}
