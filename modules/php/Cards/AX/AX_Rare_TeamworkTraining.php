c027ebbffe37284a0d19c65b2c01cb1f1ec137a1
vtoper <vincent.toper@gmail.com>
2024-03-01 20:54:56 +0100

    Rare_TeamworkTraining

---
+++ modules/php/Cards/AX/AX_Rare_TeamworkTraining.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
+
+class AX_Rare_TeamworkTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_OR_24_R2',
+      'asset' => 'ALT_CORE_B_OR_24_R1',
+
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => 'Teamwork Training',
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' => 'Through their Hive Mind, Ordis turns the thoughts of the multitude into a unified will.',
+      'artist' => 'Jean-Baptiste Andrier',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' =>
+      '$<FLEETING>.  Send to Reserve target Character with Hand Cost {X} or less, where X is the number of Characters you control.',
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN($this, FLEETING),
+        FT::ACTION(TARGET, ['maxHandCost' => 'controlledCharacter', 'effect' => FT::DISCARD_TO_RESERVE()])
+      ),
+    ];
+  }
+}
