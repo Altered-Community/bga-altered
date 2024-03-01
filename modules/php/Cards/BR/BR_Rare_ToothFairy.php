18282bed1a879b46d5e1b86a230dfb50cdb6e9d2
vtoper <vincent.toper@gmail.com>
2024-03-01 23:43:47 +0100

    BR_Rare_ToothFairy

---
+++ modules/php/Cards/BR/BR_Rare_ToothFairy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
+
+class BR_Rare_ToothFairy extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_06_R2',
+      'asset' => 'ALT_CORE_B_YZ_06_R1',
+
+      'faction' => FACTION_BR,
+      'rarity' => RARITY_RARE,
+      'name' => 'Tooth Fairy',
+      'typeline' => 'Character - Fairy',
+      'type' => CHARACTER,
+      'flavorText' => '"Show me those pearly whites."',
+      'artist' => 'Anh Tung',
+      'subtypes' => [FAIRY],
+      'effectDesc' => '{H} $<SABOTAGE>.',
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+    ];
+  }
+}
