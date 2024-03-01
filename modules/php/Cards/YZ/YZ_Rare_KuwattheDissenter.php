700eaf0ed72b63e8606826c7d17115a7e12adffb
vtoper <vincent.toper@gmail.com>
2024-03-02 00:09:20 +0100

    Rare_KuwattheDissenter

---
+++ modules/php/Cards/YZ/YZ_Rare_KuwattheDissenter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_KuwattheDissenter extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_15_R1',
+      'asset' => 'ALT_CORE_B_YZ_15_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Kuwat, the Dissenter',
+      'typeline' => 'Character - Mage',
+      'type' => CHARACTER,
+      'flavorText' => '"There are gates one should not open, there are seals one should not breach. And yet I will."',
+      'artist' => 'Ba Vo',
+      'subtypes' => [MAGE],
+      'effectDesc' => '{J} Sacrifice a Character.',
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 5,
+      'costHand' => 4,
+      'costReserve' => 3,
+      'changedStats' => ['costReserve'],
+      'effectPlayed' =>  FT::ACTION(
+        TARGET,
+        [
+          'targetPlayer' => ME,
+          'targetType' => [CHARACTER, TOKEN],
+          'targetLocation' => STORMS,
+          'effect' => FT::ACTION(DISCARD, ['desc' => 'sacrifice'])
+        ]
+      ),
+    ];
+  }
+}
