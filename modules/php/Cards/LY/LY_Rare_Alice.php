85b0cf467199278eda217a5f17d416073535466e
vtoper <vincent.toper@gmail.com>
2024-03-02 00:04:42 +0100

    LY_Rare_Alice

---
+++ modules/php/Cards/LY/LY_Rare_Alice.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
+
+class LY_Rare_Alice extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_13_R2',
+      'asset' => 'ALT_CORE_B_YZ_13_R1',
+
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_RARE,
+      'name' => 'Alice',
+      'typeline' => 'Character - Citizen',
+      'type' => CHARACTER,
+      'flavorText' => '"One good turn deserves another."',
+      'artist' => 'Taras Susak',
+      'subtypes' => [CITIZEN],
+      'supportDesc' => '{D} : <AFTER_YOU>. (Discard me from Reserve to do this.)',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 4,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['forest', 'mountain'],
+      'effectSupport' => FT::ACTION(AFTER_YOU, []),
+      'supportIcon' => 'discard'
+    ];
+  }
+}
