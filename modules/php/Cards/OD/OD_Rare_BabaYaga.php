b7d998b8361679b94a0381244bee3d403f66949f
vtoper <vincent.toper@gmail.com>
2024-03-02 00:02:23 +0100

    OD_Rare_BabaYaga

---
+++ modules/php/Cards/OD/OD_Rare_BabaYaga.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
+
+class OD_Rare_BabaYaga extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_11_R2',
+      'asset' => 'ALT_CORE_B_YZ_11_R1',
+
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => 'Baba Yaga',
+      'typeline' => 'Character - Mage',
+      'type' => CHARACTER,
+      'flavorText' => 'Help or harm? Only the card will tell.',
+      'artist' => 'Atanas Lozanski',
+      'subtypes' => [MAGE],
+      'effectDesc' => '{H} Draw a card.',
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'changedStats' => ['forest', 'mountain'],
+      'effectHand' => FT::ACTION(DRAW, ['players' => ME]),
+
+    ];
+  }
+}
