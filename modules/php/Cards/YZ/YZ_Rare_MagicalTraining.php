8e0bc78599c978887e85dd1e9a2d09fb8e2b1976
vtoper <vincent.toper@gmail.com>
2024-03-02 21:10:55 +0100

    Rare_MagicalTraining

---
+++ modules/php/Cards/YZ/YZ_Rare_MagicalTraining.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_MagicalTraining extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_19_R1',
+      'asset' => 'ALT_CORE_B_YZ_19_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Magical Training',
+      'typeline' => 'Spell - Conjuration',
+      'type' => SPELL,
+      'flavorText' => 'Without practice, the gift of Alteration will come to nothing.',
+      'artist' => 'Zero Wen',
+      'subtypes' => [CONJURATION],
+      'effectDesc' => 'Draw a card.',
+      'costHand' => 1,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME])
+
+    ];
+  }
+}
