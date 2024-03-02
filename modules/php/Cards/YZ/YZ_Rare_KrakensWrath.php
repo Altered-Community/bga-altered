6044e0aa36b8ad83325713493034dd458940dbd2
vtoper <vincent.toper@gmail.com>
2024-03-02 21:34:50 +0100

    Rare_KrakensWrath

---
+++ modules/php/Cards/YZ/YZ_Rare_KrakensWrath.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_KrakensWrath extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_26_R1',
+      'asset' => 'ALT_CORE_B_YZ_26_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => "Kraken's Wrath",
+      'typeline' => 'Spell - Disruption',
+      'type' => SPELL,
+      'flavorText' =>
+      'The roaring waves crashed down over the charging armies, and countless soldiers vanished beneath the raging waters.',
+      'artist' => 'Matteo Spirito',
+      'subtypes' => [DISRUPTION],
+      'effectDesc' => '$<FLEETING>.  Send to Reserve up to #four# target Characters with a total Hand Cost #{6} or less#.',
+      'costHand' => 5,
+      'costReserve' => 5,
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN($this, FLEETING),
+        FT::ACTION(TARGET, ['upTo' => true, 'n' => 4, 'totalCost' => 6, 'effect' => FT::DISCARD_TO_RESERVE()])
+      ),
+    ];
+  }
+}
