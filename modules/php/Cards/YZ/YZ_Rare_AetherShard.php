7aa1240ad14b98cccf5b7605960bbdbe90624d99
vtoper <vincent.toper@gmail.com>
2024-03-02 21:46:31 +0100

    Rare_AetherShard

---
+++ modules/php/Cards/YZ/YZ_Rare_AetherShard.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_AetherShard extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_29_R1',
+      'asset' => 'ALT_CORE_B_YZ_29_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Aether Shard',
+      'typeline' => 'Permanent - Landmark',
+      'type' => PERMANENT,
+      'flavorText' => 'No mere trinket, but a fragment of purest dream.',
+      'artist' => 'Anh Tung',
+      'subtypes' => [LANDMARK],
+      'effectDesc' => '#{J} Draw a card.#  At Noon — Draw a card.',
+      'costHand' => 5,
+      'costReserve' => 5,
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'myTurn',
+          'output' => FT::ACTION(DRAW, ['players' => ME])
+        ]
+      ],
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME])
+    ];
+  }
+}
