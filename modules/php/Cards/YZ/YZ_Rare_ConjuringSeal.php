6b74ef11c9df84341cc05c6f81b7126cec4032e1
vtoper <vincent.toper@gmail.com>
2024-03-02 21:25:40 +0100

    Rare_ConjuringSeal

---
+++ modules/php/Cards/YZ/YZ_Rare_ConjuringSeal.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
+
+class YZ_Rare_ConjuringSeal extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CORE_B_YZ_23_R1',
+      'asset' => 'ALT_CORE_B_YZ_23_R1',
+
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_RARE,
+      'name' => 'Conjuring Seal',
+      'typeline' => 'Spell - Conjuration',
+      'type' => SPELL,
+      'flavorText' => 'The Imagination shapes our reality just as our beliefs shaped the Æther.',
+      'artist' => 'Khoa Viet',
+      'subtypes' => [CONJURATION],
+      'effectDesc' => 'Draw #three cards#.',
+      'costHand' => 4,
+      'costReserve' => 5,
+      'changedStats' => ['costHand'],
+      'effectPlayed' => FT::ACTION(DRAW, ['n' => 3, 'players' => ME])
+    ];
+  }
+}
