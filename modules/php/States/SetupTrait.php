8e2e23c27d0424b4e627e6cefabf240312372f0a
vtoper <vincent.toper@gmail.com>
2024-03-13 21:18:19 +0100

    Use of BGA API

--- modules/php/States/SetupTrait.php
+++ modules/php/States/SetupTrait.php
@@ -93,7 +93,11 @@ trait SetupTrait
 
   function connectToAPI($user, $secret)
   {
-    $response = $this->equinoxAPIConnect(['mode' => 'login', 'user' => $user, 'secret' => $secret]);
+    // $response = $this->equinoxAPIConnect(['mode' => 'login', 'user' => $user, 'secret' => $secret]);
+    $response = self::masterNodeRequest('getGameSpecificMetaInfos', [
+      "game" => "altered",
+      'mode' => 'login', 'user' => $user, 'secret' => $secret
+    ]);
     if (!isset($response['token'])) {
       throw new \feException('Invalid login or password');
     }
@@ -103,8 +107,11 @@ trait SetupTrait
 
   function updateAPIDeckList($pId, $token)
   {
-    $decks = $this->equinoxAPIConnect(['mode' => 'deckList', 'token' => $token]);
-
+    // $decks = $this->equinoxAPIConnect(['mode' => 'deckList', 'token' => $token]);
+    $decks = self::masterNodeRequest('getGameSpecificMetaInfos', [
+      "game" => "altered",
+      'mode' => 'deckList', 'token' => $token
+    ]);
     $deckList = [];
     $numDeck = 0;
     foreach ($decks['hydra:member'] as $deck) {
@@ -117,8 +124,11 @@ trait SetupTrait
 
   function getAPIDeckContent($token, $deckId)
   {
-    $deck = $this->equinoxAPIConnect(['mode' => 'deck', 'token' => $token, 'deckId' => $deckId]);
-
+    // $deck = $this->equinoxAPIConnect(['mode' => 'deck', 'token' => $token, 'deckId' => $deckId]);
+    $deck = self::masterNodeRequest('getGameSpecificMetaInfos', [
+      "game" => "altered",
+      'mode' => 'deck', 'token' => $token, 'deckId' => $deckId
+    ]);
     $deckContent = [];
     $deckContent[HERO] = ['card' => Cards::getCardClass($deck['alterator']['reference']), 'n' => 1];
     foreach ($deck['deckUserListCards'] as $c) {
