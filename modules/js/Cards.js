9d62c67a1e2a55e4be733b1461eda632a20f82b7
vtoper <vincent.toper@gmail.com>
2024-05-22 10:43:37 +0200

    124588: "Ouroboros Croupier text error

--- modules/js/Cards.js
+++ modules/js/Cards.js
@@ -282,8 +282,8 @@ define(['dojo', 'dojo/_base/declare', g_gamethemeurl + 'modules/js/cardsData.js'
             `<div class='card-compare'>
               ${this.tplCard(card)}
               <div class='card-mockup' style='background-image:url("${g_gamethemeurl}misc/API/assets/${
-                card.properties.uid
-              }.jpg");'></div>
+              card.properties.uid
+            }.jpg");'></div>
             </div>`
           );
         });
@@ -1377,11 +1377,11 @@ define(['dojo', 'dojo/_base/declare', g_gamethemeurl + 'modules/js/cardsData.js'
           <div class='card-typeline'>${_(p.typeline)}</div>
 
           <div class='card-forest' data-size='${sizes.forest}' data-initial='${p.forest}' data-boost='${i.boost}'>${
-            p.forest
-          }</div>
+        p.forest
+      }</div>
           <div class='card-mountain' data-size='${sizes.mountain}' data-initial='${p.mountain}' data-boost='${i.boost}'>${
-            p.mountain
-          }</div>
+        p.mountain
+      }</div>
           <div class='card-ocean' data-size='${sizes.ocean}' data-initial='${p.ocean}' data-boost='${i.boost}'>${p.ocean}</div>
 
           <div class='card-text' style="font-size:${i.textFontSize}">
@@ -1779,11 +1779,19 @@ define(['dojo', 'dojo/_base/declare', g_gamethemeurl + 'modules/js/cardsData.js'
           text: _('Resupply'),
           reminder: _('Put the top card of your deck in Reserve.'),
         },
+        RESUPPLY_LOW: {
+          text: _('Resupply'),
+          reminder: _('Put the top card of your deck in Reserve.'),
+        },
         RESUPPLY_T: {
           text: _('Resupply'),
           reminder: _('Put the top card of your deck in Reserve.'),
         },
         SABOTAGE: {
+          text: _('Sabotage'),
+          reminder: _('Discard up to one target card from a Reserve.'),
+        },
+        SABOTAGE_LOW: {
           text: _('Sabotage'),
           reminder: _('Discard up to one target card from a Reserve.'),
         },
