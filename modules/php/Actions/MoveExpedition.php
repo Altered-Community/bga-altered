0e0f5e90d03ac8f62c2ed897d4cc1f4d841c6fc6
vtoper <vincent.toper@gmail.com>
2024-06-22 22:35:32 +0200

    128050: "Grand Endeavor: Region types not updated after expedition progress"

--- modules/php/Actions/MoveExpedition.php
+++ modules/php/Actions/MoveExpedition.php
@@ -87,6 +87,7 @@ class MoveExpedition extends \ALT\Models\Action
 
     foreach ($players as $player) {
       $player->advanceStorm($token, null, $n, true, $source);
+      Notifications::updateBiomes($player);
       $this->checkAfterListeners($player, ['moveExpedition' => $n]);
     }
 
