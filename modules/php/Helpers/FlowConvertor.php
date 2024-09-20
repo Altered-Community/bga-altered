90505fc9064d0aa75c51abedefdefeb215e8a093
Syarwin <timothee.pecatta@ens-lyon.fr>
2024-09-20 23:36:43 +0200

    138934: "mauvais effet de mon unique anubis"

--- modules/php/Helpers/FlowConvertor.php
+++ modules/php/Helpers/FlowConvertor.php
@@ -737,7 +737,7 @@ abstract class FlowConvertor
       ],
       147 => [
         'description' => clienttranslate('You may discard target Character.'),
-        'output' => FT::ACTION(TARGET, ['targetType' => [PERMANENT], 'upTo' => true, 'effect' => FT::ACTION(DISCARD, [])]),
+        'output' => FT::ACTION(TARGET, ['targetType' => [CHARACTER], 'upTo' => true, 'effect' => FT::ACTION(DISCARD, [])]),
       ],
       148 => [
         'description' => clienttranslate('All Characters in target Expedition gain <ASLEEP>.'),
