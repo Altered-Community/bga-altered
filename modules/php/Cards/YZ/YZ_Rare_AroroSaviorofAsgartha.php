51754d1a70241eb8981093749530b30a0b0b3dab
vtoper <vincent.toper@gmail.com>
2024-10-26 22:51:15 +0200

    Aroro, Savior of Asgartha

--- modules/php/Cards/YZ/YZ_Rare_AroroSaviorofAsgartha.php
+++ modules/php/Cards/YZ/YZ_Rare_AroroSaviorofAsgartha.php
@@ -1,32 +1,51 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
 
 class YZ_Rare_AroroSaviorofAsgartha extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_YZ_36_R1',
             'asset'  => 'ALT_ALIZE_B_YZ_36_R',
 
-    		'faction'  => FACTION_YZ,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Aroro, Savior of Asgartha"),
+            'faction'  => FACTION_YZ,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Aroro, Savior of Asgartha"),
             'typeline' => clienttranslate("Character - Mage"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Alone, she was able to turn back the tide.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Alone, she was able to turn back the tide.'),
             'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'TBF',
-   'subtypes'  => [MAGE],
- 				'effectDesc' => clienttranslate('{H} You may discard a Spell from your Reserve. If you do, I gain #2 boosts#.'),
- 				'supportDesc' => clienttranslate('#{D} : The next Spell you play this turn costs {1} less.# (Discard me from Reserve to do this.)'),
- 			     'supportIcon' => 'discard',
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 4, 
-     'costReserve' => 3, 
-     'changedStats' => ['ocean'], 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [MAGE],
+            'effectDesc' => clienttranslate('{H} You may discard a Spell from your Reserve. If you do, I gain #2 boosts#.'),
+            'supportDesc' => clienttranslate('#{D} : The next Spell you play this turn costs {1} less.# (Discard me from Reserve to do this.)'),
+            'supportIcon' => 'discard',
+            'forest' => 3,
+            'mountain' => 3,
+            'ocean' => 3,
+            'costHand' => 4,
+            'costReserve' => 3,
+            'changedStats' => ['ocean'],
+            'effectHand' => FT::ACTION(
+                TARGET,
+                [
+                    'targetType' => [SPELL],
+                    'targetPlayer' => ME,
+                    'targetLocation' => [RESERVE],
+                    'upTo' => true,
+                    'effect' => FT::SEQ(FT::ACTION(DISCARD, []), FT::GAIN(ME, BOOST, 2)),
+                ],
+                ['optional' => true]
+            ),
+            'effectSupport' => [
+                'action' => SPECIAL_EFFECT,
+                'args' => ['effect' => 'costReduction', 'args' => ['type' => SPELL, 'reduction' => 1]],
+            ],
+        ];
+    }
 }
