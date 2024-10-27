f0741e3dc0e1d03c85c439af31a82d25ed64827f
vtoper <vincent.toper@gmail.com>
2024-10-27 21:23:02 +0100

    Daring Porter

--- modules/php/Cards/AX/AX_Rare_DaringPorter.php
+++ modules/php/Cards/AX/AX_Rare_DaringPorter.php
@@ -1,29 +1,35 @@
 <?php
+
 namespace ALT\Cards\AX;
+
+use ALT\Helpers\FT;
 
 class AX_Rare_DaringPorter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_AX_37_R1',
             'asset'  => 'ALT_ALIZE_B_AX_37_R',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Daring Porter"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Daring Porter"),
             'typeline' => clienttranslate("Character - Adventurer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Someone has to shoulder the heavy burden of responsibility.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Someone has to shoulder the heavy burden of responsibility.'),
             'artist' => "Zero Wen",
-			'extension'=>'TBF',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('#{J}# <EXHAUSTED_RESUPPLY>. (Put the top card of your deck in Reserve, then exhaust it {T}. Exhausted cards can\'t be played and have no Support abilities.)'),
-     'forest' => 2, 
-     'mountain' => 3, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [ADVENTURER],
+            'effectDesc' => clienttranslate('#{J}# <EXHAUSTED_RESUPPLY>. (Put the top card of your deck in Reserve, then exhaust it {T}. Exhausted cards can\'t be played and have no Support abilities.)'),
+            'forest' => 2,
+            'mountain' => 3,
+            'ocean' => 2,
+            'costHand' => 3,
+            'costReserve' => 3,
+            'effectPlayed' => FT::ACTION(RESUPPLY, ['exhausted' => true])
+
+        ];
+    }
 }
