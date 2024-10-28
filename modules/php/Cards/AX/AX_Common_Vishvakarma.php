cae977a8b62142f3876f42c9b5e333741cd1f43d
vtoper <vincent.toper@gmail.com>
2024-10-28 21:31:14 +0100

    Vishvakarma

--- modules/php/Cards/AX/AX_Common_Vishvakarma.php
+++ modules/php/Cards/AX/AX_Common_Vishvakarma.php
@@ -1,29 +1,36 @@
 <?php
+
 namespace ALT\Cards\AX;
 
 class AX_Common_Vishvakarma extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_AX_39_C',
             'asset'  => 'ALT_ALIZE_B_AX_39_C',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Vishvakarma"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Vishvakarma"),
             'typeline' => clienttranslate("Character - Deity"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Each construction is a poem, and building is an act of eloquence.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Each construction is a poem, and building is an act of eloquence.'),
             'artist' => "Taras Susak",
-			'extension'=>'TBF',
-   'subtypes'  => [DEITY],
- 				'effectDesc' => clienttranslate('{J} The next Permanent you play this Afternoon costs {4} less.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 5, 
-     'costReserve' => 5, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [DEITY],
+            'effectDesc' => clienttranslate('{J} The next Permanent you play this Afternoon costs {4} less.'),
+            'forest' => 3,
+            'mountain' => 3,
+            'ocean' => 3,
+            'costHand' => 5,
+            'costReserve' => 5,
+            'effectPlayed' => [
+                'action' => SPECIAL_EFFECT,
+                'args' => ['effect' => 'costReduction', 'args' => ['type' => PERMANENT, 'reduction' => 4, 'permanent' => true]],
+            ],
+
+        ];
+    }
 }
