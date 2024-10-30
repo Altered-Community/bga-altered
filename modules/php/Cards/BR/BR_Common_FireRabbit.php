3bee82b7511a26bdd78a684d30cb3ec9b1ae3c94
vtoper <vincent.toper@gmail.com>
2024-10-30 22:06:44 +0100

    Fire Rabbit

--- modules/php/Cards/BR/BR_Common_FireRabbit.php
+++ modules/php/Cards/BR/BR_Common_FireRabbit.php
@@ -1,29 +1,34 @@
 <?php
+
 namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
 
 class BR_Common_FireRabbit extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_BR_32_C',
             'asset'  => 'ALT_ALIZE_B_BR_32_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Fire Rabbit"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Fire Rabbit"),
             'typeline' => clienttranslate("Character - Animal Spirit"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Hot bunny in your area wants to meet!'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Hot bunny in your area wants to meet!'),
             'artist' => "Khoa Viet",
-			'extension'=>'TBF',
-   'subtypes'  => [ANIMAL,SPIRIT],
- 				'effectDesc' => clienttranslate('{H} You may have me gain <FLEETING_CHAR>. If you do, another target Character gains 1 boost.'),
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 0, 
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [ANIMAL, SPIRIT],
+            'effectDesc' => clienttranslate('{H} You may have me gain <FLEETING_CHAR>. If you do, another target Character gains 1 boost.'),
+            'forest' => 1,
+            'mountain' => 1,
+            'ocean' => 0,
+            'costHand' => 1,
+            'costReserve' => 1,
+            'effectHand' => FT::SEQ_OPTIONAL(FT::GAIN(ME, FLEETING), FT::ACTION(TARGET, ['excludeSelf' => true, 'effect' => FT::ACTION(GAIN, ['type' => BOOST])])),
+        ];
+    }
 }
