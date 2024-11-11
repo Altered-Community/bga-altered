266e0e14bf2b0965310e31f606a977f0e094bc92
vtoper <vincent.toper@gmail.com>
2024-11-12 00:01:49 +0100

    Geyser

--- modules/php/Cards/BR/BR_Common_Geyser.php
+++ modules/php/Cards/BR/BR_Common_Geyser.php
@@ -1,26 +1,39 @@
 <?php
+
 namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
 
 class BR_Common_Geyser extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_BR_42_C',
             'asset'  => 'ALT_ALIZE_B_BR_42_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Geyser"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Geyser"),
             'typeline' => clienttranslate("Spell - Disruption"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('Under pressure!'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('Under pressure!'),
             'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'TBF',
-   'subtypes'  => [DISRUPTION],
- 				'effectDesc' => clienttranslate('Return target Character or Permanent to its owner\'s hand, then put me in your Mana Orbs (as an exhausted Mana Orb).'),
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [DISRUPTION],
+            'effectDesc' => clienttranslate('Return target Character or Permanent to its owner\'s hand, then put me in your Mana Orbs (as an exhausted Mana Orb).'),
+            'costHand' => 4,
+            'costReserve' => 4,
+            'effectPlayed' => FT::SEQ(
+                FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN, PERMANENT], 'effect' => FT::RETURN_TO_HAND()]),
+                FT::ACTION(DISCARD, [
+                    'cardId' => ME,
+                    'destination' => MANA,
+                    'tapped' => true,
+                    'force' => true,
+                ]),
+            ),
+        ];
+    }
 }
