09b80ea709633c3c9a52007ddf942ca6d71ecc0f
vtoper <vincent.toper@gmail.com>
2024-11-16 00:19:21 +0100

    Heimdall

--- modules/php/Cards/LY/LY_Rare_Heimdall.php
+++ modules/php/Cards/LY/LY_Rare_Heimdall.php
@@ -1,30 +1,39 @@
 <?php
+
 namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
 
 class LY_Rare_Heimdall extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_LY_38_R1',
             'asset'  => 'ALT_ALIZE_B_LY_38_R',
 
-    		'faction'  => FACTION_LY,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Heimdall"),
+            'faction'  => FACTION_LY,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Heimdall"),
             'typeline' => clienttranslate("Character - Soldier Deity"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Proud guardian of the Bifröst, he decides who gets to switch sides.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Proud guardian of the Bifröst, he decides who gets to switch sides.'),
             'artist' => "Fahmi Fauzi",
-			'extension'=>'TBF',
-   'subtypes'  => [SOLDIER,DEITY],
- 				'effectDesc' => clienttranslate('{H} You may have target Character #or Expedition Permanent# switch Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
-     'forest' => 5, 
-     'mountain' => 0, 
-     'ocean' => 5, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-     'changedStats' => ['forest','mountain','ocean'], 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [SOLDIER, DEITY],
+            'effectDesc' => clienttranslate('{H} You may have target Character #or Expedition Permanent# switch Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
+            'forest' => 5,
+            'mountain' => 0,
+            'ocean' => 5,
+            'costHand' => 4,
+            'costReserve' => 4,
+            'changedStats' => ['forest', 'mountain', 'ocean'],
+            'effectPlayed' => FT::XOR(
+                FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN], 'effect' => FT::ACTION(MOVE_CARD, [])]),
+                FT::ACTION(TARGET, ['targetType' => [PERMANENT], 'subtype' => EXPEDITION, 'effect' => FT::ACTION(MOVE_CARD, [])]),
+            ),
+
+        ];
+    }
 }
--- modules/php/Cards/OD/OD_Rare_Heimdall.php
+++ modules/php/Cards/OD/OD_Rare_Heimdall.php
@@ -1,29 +1,35 @@
 <?php
+
 namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
 
 class OD_Rare_Heimdall extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_LY_38_R2',
-            'asset'  => 'ALT_ALIZE_B_LY_38_R2',
+            'asset'  => 'ALT_ALIZE_B_LY_38_R',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Heimdall"),
+            'faction'  => FACTION_OD,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Heimdall"),
             'typeline' => clienttranslate("Character - Soldier Deity"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Proud guardian of the Bifröst, he decides who gets to switch sides.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Proud guardian of the Bifröst, he decides who gets to switch sides.'),
             'artist' => "Fahmi Fauzi",
-			'extension'=>'TBF',
-   'subtypes'  => [SOLDIER,DEITY],
- 				'effectDesc' => clienttranslate('{H} You may have target Character switch Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
-     'forest' => 4, 
-     'mountain' => 4, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [SOLDIER, DEITY],
+            'effectDesc' => clienttranslate('{H} You may have target Character switch Expeditions. (It leaves its Expedition and joins its controller\'s other Expedition.)'),
+            'forest' => 4,
+            'mountain' => 4,
+            'ocean' => 4,
+            'costHand' => 4,
+            'costReserve' => 4,
+            'effectPlayed' => FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN], 'effect' => FT::ACTION(MOVE_CARD, [])]),
+
+        ];
+    }
 }
