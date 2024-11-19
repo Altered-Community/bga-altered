703924365f21ce1f690e5fb4765a37f3bfbd738e
vtoper <vincent.toper@gmail.com>
2024-11-19 21:38:19 +0100

    Snoozer Shroom

--- modules/php/Cards/OD/OD_Rare_SnoozerShroom.php
+++ modules/php/Cards/OD/OD_Rare_SnoozerShroom.php
@@ -1,31 +1,50 @@
 <?php
+
 namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
 
 class OD_Rare_SnoozerShroom extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_MU_32_R2',
-            'asset'  => 'ALT_ALIZE_B_MU_32_R2',
+            'asset'  => 'ALT_ALIZE_B_MU_32_R',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Snoozer Shroom"),
+            'faction'  => FACTION_OD,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Snoozer Shroom"),
             'typeline' => clienttranslate("Character - Plant"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('"Zzz..."'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('"Zzz..."'),
             'artist' => "Ba Vo",
-			'extension'=>'TBF',
-   'subtypes'  => [PLANT],
- 				'effectDesc' => clienttranslate('#{H} You may pay {2}. If you do, target Character in the Expedition facing me gains <ASLEEP>.#'),
- 				'supportDesc' => clienttranslate('{D} : Target Character you control with Hand Cost {4} or less gains <ASLEEP>. (Discard me from Reserve to do this.)'),
- 			     'supportIcon' => 'discard',
-     'forest' => 3, 
-     'mountain' => 0, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [PLANT],
+            'effectDesc' => clienttranslate('#{H} You may pay {2}. If you do, target Character in the Expedition facing me gains <ASLEEP>.#'),
+            'supportDesc' => clienttranslate('{D} : Target Character you control with Hand Cost {4} or less gains <ASLEEP>. (Discard me from Reserve to do this.)'),
+            'supportIcon' => 'discard',
+            'forest' => 3,
+            'mountain' => 0,
+            'ocean' => 2,
+            'costHand' => 2,
+            'costReserve' => 2,
+            'effectSupport' => FT::ACTION(TARGET, [
+                'targetPlayer' => ME,
+                'targetType' => [CHARACTER, TOKEN],
+                'maxHandCost' => 4,
+                'effect' => FT::GAIN(EFFECT, ASLEEP),
+            ]),
+            'effectHand' => FT::SEQ_OPTIONAL(
+                FT::ACTION(PAY, ['pay' => 2]),
+                FT::ACTION(TARGET, [
+                    'targetPlayer' => OPPONENT,
+                    'targetLocation' => 'source',
+                    'targetType' => [CHARACTER, TOKEN],
+                    'effect' => FT::GAIN(EFFECT, ASLEEP),
+                ])
+            )
+        ];
+    }
 }
