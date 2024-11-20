c3e765e5a25b06df7d71b42112608196c5e8b83f
vtoper <vincent.toper@gmail.com>
2024-11-20 22:27:47 +0100

    sow

--- modules/php/Cards/BR/BR_Rare_Sow.php
+++ modules/php/Cards/BR/BR_Rare_Sow.php
@@ -1,28 +1,35 @@
 <?php
+
 namespace ALT\Cards\BR;
+
+use ALT\Helpers\FT;
 
 class BR_Rare_Sow extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_MU_41_R2',
-            'asset'  => 'ALT_ALIZE_B_MU_41_R2',
+            'asset'  => 'ALT_ALIZE_B_MU_41_R',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Sow"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Sow"),
             'typeline' => clienttranslate("Spell - Boon"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('The wise gardener prepares ahead of spring.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('The wise gardener prepares ahead of spring.'),
             'artist' => "Romain Kurdi",
-			'extension'=>'TBF',
-   'subtypes'  => [BOON],
- 				'effectDesc' => clienttranslate('$<COOLDOWN>.  Target Character gains 1 boost.'),
- 				'supportDesc' => clienttranslate('{D} : #Target Character with Hand Cost {3} or less gains <ANCHORED>.# (Discard me from Reserve to do this.)'),
- 			     'supportIcon' => 'discard',
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [BOON],
+            'effectDesc' => clienttranslate('$<COOLDOWN>.  Target Character gains 1 boost.'),
+            'supportDesc' => clienttranslate('{D} : #Target Character with Hand Cost {3} or less gains <ANCHORED>.# (Discard me from Reserve to do this.)'),
+            'supportIcon' => 'discard',
+            'costHand' => 1,
+            'costReserve' => 1,
+            'cooldown' => true,
+            'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST)]),
+            'effectSupport' => FT::ACTION(TARGET, ['maxHandCost' => 3, 'effect' => FT::GAIN($this, ANCHORED)]),
+        ];
+    }
 }
--- modules/php/Cards/MU/MU_Common_Sow.php
+++ modules/php/Cards/MU/MU_Common_Sow.php
@@ -1,28 +1,35 @@
 <?php
+
 namespace ALT\Cards\MU;
+
+use ALT\Helpers\FT;
 
 class MU_Common_Sow extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_MU_41_C',
             'asset'  => 'ALT_ALIZE_B_MU_41_C',
 
-    		'faction'  => FACTION_MU,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Sow"),
+            'faction'  => FACTION_MU,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Sow"),
             'typeline' => clienttranslate("Spell - Boon"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('The wise gardener prepares ahead of spring.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('The wise gardener prepares ahead of spring.'),
             'artist' => "Romain Kurdi",
-			'extension'=>'TBF',
-   'subtypes'  => [BOON],
- 				'effectDesc' => clienttranslate('<COOLDOWN>. (If I go to Reserve after my effect resolves, exhaust me {T}. Exhausted cards can\'t be played and have no Support abilities.)  Target Character gains 1 boost.'),
- 				'supportDesc' => clienttranslate('{D} : The next Character you play this turn gains 1 boost. (Discard me from Reserve to do this.)'),
- 			     'supportIcon' => 'discard',
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [BOON],
+            'effectDesc' => clienttranslate('<COOLDOWN>. (If I go to Reserve after my effect resolves, exhaust me {T}. Exhausted cards can\'t be played and have no Support abilities.)  Target Character gains 1 boost.'),
+            'supportDesc' => clienttranslate('{D} : The next Character you play this turn gains 1 boost. (Discard me from Reserve to do this.)'),
+            'supportIcon' => 'discard',
+            'costHand' => 1,
+            'costReserve' => 1,
+            'cooldown' => true,
+            'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST)]),
+            'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterGains1Boost']),
+        ];
+    }
 }
