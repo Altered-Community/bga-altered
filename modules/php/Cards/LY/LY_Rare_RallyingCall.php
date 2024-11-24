d7d84c8528ac09f006b056cd032c66bcf0ea199f
vtoper <vincent.toper@gmail.com>
2024-11-24 22:51:58 +0100

    Rallying call

--- modules/php/Cards/LY/LY_Rare_RallyingCall.php
+++ modules/php/Cards/LY/LY_Rare_RallyingCall.php
@@ -1,27 +1,36 @@
 <?php
+
 namespace ALT\Cards\LY;
+
+use ALT\Helpers\FT;
 
 class LY_Rare_RallyingCall extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_OR_44_R2',
-            'asset'  => 'ALT_ALIZE_B_OR_44_R2',
+            'asset'  => 'ALT_ALIZE_B_OR_44_R',
 
-    		'faction'  => FACTION_LY,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Rallying Call"),
+            'faction'  => FACTION_LY,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Rallying Call"),
             'typeline' => clienttranslate("Spell - Song"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('The sound of the horn stirred them from their slumber as the white shadows emerged from the woods.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('The sound of the horn stirred them from their slumber as the white shadows emerged from the woods.'),
             'artist' => "Jean-Baptiste andrier",
-			'extension'=>'TBF',
-   'subtypes'  => [SONG],
- 				'effectDesc' => clienttranslate('Target Character gains 1 boost. If it is <ASLEEP>, #it gains <ANCHORED># and loses <ASLEEP>. (At Rest, it doesn\'t go to Reserve and loses Anchored).'),
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['costHand','costReserve'], 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [SONG],
+            'effectDesc' => clienttranslate('Target Character gains 1 boost. If it is <ASLEEP>, #it gains <ANCHORED># and loses <ASLEEP>. (At Rest, it doesn\'t go to Reserve and loses Anchored).'),
+            'costHand' => 2,
+            'costReserve' => 2,
+            'changedStats' => ['costHand', 'costReserve'],
+            'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::SEQ(
+                FT::ACTION(GAIN, ['type' => BOOST]),
+                FT::GAIN(EFFECT, ANCHORED),
+                FT::LOOSE(EFFECT, ASLEEP)
+            )]),
+        ];
+    }
 }
--- modules/php/Cards/OD/OD_Common_RallyingCall.php
+++ modules/php/Cards/OD/OD_Common_RallyingCall.php
@@ -1,26 +1,34 @@
 <?php
+
 namespace ALT\Cards\OD;
+
+use ALT\Helpers\FT;
 
 class OD_Common_RallyingCall extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_OR_44_C',
             'asset'  => 'ALT_ALIZE_B_OR_44_C',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Rallying Call"),
+            'faction'  => FACTION_OD,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Rallying Call"),
             'typeline' => clienttranslate("Spell - Song"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('The sound of the horn stirred them from their slumber as the white shadows emerged from the woods.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('The sound of the horn stirred them from their slumber as the white shadows emerged from the woods.'),
             'artist' => "Jean-Baptiste andrier",
-			'extension'=>'TBF',
-   'subtypes'  => [SONG],
- 				'effectDesc' => clienttranslate('Target Character gains 1 boost. If it is <ASLEEP>, it loses <ASLEEP>.'),
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [SONG],
+            'effectDesc' => clienttranslate('Target Character gains 1 boost. If it is <ASLEEP>, it loses <ASLEEP>.'),
+            'costHand' => 1,
+            'costReserve' => 1,
+            'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::SEQ(
+                FT::ACTION(GAIN, ['type' => BOOST]),
+                FT::LOOSE(EFFECT, ASLEEP)
+            )]),
+        ];
+    }
 }
