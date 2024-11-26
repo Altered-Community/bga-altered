9dbd14c3114b411571a76be43bf9596343df47e7
vtoper <vincent.toper@gmail.com>
2024-11-26 22:54:45 +0100

    Chrysalis

--- modules/php/Cards/YZ/YZ_Common_Chrysalis.php
+++ modules/php/Cards/YZ/YZ_Common_Chrysalis.php
@@ -1,29 +1,43 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
+use ALT\Helpers\FT;
 
 class YZ_Common_Chrysalis extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_YZ_33_C',
             'asset'  => 'ALT_ALIZE_B_YZ_33_C',
 
-    		'faction'  => FACTION_YZ,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Chrysalis"),
+            'faction'  => FACTION_YZ,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Chrysalis"),
             'typeline' => clienttranslate("Character - Animal"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('...They build their ice cocoon to turn ethereal.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('...They build their ice cocoon to turn ethereal.'),
             'artist' => "Zael",
-			'extension'=>'TBF',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('$<DEFENDER>.  When I leave the Expedition Zone — Create a <MANA_MOTH> Illusion token in my Expedition.'),
-     'forest' => 2, 
-     'mountain' => 1, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
-  }
+            'extension' => 'TBF',
+            'subtypes'  => [ANIMAL],
+            'effectDesc' => clienttranslate('$<DEFENDER>.  When I leave the Expedition Zone — Create a <MANA_MOTH> Illusion token in my Expedition.'),
+            'forest' => 2,
+            'mountain' => 1,
+            'ocean' => 2,
+            'costHand' => 2,
+            'costReserve' => 2,
+            'defender' => true,
+            'effectPassive' => [
+                'LeaveExpedition' => [
+                    'output' => FT::ACTION(INVOKE_TOKEN, [
+                        'pId' => 'source',
+                        'tokenType' => 'YZ_Common_ManaMoth',
+                        'targetLocation' => ['source'],
+                    ]),
+                ],
+            ],
+        ];
+    }
 }
