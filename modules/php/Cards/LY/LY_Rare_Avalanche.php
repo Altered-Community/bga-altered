9723836d7117dba97b6ef4e463f6388e7c72c405
vtoper <vincent.toper@gmail.com>
2024-12-03 11:20:51 +0100

    147889: "avalavance rare, lyra no art

--- modules/php/Cards/LY/LY_Rare_Avalanche.php
+++ modules/php/Cards/LY/LY_Rare_Avalanche.php
@@ -11,7 +11,7 @@ class LY_Rare_Avalanche extends \ALT\Models\Card
         parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_AX_42_R2',
-            'asset'  => 'ALT_ALIZE_B_AX_42_R2',
+            'asset'  => 'ALT_ALIZE_B_AX_42_R',
 
             'faction'  => FACTION_LY,
             'rarity'  => RARITY_RARE,
