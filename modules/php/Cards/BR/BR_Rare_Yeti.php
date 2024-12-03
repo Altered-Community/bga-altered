9ad1443a339175202678041f8fb6b06574bdb57c
vtoper <vincent.toper@gmail.com>
2024-12-03 21:37:10 +0100

    148040: "Yéti Rare Bravos"

--- modules/php/Cards/BR/BR_Rare_Yeti.php
+++ modules/php/Cards/BR/BR_Rare_Yeti.php
@@ -11,7 +11,7 @@ class BR_Rare_Yeti extends \ALT\Models\Card
         parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_ALIZE_B_MU_36_R2',
-            'asset'  => 'ALT_ALIZE_B_MU_36_R2',
+            'asset'  => 'ALT_ALIZE_B_MU_36_R',
 
             'faction'  => FACTION_BR,
             'rarity'  => RARITY_RARE,
