830fac111b92bc985a514e5e983afc063383a99d
vtoper <vincent.toper@gmail.com>
2024-12-10 23:20:15 +0100

    Fix Nevenka

--- modules/php/Actions/RollDie.php
+++ modules/php/Actions/RollDie.php
@@ -127,7 +127,7 @@ class RollDie extends \ALT\Models\Action
     for ($i = 0; $i < $nTotal; $i++) {
       $roll = bga_rand(1, 6);
       if (Game::get()->getBgaEnvironment() == 'studio') {
-        // $roll = 1;
+        $roll = 1;
       }
       $rolls[] = $roll;
     }
