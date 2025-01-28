6daf1c03453e979ededd4eb15d530339864705fc
vtoper <vincent.toper@gmail.com>
2025-01-28 23:38:47 +0100

    154825: "j'ai été forcé de mettre mon mighty djinn ancré en réserve ou de le défausser."

--- modules/php/Models/Player.php
+++ modules/php/Models/Player.php
@@ -416,7 +416,7 @@ class Player extends \ALT\Helpers\DB_Model
 
       // Jinn's effect (ask question to put in mana instead of discarding)
       if (
-        !$card->hasToken(ASLEEP) &&
+        !$card->hasToken(ASLEEP) && !card->hasToken(ANCHORED) && 
         ($card->isLeaveExpeditionToMana() // Mighty Jinn
           || $card->isLeaveExpeditionToManaOrDraw() // Mighty Jinn rare
           || ($card->isLeaveExpeditionBoostedToMana() && $card->countToken(BOOST) > 0) // Tiny Jinn
