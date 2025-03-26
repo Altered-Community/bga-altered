3607511a7c3d22ee7fc80f874ecbd836c443f73d
vtoper <vincent.toper@gmail.com>
2025-03-26 22:34:06 +0100

    ShortageSupervisor

--- modules/php/Cards/AX/AX_Common_ShortageSupervisor.php
+++ modules/php/Cards/AX/AX_Common_ShortageSupervisor.php
@@ -1,30 +1,34 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_ShortageSupervisor extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_55_C',
             'asset'  => 'ALT_BISE_B_AX_55_C',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Shortage Supervisor"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Shortage Supervisor"),
             'typeline' => clienttranslate("Character - Engineer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('With the appropriate tech, Sap can exhibit energetic properties sufficient to replace Kelon.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('With the appropriate tech, Sap can exhibit energetic properties sufficient to replace Kelon.'),
             'artist' => "Anh Tung",
-			'extension'=>'WFTM',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('{R} If your hand is empty, I gain 1 boost.'),
-     'forest' => 2, 
-     'mountain' => 3, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ENGINEER],
+            'effectDesc' => clienttranslate('{R} If your hand is empty, I gain 1 boost.'),
+            'forest' => 2,
+            'mountain' => 3,
+            'ocean' => 2,
+            'costHand' => 3,
+            'costReserve' => 2,
+            'effectReserve' => FT::ACTION(CHECK_CONDITION, ['condition' => 'isHandEmpty', 'effect' => FT::GAIN(ME, BOOST)])
+        ];
+    }
 }
--- modules/php/Cards/BR/BR_Rare_ShortageSupervisor.php
+++ modules/php/Cards/BR/BR_Rare_ShortageSupervisor.php
@@ -1,31 +1,39 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_ShortageSupervisor extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_55_R2',
             'asset'  => 'ALT_BISE_B_AX_55_R',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Shortage Supervisor"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Shortage Supervisor"),
             'typeline' => clienttranslate("Character - Engineer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('With the appropriate tech, Sap can exhibit energetic properties sufficient to replace Kelon.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('With the appropriate tech, Sap can exhibit energetic properties sufficient to replace Kelon.'),
             'artist' => "Anh Tung",
-			'extension'=>'WFTM',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('{R} If your hand is empty, #I gain 2 boosts, otherwise I gain 1 boost.#'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-     'changedStats' => ['mountain'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ENGINEER],
+            'effectDesc' => clienttranslate('{R} If your hand is empty, #I gain 2 boosts, otherwise I gain 1 boost.#'),
+            'forest' => 2,
+            'mountain' => 2,
+            'ocean' => 2,
+            'costHand' => 3,
+            'costReserve' => 2,
+            'changedStats' => ['mountain'],
+            'effectReserve' => FT::ACTION(CHECK_CONDITION, [
+                'condition' => 'isHandEmpty',
+                'oppositeEffect' => FT::GAIN(ME, BOOST),
+                'effect' => FT::GAIN(ME, BOOST, 2)
+            ])
+        ];
+    }
 }
