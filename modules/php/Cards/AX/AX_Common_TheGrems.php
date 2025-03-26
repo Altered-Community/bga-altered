4f4bc12fe6eb3a970becc9e06373ed554fd93df1
vtoper <vincent.toper@gmail.com>
2025-03-26 22:15:15 +0100

    The grems

--- modules/php/Cards/AX/AX_Common_TheGrems.php
+++ modules/php/Cards/AX/AX_Common_TheGrems.php
@@ -1,30 +1,49 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_TheGrems extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_53_C',
             'asset'  => 'ALT_BISE_B_AX_53_C',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("The Grems"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("The Grems"),
             'typeline' => clienttranslate("Character - Engineer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('It\'s not demolition, but basic recycling. Or so they say.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('It\'s not demolition, but basic recycling. Or so they say.'),
             'artist' => "Atanas Lozanski",
-			'extension'=>'WFTM',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('{R} Choose up to one:  • Discard target Permanent with Hand Cost {3} or less.  • Sacrifice a Permanent. If you do, create a <BRASSBUG> Robot token in target Expedition.'),
-     'forest' => 1, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ENGINEER],
+            'effectDesc' => clienttranslate('{R} Choose up to one:  • Discard target Permanent with Hand Cost {3} or less.  • Sacrifice a Permanent. If you do, create a <BRASSBUG> Robot token in target Expedition.'),
+            'forest' => 1,
+            'mountain' => 2,
+            'ocean' => 2,
+            'costHand' => 2,
+            'costReserve' => 2,
+            'effectReserve' => FT::XOR_OPTIONAL(
+                FT::ACTION(TARGET, ['maxHandCost' => 3, 'targetType' => [PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+                FT::ACTION(TARGET, [
+                    'targetPlayer' => ME,
+                    'targetType' => [PERMANENT],
+                    'effect' => FT::SEQ(
+                        FT::ACTION(DISCARD, ['desc' => 'sacrifice']),
+                        FT::ACTION(INVOKE_TOKEN, [
+                            'pId' => 'source',
+                            'tokenType' => 'AX_Common_Brassbug',
+                            'targetLocation' => STORMS,
+                        ]),
+                    ),
+                ])
+
+            )
+        ];
+    }
 }
