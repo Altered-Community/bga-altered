53f3518398b57cfda3ecb0f9ee9624e8c930e2b0
vtoper <vincent.toper@gmail.com>
2025-03-26 22:00:24 +0100

    Drillmole

--- modules/php/Cards/AX/AX_Common_Drillmole.php
+++ modules/php/Cards/AX/AX_Common_Drillmole.php
@@ -1,32 +1,57 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_Drillmole extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_52_C',
             'asset'  => 'ALT_BISE_B_AX_52_C',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Drillmole"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Drillmole"),
             'typeline' => clienttranslate("Character - Robot"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Not my cabbages!'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Not my cabbages!'),
             'artist' => "Anh Tung",
-			'extension'=>'WFTM',
-   'subtypes'  => [ROBOT],
- 				'effectDesc' => clienttranslate('{R} You may spend 1 of my boosts to <SABOTAGE_INF>. (Discard up to one target card from a Reserve.)'),
- 				'supportDesc' => clienttranslate('{I} At Noon — Choose one:  • I gain 1 boost, up to a max of 3.  • {T} : I gain 2 boosts, up to a max of 3.'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ROBOT],
+            'effectDesc' => clienttranslate('{R} You may spend 1 of my boosts to <SABOTAGE_INF>. (Discard up to one target card from a Reserve.)'),
+            'supportDesc' => clienttranslate('{I} At Noon — Choose one:  • I gain 1 boost, up to a max of 3.  • {T} : I gain 2 boosts, up to a max of 3.'),
+            'supportIcon' => 'infinity',
+            'forest' => 2,
+            'mountain' => 2,
+            'ocean' => 0,
+            'costHand' => 2,
+            'costReserve' => 3,
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'Noon' => [
+                        'condition' => 'isMe',
+                        'output' => FT::XOR(
+                            FT::ACTION(GAIN, ['cardId' => ME, 'type' => BOOST, 'upTo' => 3]),
+                            FT::SEQ(
+                                FT::ACTION(TAP, []),
+                                FT::ACTION(GAIN, ['cardId' => ME, 'type' => BOOST, 'n' => 2, 'upTo' => 3]),
+                            )
+                        ),
+                    ],
+                ],
+            ],
+            'effectReserve' => FT::SEQ_OPTIONAL(
+                FT::ACTION(SPEND, ['cardId' => ME, 'effect' => FT::ACTION(TARGET, [
+                    'targetType' => [CHARACTER, SPELL, PERMANENT],
+                    'targetLocation' => [RESERVE],
+                    'upTo' => true,
+                    'effect' => FT::ACTION(DISCARD, []),
+                ]),])
+            ),
+        ];
+    }
 }
--- modules/php/Cards/OD/OD_Rare_Drillmole.php
+++ modules/php/Cards/OD/OD_Rare_Drillmole.php
@@ -1,33 +1,67 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_Drillmole extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_52_R2',
             'asset'  => 'ALT_BISE_B_AX_52_R',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Drillmole"),
+            'faction'  => FACTION_OD,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Drillmole"),
             'typeline' => clienttranslate("Character - Robot"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Not my cabbages!'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Not my cabbages!'),
             'artist' => "Anh Tung",
-			'extension'=>'WFTM',
-   'subtypes'  => [ROBOT],
- 				'effectDesc' => clienttranslate('{R} You may spend #1 counter# from #a card you control or in your Reserve# to <SABOTAGE_INF>.'),
- 				'supportDesc' => clienttranslate('{I} #When another Character joins your Expeditions# — I gain 1 boost, up to a #max of 2.#'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 1, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-     'changedStats' => ['ocean'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ROBOT],
+            'effectDesc' => clienttranslate('{R} You may spend #1 counter# from #a card you control or in your Reserve# to <SABOTAGE_INF>.'),
+            'supportDesc' => clienttranslate('{I} #When another Character joins your Expeditions# — I gain 1 boost, up to a #max of 2.#'),
+            'supportIcon' => 'infinity',
+            'forest' => 2,
+            'mountain' => 2,
+            'ocean' => 1,
+            'costHand' => 2,
+            'costReserve' => 3,
+            'changedStats' => ['ocean'],
+            'effectReserve' => FT::ACTION(
+                TARGET,
+                [
+                    'targetLocation' => CONTROLLED_RESERVE,
+                    'targetPlayer' => ME,
+                    'augmentOnly' => true,
+                    'targetType' => TYPES,
+                    'upTo' => true,
+                    'effect' => FT::ACTION(SPEND, [
+                        'cardId' => EFFECT,
+                        'effect' => FT::ACTION(TARGET, [
+                            'targetType' => [CHARACTER, SPELL, PERMANENT],
+                            'targetLocation' => [RESERVE],
+                            'upTo' => true,
+                            'effect' => FT::ACTION(DISCARD, []),
+                        ])
+                    ])
+                ]
+            ),
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'ChooseAssignment' => [
+                        'conditions' => ['isCardAdded:character:::true'],
+                        'output' => FT::GAIN(ME, BOOST, 1, 2),
+                    ],
+                    'InvokeToken' => [
+                        'conditions' => ['isCardAdded:character:::true'],
+                        'output' => FT::GAIN(ME, BOOST, 1, 2),
+                    ],
+                ],
+            ]
+        ];
+    }
 }
