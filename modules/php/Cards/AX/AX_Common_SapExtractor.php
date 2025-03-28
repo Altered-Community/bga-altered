27e7b4a234fe78e3d50951e12afdf41bb1a398cd
vtoper <vincent.toper@gmail.com>
2025-03-28 22:36:22 +0100

    Sap Extractor

--- modules/php/Cards/AX/AX_Common_SapExtractor.php
+++ modules/php/Cards/AX/AX_Common_SapExtractor.php
@@ -1,27 +1,42 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_SapExtractor extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_60_C',
             'asset'  => 'ALT_BISE_B_AX_60_C',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Sap Extractor"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Sap Extractor"),
             'typeline' => clienttranslate("Landmark_permanent - Construction"),
-    		'type'  => PERMANENT,
-    		'flavorText'  => clienttranslate('The Sap courses through the whole city like blood in a mortal\'s veins.'),
+            'type'  => PERMANENT,
+            'flavorText'  => clienttranslate('The Sap courses through the whole city like blood in a mortal\'s veins.'),
             'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'WFTM',
-   'subtypes'  => [CONSTRUCTION,LANDMARK],
- 				'effectDesc' => clienttranslate('At Noon — You may <AUGMENT> target card in play or in Reserve. (If it has counters, it gains 1 more. This can\'t target Hero cards.)'),
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [CONSTRUCTION, LANDMARK],
+            'effectDesc' => clienttranslate('At Noon — You may <AUGMENT> target card in play or in Reserve. (If it has counters, it gains 1 more. This can\'t target Hero cards.)'),
+            'costHand' => 1,
+            'costReserve' => 1,
+            'effectPassive' => [
+                'Noon' => [
+                    'condition' => 'isMe',
+                    'output' => FT::ACTION(TARGET, [
+                        'targetType' => [CHARACTER, SPELL, PERMANENT],
+                        'targetLocation' => [STORM_LEFT, STORM_RIGHT, LANDMARK, RESERVE],
+                        'upTo' => true,
+                        'augmentOnly' => true,
+                        'effect' => FT::AUGMENT($this)
+                    ]),
+                ]
+            ]
+        ];
+    }
 }
--- modules/php/Cards/AX/AX_Rare_SapExtractor.php
+++ modules/php/Cards/AX/AX_Rare_SapExtractor.php
@@ -1,28 +1,44 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_SapExtractor extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_AX_60_R1',
             'asset'  => 'ALT_BISE_B_AX_60_R',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Sap Extractor"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Sap Extractor"),
             'typeline' => clienttranslate("Landmark_permanent - Construction"),
-    		'type'  => PERMANENT,
-    		'flavorText'  => clienttranslate('The Sap courses through the whole city like blood in a mortal\'s veins.'),
+            'type'  => PERMANENT,
+            'flavorText'  => clienttranslate('The Sap courses through the whole city like blood in a mortal\'s veins.'),
             'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'WFTM',
-   'subtypes'  => [CONSTRUCTION,LANDMARK],
- 				'effectDesc' => clienttranslate('At Noon — <AUGMENT_IMP> #any number# of target cards in play or in Reserve. (If they have counters, they gain 1 more. This can\'t target Hero cards.)'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand','costReserve'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [CONSTRUCTION, LANDMARK],
+            'effectDesc' => clienttranslate('At Noon — <AUGMENT_IMP> #any number# of target cards in play or in Reserve. (If they have counters, they gain 1 more. This can\'t target Hero cards.)'),
+            'costHand' => 3,
+            'costReserve' => 3,
+            'changedStats' => ['costHand', 'costReserve'],
+            'effectPassive' => [
+                'Noon' => [
+                    'condition' => 'isMe',
+                    'output' => FT::ACTION(TARGET, [
+                        'targetType' => [CHARACTER, SPELL, PERMANENT],
+                        'targetLocation' => [STORM_LEFT, STORM_RIGHT, LANDMARK, RESERVE],
+                        'upTo' => true,
+                        'n' => INFTY,
+                        'augmentOnly' => true,
+                        'effect' => FT::AUGMENT($this)
+                    ]),
+                ]
+            ]
+        ];
+    }
 }
