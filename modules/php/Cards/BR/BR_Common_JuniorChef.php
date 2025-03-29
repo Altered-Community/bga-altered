d0b7c8f603578d8c7503172a11cb789853406fb4
vtoper <vincent.toper@gmail.com>
2025-03-29 22:09:08 +0100

    Junior chef

--- modules/php/Cards/BR/BR_Common_JuniorChef.php
+++ modules/php/Cards/BR/BR_Common_JuniorChef.php
@@ -1,31 +1,42 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Common_JuniorChef extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_BR_49_C',
             'asset'  => 'ALT_BISE_B_BR_49_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Junior Chef"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Junior Chef"),
             'typeline' => clienttranslate("Character - Animal Apprentice"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('The Bravos are always the best with a blade, whether it\'s in a duel or in the kitchen.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('The Bravos are always the best with a blade, whether it\'s in a duel or in the kitchen.'),
             'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'WFTM',
-   'subtypes'  => [ANIMAL,APPRENTICE],
- 				'supportDesc' => clienttranslate('{I} When you play another Character — I gain 1 boost, up to a max of 3.'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 1, 
-     'costHand' => 1, 
-     'costReserve' => 3, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ANIMAL, APPRENTICE],
+            'supportDesc' => clienttranslate('{I} When you play another Character — I gain 1 boost, up to a max of 3.'),
+            'supportIcon' => 'infinity',
+            'forest' => 1,
+            'mountain' => 1,
+            'ocean' => 1,
+            'costHand' => 1,
+            'costReserve' => 3,
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'ChooseAssignment' => [
+                        'condition' => 'isCardPlayed:character',
+                        'output' => FT::GAIN(ME, BOOST, 1, 3),
+                    ],
+                ]
+            ]
+        ];
+    }
 }
--- modules/php/Cards/BR/BR_Rare_JuniorChef.php
+++ modules/php/Cards/BR/BR_Rare_JuniorChef.php
@@ -1,32 +1,49 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_JuniorChef extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_BR_49_R1',
             'asset'  => 'ALT_BISE_B_BR_49_R',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Junior Chef"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Junior Chef"),
             'typeline' => clienttranslate("Character - Animal Apprentice"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('The Bravos are always the best with a blade, whether it\'s in a duel or in the kitchen.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('The Bravos are always the best with a blade, whether it\'s in a duel or in the kitchen.'),
             'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'WFTM',
-   'subtypes'  => [ANIMAL,APPRENTICE],
- 				'effectDesc' => clienttranslate('#{R} If I have 2 or more boosts, <RESUPPLY_LOW>.#'),
- 				'supportDesc' => clienttranslate('{I} When you play another Character — I gain 1 boost, up to a max of 3.'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 1, 
-     'costHand' => 1, 
-     'costReserve' => 3, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ANIMAL, APPRENTICE],
+            'effectDesc' => clienttranslate('#{R} If I have 2 or more boosts, <RESUPPLY_LOW>.#'),
+            'supportDesc' => clienttranslate('{I} When you play another Character — I gain 1 boost, up to a max of 3.'),
+            'supportIcon' => 'infinity',
+            'forest' => 1,
+            'mountain' => 1,
+            'ocean' => 1,
+            'costHand' => 1,
+            'costReserve' => 3,
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'ChooseAssignment' => [
+                        'condition' => 'isCardPlayed:character',
+                        'output' => FT::GAIN(ME, BOOST, 1, 3),
+                    ],
+                ]
+            ],
+            'effectReserve' => FT::ACTION(CHECK_CONDITION, [
+                'condition' => 'hasBoost:2',
+                'description' => clienttranslate('Resupply if the card has 2+ boosts'),
+                'effect' => FT::ACTION(RESUPPLY, [])
+            ]),
+            'blockAutomaticAction' => [GAIN => [BOOST => 1], CHECK_CONDITION => ['hasBoost:2']],
+        ];
+    }
 }
