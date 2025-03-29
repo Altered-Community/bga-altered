2220295624368b2ae050d70cbb4a8eb031d24e31
vtoper <vincent.toper@gmail.com>
2025-03-29 22:35:52 +0100

    Talarian Skater

--- modules/php/Cards/BR/BR_Common_TalarianSkater.php
+++ modules/php/Cards/BR/BR_Common_TalarianSkater.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Common_TalarianSkater extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_BR_52_C',
-            'asset'  => 'ALT_BISE_B_BR_52_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_BR_52_C',
+      'asset'  => 'ALT_BISE_B_BR_52_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Talarian Skater"),
-            'typeline' => clienttranslate("Character - Messenger"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('To explore the City, adventurers need daily deliveries from the surface.'),
-            'artist' => "Exia",
-			'extension'=>'WFTM',
-   'subtypes'  => [MESSENGER],
- 				'effectDesc' => clienttranslate('$<SCOUT_1> {1}.  {J} Target Character gains 1 boost.'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Talarian Skater"),
+      'typeline' => clienttranslate("Character - Messenger"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('To explore the City, adventurers need daily deliveries from the surface.'),
+      'artist' => "Exia",
+      'extension' => 'WFTM',
+      'subtypes'  => [MESSENGER],
+      'effectDesc' => clienttranslate('$<SCOUT_1> {1}.  {J} Target Character gains 1 boost.'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'scout' => 1,
+      'effectPlayed' => FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST])]),
+    ];
   }
 }
