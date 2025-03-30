26c4bc47be5e58146f32744216766cf441fd98f9
vtoper <vincent.toper@gmail.com>
2025-03-30 23:25:47 +0200

    Theseus

--- modules/php/Cards/BR/BR_Common_Theseus.php
+++ modules/php/Cards/BR/BR_Common_Theseus.php
@@ -1,31 +1,36 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Common_Theseus extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_BR_58_C',
-            'asset'  => 'ALT_BISE_B_BR_58_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_BR_58_C',
+      'asset'  => 'ALT_BISE_B_BR_58_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Theseus"),
-            'typeline' => clienttranslate("Character - Adventurer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('For the brave souls exploring the labyrinthine depths of the city, life often hangs by the thinnest of threads.'),
-            'artist' => "Ed Chee, S.Yong & Stephen",
-			'extension'=>'WFTM',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('$<SCOUT_3> {3}.  {J} Return target Character to its owner\'s hand.  {R} I gain 2 boosts.'),
-     'forest' => 6, 
-     'mountain' => 6, 
-     'ocean' => 6, 
-     'costHand' => 8, 
-     'costReserve' => 8, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Theseus"),
+      'typeline' => clienttranslate("Character - Adventurer"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('For the brave souls exploring the labyrinthine depths of the city, life often hangs by the thinnest of threads.'),
+      'artist' => "Ed Chee, S.Yong & Stephen",
+      'extension' => 'WFTM',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('$<SCOUT_3> {3}.  {J} Return target Character to its owner\'s hand.  {R} I gain 2 boosts.'),
+      'forest' => 6,
+      'mountain' => 6,
+      'ocean' => 6,
+      'costHand' => 8,
+      'costReserve' => 8,
+      'scout' => 3,
+      'effectPlayed' =>  FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN], 'effect' => FT::RETURN_TO_HAND()]),
+      'effectReserve' => FT::GAIN(ME, BOOST, 2)
+    ];
   }
 }
--- modules/php/Cards/MU/MU_Rare_Theseus.php
+++ modules/php/Cards/MU/MU_Rare_Theseus.php
@@ -1,31 +1,36 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_Theseus extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_BR_58_R2',
-            'asset'  => 'ALT_BISE_B_BR_58_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_BR_58_R2',
+      'asset'  => 'ALT_BISE_B_BR_58_R',
 
-    		'faction'  => FACTION_MU,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Theseus"),
-            'typeline' => clienttranslate("Character - Adventurer"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('For the brave souls exploring the labyrinthine depths of the city, life often hangs by the thinnest of threads.'),
-            'artist' => "Ed Chee, S.Yong & Stephen",
-			'extension'=>'WFTM',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('$<SCOUT_3> {3}.  {J} Return target Character to its owner\'s hand.  {R} I gain 2 boosts.'),
-     'forest' => 6, 
-     'mountain' => 6, 
-     'ocean' => 6, 
-     'costHand' => 8, 
-     'costReserve' => 8, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Theseus"),
+      'typeline' => clienttranslate("Character - Adventurer"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('For the brave souls exploring the labyrinthine depths of the city, life often hangs by the thinnest of threads.'),
+      'artist' => "Ed Chee, S.Yong & Stephen",
+      'extension' => 'WFTM',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('$<SCOUT_3> {3}.  {J} Return target Character to its owner\'s hand.  {R} I gain 2 boosts.'),
+      'forest' => 6,
+      'mountain' => 6,
+      'ocean' => 6,
+      'costHand' => 8,
+      'costReserve' => 8,
+      'scout' => 3,
+      'effectPlayed' =>  FT::ACTION(TARGET, ['targetType' => [CHARACTER, TOKEN], 'effect' => FT::RETURN_TO_HAND()]),
+      'effectReserve' => FT::GAIN(ME, BOOST, 2)
+    ];
   }
 }
