4a9a8cbb3d83226f30f0d521943614658bc84e4f
vtoper <vincent.toper@gmail.com>
2025-03-30 22:35:00 +0200

    Hestia

--- modules/php/Cards/BR/BR_Common_Hestia.php
+++ modules/php/Cards/BR/BR_Common_Hestia.php
@@ -1,31 +1,46 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Common_Hestia extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_BR_55_C',
             'asset'  => 'ALT_BISE_B_BR_55_C',
 
-    		'faction'  => FACTION_BR,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Hestia"),
+            'faction'  => FACTION_BR,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Hestia"),
             'typeline' => clienttranslate("Character - Deity"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('One offering for one serving of stew. House rules.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('One offering for one serving of stew. House rules.'),
             'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'WFTM',
-   'subtypes'  => [DEITY],
- 				'supportDesc' => clienttranslate('{I} When you play another Character — You may pay {1} to give it 1 boost.'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 4, 
-     'mountain' => 2, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [DEITY],
+            'supportDesc' => clienttranslate('{I} When you play another Character — You may pay {1} to give it 1 boost.'),
+            'supportIcon' => 'infinity',
+            'forest' => 4,
+            'mountain' => 2,
+            'ocean' => 4,
+            'costHand' => 4,
+            'costReserve' => 4,
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'ChooseAssignment' => [
+                        'condition' => 'isCardPlayed:character:::true',
+                        'output' => FT::SEQ_OPTIONAL_MANUAL(
+                            FT::ACTION(PAY, ['pay' => 1]),
+                            FT::GAIN(EFFECT, BOOST)
+                        )
+                    ],
+                ]
+            ]
+
+        ];
+    }
 }
