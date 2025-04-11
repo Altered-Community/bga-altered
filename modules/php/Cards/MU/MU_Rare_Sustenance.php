a7ce9622020689b389fee5ded368e300ed956fd9
vtoper <vincent.toper@gmail.com>
2025-04-11 22:56:15 +0200

    Sustenance

--- modules/php/Cards/MU/MU_Rare_Sustenance.php
+++ modules/php/Cards/MU/MU_Rare_Sustenance.php
@@ -1,28 +1,36 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_Sustenance extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_MU_61_R1',
             'asset'  => 'ALT_BISE_B_MU_61_R',
 
-    		'faction'  => FACTION_MU,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Sustenance"),
+            'faction'  => FACTION_MU,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Sustenance"),
             'typeline' => clienttranslate("Spell - Conjuration"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('Anything goes when there are empty bellies to be filled.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('Anything goes when there are empty bellies to be filled.'),
             'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'WFTM',
-   'subtypes'  => [CONJURATION],
- 				'effectDesc' => clienttranslate('<FLEETING>.  #Each player# draws a card.  Put the top card of #your# deck in #your# Mana zone (as an exhausted Mana Orb).'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand','costReserve'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [CONJURATION],
+            'effectDesc' => clienttranslate('<FLEETING>.  #Each player# draws a card.  Put the top card of #your# deck in #your# Mana zone (as an exhausted Mana Orb).'),
+            'costHand' => 3,
+            'costReserve' => 3,
+            'changedStats' => ['costHand', 'costReserve'],
+            'effectPlayed' => FT::SEQ(
+                FT::GAIN(ME, FLEETING),
+                FT::ACTION(DRAW, []),
+                FT::ACTION(DRAW, ['players' => ME, 'location' => MANA, 'tapped' => true]),
+            )
+        ];
+    }
 }
