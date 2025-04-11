289891cd624b14ea88b0de4c514bd6d0d7e898c8
vtoper <vincent.toper@gmail.com>
2025-04-11 22:49:57 +0200

    Requiem

--- modules/php/Cards/MU/MU_Common_Requiem.php
+++ modules/php/Cards/MU/MU_Common_Requiem.php
@@ -1,27 +1,43 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Common_Requiem extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_MU_60_C',
             'asset'  => 'ALT_BISE_B_MU_60_C',
 
-    		'faction'  => FACTION_MU,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Requiem"),
+            'faction'  => FACTION_MU,
+            'rarity'  => RARITY_COMMON,
+            'name'  => clienttranslate("Requiem"),
             'typeline' => clienttranslate("Spell - Disruption Song"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('Preserving harmony. Maintaining balance. Sometimes one thing must die so that another may live.'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('Preserving harmony. Maintaining balance. Sometimes one thing must die so that another may live.'),
             'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'WFTM',
-   'subtypes'  => [DISRUPTION,SONG],
- 				'effectDesc' => clienttranslate('$<FLEETING>.  Choose one:  • Discard target <FLEETING> Character with Hand Cost {4} or less.  • Discard target Permanent with Hand Cost {4} or less.'),
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [DISRUPTION, SONG],
+            'effectDesc' => clienttranslate('$<FLEETING>.  Choose one:  • Discard target <FLEETING> Character with Hand Cost {4} or less.  • Discard target Permanent with Hand Cost {4} or less.'),
+            'costHand' => 2,
+            'costReserve' => 2,
+            'effectPlayed' => FT::XOR(
+                FT::ACTION(TARGET, [
+                    'targetType' => [CHARACTER, TOKEN],
+                    'maxHandCost' => 4,
+                    'statuses' => [FLEETING],
+                    'effect' => FT::ACTION(DISCARD, []),
+                ]),
+                FT::ACTION(TARGET, [
+                    'targetType' => [PERMANENT],
+                    'maxHandCost' => 4,
+                    'effect' => FT::ACTION(DISCARD, []),
+                ])
+            )
+        ];
+    }
 }
