49918e9ca729b05b060c1b88339b750dc5ea15e8
vtoper <vincent.toper@gmail.com>
2025-04-14 23:00:14 +0200

    Staff clerk

--- modules/php/Cards/AX/AX_Rare_StaffClerk.php
+++ modules/php/Cards/AX/AX_Rare_StaffClerk.php
@@ -1,31 +1,44 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_StaffClerk extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_OR_51_R2',
             'asset'  => 'ALT_BISE_B_OR_51_R',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Staff Clerk"),
+            'faction'  => FACTION_AX,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Staff Clerk"),
             'typeline' => clienttranslate("Character - Bureaucrat"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('Don\'t venture into the depths alone.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Don\'t venture into the depths alone.'),
             'artist' => "Romain Kurdi",
-			'extension'=>'WFTM',
-   'subtypes'  => [BUREAUCRAT],
- 				'effectDesc' => clienttranslate('When #you# play another card from Reserve — Create an <ORDIS_RECRUIT> Soldier token in my Expedition.'),
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['ocean','costHand','costReserve'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [BUREAUCRAT],
+            'effectDesc' => clienttranslate('When #you# play another card from Reserve — Create an <ORDIS_RECRUIT> Soldier token in my Expedition.'),
+            'forest' => 1,
+            'mountain' => 1,
+            'ocean' => 2,
+            'costHand' => 3,
+            'costReserve' => 3,
+            'changedStats' => ['ocean', 'costHand', 'costReserve'],
+            'effectPassive' => [
+                'ChooseAssignment' => [
+                    'conditions' => ['isMe', 'isFromReserve', 'excludeSelf'],
+                    'output' => FT::ACTION(INVOKE_TOKEN, [
+                        'pId' => 'source',
+                        'tokenType' => 'OD_Common_OrdisRecruit',
+                        'targetLocation' => ['source'],
+                    ]),
+                ],
+            ]
+        ];
+    }
 }
