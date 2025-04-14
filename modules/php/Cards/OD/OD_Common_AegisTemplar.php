1c0c842c4e5bffc0aa99217a0ee04b05bde4bf33
vtoper <vincent.toper@gmail.com>
2025-04-14 23:26:06 +0200

    Aegis Templar

--- modules/php/Cards/OD/OD_Common_AegisTemplar.php
+++ modules/php/Cards/OD/OD_Common_AegisTemplar.php
@@ -1,31 +1,44 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Common_AegisTemplar extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_OR_54_C',
-            'asset'  => 'ALT_BISE_B_OR_54_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_OR_54_C',
+      'asset'  => 'ALT_BISE_B_OR_54_C',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Aegis Templar"),
-            'typeline' => clienttranslate("Character - Soldier"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('The Templars always ensure that the area is safe.'),
-            'artist' => "Taras Susak",
-			'extension'=>'WFTM',
-   'subtypes'  => [SOLDIER],
- 				'effectDesc' => clienttranslate('$<SCOUT_1> {1}.  When I leave the Expedition zone — Create an <ORDIS_RECRUIT> Soldier token in my Expedition.'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Aegis Templar"),
+      'typeline' => clienttranslate("Character - Soldier"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The Templars always ensure that the area is safe.'),
+      'artist' => "Taras Susak",
+      'extension' => 'WFTM',
+      'subtypes'  => [SOLDIER],
+      'effectDesc' => clienttranslate('$<SCOUT_1> {1}.  When I leave the Expedition zone — Create an <ORDIS_RECRUIT> Soldier token in my Expedition.'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'scout' => 1,
+      'effectPassive' => [
+        'LeaveExpedition' => [
+          'output' =>
+          FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'OD_Common_OrdisRecruit',
+            'targetLocation' => ['source'],
+          ]),
+        ],
+      ],
+    ];
   }
 }
--- modules/php/Cards/OD/OD_Rare_AegisTemplar.php
+++ modules/php/Cards/OD/OD_Rare_AegisTemplar.php
@@ -1,32 +1,51 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_AegisTemplar extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_OR_54_R1',
-            'asset'  => 'ALT_BISE_B_OR_54_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_OR_54_R1',
+      'asset'  => 'ALT_BISE_B_OR_54_R',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Aegis Templar"),
-            'typeline' => clienttranslate("Character - Soldier"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('The Templars always ensure that the area is safe.'),
-            'artist' => "Taras Susak",
-			'extension'=>'WFTM',
-   'subtypes'  => [SOLDIER],
- 				'effectDesc' => clienttranslate('#$<SCOUT_2> {2}.#  When I leave the Expedition zone — Create #two# <ORDIS_RECRUIT> Soldier tokens in my Expedition.'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 1, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['ocean'], 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Aegis Templar"),
+      'typeline' => clienttranslate("Character - Soldier"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The Templars always ensure that the area is safe.'),
+      'artist' => "Taras Susak",
+      'extension' => 'WFTM',
+      'subtypes'  => [SOLDIER],
+      'effectDesc' => clienttranslate('#$<SCOUT_2> {2}.#  When I leave the Expedition zone — Create #two# <ORDIS_RECRUIT> Soldier tokens in my Expedition.'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 1,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['ocean'],
+      'scout' => 2,
+      'effectPassive' => [
+        'LeaveExpedition' => [
+          'output' => FT::SEQ(
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'OD_Common_OrdisRecruit',
+              'targetLocation' => ['source'],
+            ]),
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'OD_Common_OrdisRecruit',
+              'targetLocation' => ['source'],
+            ]),
+          )
+        ],
+      ],
+    ];
   }
 }
