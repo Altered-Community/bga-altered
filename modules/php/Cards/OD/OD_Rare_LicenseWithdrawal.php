3e82eb8e8de699343f3a8a34af70a5634ddb72c2
vtoper <vincent.toper@gmail.com>
2025-04-17 23:12:41 +0200

    License Withdrawal

--- modules/php/Cards/OD/OD_Rare_LicenseWithdrawal.php
+++ modules/php/Cards/OD/OD_Rare_LicenseWithdrawal.php
@@ -1,28 +1,47 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_LicenseWithdrawal extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_OR_59_R1',
             'asset'  => 'ALT_BISE_B_OR_59_R',
 
-    		'faction'  => FACTION_OD,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("License Withdrawal"),
+            'faction'  => FACTION_OD,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("License Withdrawal"),
             'typeline' => clienttranslate("Spell - Disruption Maneuver"),
-    		'type'  => SPELL,
-    		'flavorText'  => clienttranslate('"Enough of this mess!"'),
+            'type'  => SPELL,
+            'flavorText'  => clienttranslate('"Enough of this mess!"'),
             'artist' => "Justice Wong",
-			'extension'=>'WFTM',
-   'subtypes'  => [DISRUPTION,MANEUVER],
- 				'effectDesc' => clienttranslate('<FLEETING>.  Choose one:  • Discard target <BOOSTED> Character #with Hand Cost {3} or less.#  • Discard target Permanent #with Hand Cost {3} or less.#'),
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['costHand','costReserve'], 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [DISRUPTION, MANEUVER],
+            'effectDesc' => clienttranslate('<FLEETING>.  Choose one:  • Discard target <BOOSTED> Character #with Hand Cost {3} or less.#  • Discard target Permanent #with Hand Cost {3} or less.#'),
+            'costHand' => 2,
+            'costReserve' => 2,
+            'changedStats' => ['costHand', 'costReserve'],
+            'effectPlayed' => FT::SEQ(
+                FT::GAIN(ME, FLEETING),
+                FT::XOR(
+                    FT::ACTION(TARGET, [
+                        'targetType' => [CHARACTER, TOKEN],
+                        'statuses' => BOOST,
+                        'maxHandCost' => 3,
+                        'effect' => FT::ACTION(DISCARD, [])
+                    ]),
+                    FT::ACTION(TARGET, [
+                        'targetType' => [PERMANENT],
+                        'maxHandCost' => 3,
+                        'effect' => FT::ACTION(DISCARD, [])
+                    ]),
+                )
+            )
+        ];
+    }
 }
