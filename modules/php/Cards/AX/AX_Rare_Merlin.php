5a3f7af14354dfb69b3897eec2e40e40f61dd5d4
vtoper <vincent.toper@gmail.com>
2025-04-24 22:59:50 +0200

    Merlin

--- modules/php/Cards/AX/AX_Rare_Merlin.php
+++ modules/php/Cards/AX/AX_Rare_Merlin.php
@@ -1,31 +1,38 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_Merlin extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_YZ_54_R2',
-            'asset'  => 'ALT_BISE_B_YZ_54_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_YZ_54_R2',
+      'asset'  => 'ALT_BISE_B_YZ_54_R',
 
-    		'faction'  => FACTION_AX,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Merlin"),
-            'typeline' => clienttranslate("Character - Mage"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('"You and I both know it has never been about the sword."'),
-            'artist' => "Taras Susak",
-			'extension'=>'WFTM',
-   'subtypes'  => [MAGE],
- 				'effectDesc' => clienttranslate('$<SCOUT_4> {4}.  {H} You may #discard# target Character #or Permanent.#'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 6, 
-     'costReserve' => 3, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Merlin"),
+      'typeline' => clienttranslate("Character - Mage"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"You and I both know it has never been about the sword."'),
+      'artist' => "Taras Susak",
+      'extension' => 'WFTM',
+      'subtypes'  => [MAGE],
+      'effectDesc' => clienttranslate('$<SCOUT_4> {4}.  {H} You may #discard# target Character #or Permanent.#'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 6,
+      'costReserve' => 3,
+      'scout' => 4,
+      FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, TOKEN, PERMANENT],
+        'effect' => FT::ACTION(DISCARD, []),
+      ])
+    ];
   }
 }
--- modules/php/Cards/YZ/YZ_Common_Merlin.php
+++ modules/php/Cards/YZ/YZ_Common_Merlin.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_Merlin extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_BISE_B_YZ_54_C',
-            'asset'  => 'ALT_BISE_B_YZ_54_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_BISE_B_YZ_54_C',
+      'asset'  => 'ALT_BISE_B_YZ_54_C',
 
-    		'faction'  => FACTION_YZ,
-    		'rarity'  => RARITY_COMMON,
-    		'name'  => clienttranslate("Merlin"),
-            'typeline' => clienttranslate("Character - Mage"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('"You and I both know it has never been about the sword."'),
-            'artist' => "Taras Susak",
-			'extension'=>'WFTM',
-   'subtypes'  => [MAGE],
- 				'effectDesc' => clienttranslate('$<SCOUT_4> {4}.  {H} You may send target Character to Reserve.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 6, 
-     'costReserve' => 3, 
- 		'scout' => 99,
-];
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Merlin"),
+      'typeline' => clienttranslate("Character - Mage"),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"You and I both know it has never been about the sword."'),
+      'artist' => "Taras Susak",
+      'extension' => 'WFTM',
+      'subtypes'  => [MAGE],
+      'effectDesc' => clienttranslate('$<SCOUT_4> {4}.  {H} You may send target Character to Reserve.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 6,
+      'costReserve' => 3,
+      'scout' => 4,
+      'effectHand' => FT::ACTION(TARGET, ['effect' => FT::DISCARD_TO_RESERVE()], ['optional' => true]),
+    ];
   }
 }
