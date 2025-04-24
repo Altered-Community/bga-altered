03874f2f173e2328113e6d6c5e331b4d4373d1b6
vtoper <vincent.toper@gmail.com>
2025-04-24 21:27:49 +0200

    Yzmir engraver

--- modules/php/Cards/YZ/YZ_Rare_YzmirEngraver.php
+++ modules/php/Cards/YZ/YZ_Rare_YzmirEngraver.php
@@ -1,32 +1,53 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_YzmirEngraver extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
+    public function __construct($row)
+    {
+        parent::__construct($row);
         $this->properties = [
             'uid' => 'ALT_BISE_B_YZ_50_R1',
             'asset'  => 'ALT_BISE_B_YZ_50_R',
 
-    		'faction'  => FACTION_YZ,
-    		'rarity'  => RARITY_RARE,
-    		'name'  => clienttranslate("Yzmir Engraver"),
+            'faction'  => FACTION_YZ,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Yzmir Engraver"),
             'typeline' => clienttranslate("Character - Artist Mage"),
-    		'type'  => CHARACTER,
-    		'flavorText'  => clienttranslate('By mixing Sap into the ink, the power of the Sigils is augmented tenfold.'),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('By mixing Sap into the ink, the power of the Sigils is augmented tenfold.'),
             'artist' => "Zero Wen",
-			'extension'=>'WFTM',
-   'subtypes'  => [ARTIST,MAGE],
- 				'effectDesc' => clienttranslate('{R} You may spend 1 of my boosts to #discard target Character or Permanent# with Hand Cost {2} or less.'),
- 				'supportDesc' => clienttranslate('{I} When you play a Spell — I gain 1 boost, up to a max of 2.'),
- 			     'supportIcon' => 'infinity',
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-];
-  }
+            'extension' => 'WFTM',
+            'subtypes'  => [ARTIST, MAGE],
+            'effectDesc' => clienttranslate('{R} You may spend 1 of my boosts to #discard target Character or Permanent# with Hand Cost {2} or less.'),
+            'supportDesc' => clienttranslate('{I} When you play a Spell — I gain 1 boost, up to a max of 2.'),
+            'supportIcon' => 'infinity',
+            'forest' => 2,
+            'mountain' => 2,
+            'ocean' => 0,
+            'costHand' => 2,
+            'costReserve' => 3,
+            'effectInfinity' => [
+                'effectPassive' => [
+                    'ChooseAssignment' => [
+                        'conditions' => ['isCardPlayed:spell', 'excludeSelf'],
+                        'output' => FT::GAIN(ME, BOOST, 1, 2)
+                    ],
+                ]
+            ],
+            'effectReserve' => FT::SEQ_OPTIONAL(
+                FT::ACTION(SPEND, [
+                    'cardId' => ME,
+                    'effect' =>  FT::ACTION(TARGET, [
+                        'targetType' => [CHARACTER, TOKEN, PERMANENT],
+                        'maxHandCost' => 2,
+                        'effect' => FT::ACTION(DISCARD, []),
+                    ])
+                ])
+            ),
+        ];
+    }
 }
