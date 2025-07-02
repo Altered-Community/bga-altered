dfc52feef352014eae6c6de0d14ec17f2e9a0faf
vtoper <vincent.toper@gmail.com>
2025-07-02 20:24:58 +0200

    medusa

--- modules/php/Cards/AX/AX_Rare_Medusa.php
+++ modules/php/Cards/AX/AX_Rare_Medusa.php
@@ -1,31 +1,47 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_Medusa extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_AX_71_R1',
-            'asset'  => 'ALT_CYCLONE_B_AX_71_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_AX_71_R1',
+      'asset'  => 'ALT_CYCLONE_B_AX_71_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Medusa"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Medusa"),
       'typeline' => clienttranslate("Character - Deity"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('They made her a monster simply for defending herself.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('They made her a monster simply for defending herself.'),
       'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'SO',
-   'subtypes'  => [DEITY],
- 				'effectDesc' => clienttranslate('{R} You may discard target Character. If you do, create an <AEROLITH> token in #each player\'s# Landmarks.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 5, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [DEITY],
+      'effectDesc' => clienttranslate('{R} You may discard target Character. If you do, create an <AEROLITH> token in #each player\'s# Landmarks.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 5,
+      'changedStats' => ['mountain'],
+      'effectReserve' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, TOKEN, PERMANENT],
+        'upTo' => true,
+        'effect' => FT::SEQ(
+          FT::ACTION(DISCARD, []),
+          FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Aerolith',
+            'targetLocation' => [LANDMARK],
+            'targetPlayer' => 'owner'
+          ]),
+        )
+      ])
+    ];
   }
 }
