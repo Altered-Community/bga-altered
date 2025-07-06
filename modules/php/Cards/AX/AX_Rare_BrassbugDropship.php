ea83e19d4d9186f5d80484232de8236c75206ca4
vtoper <vincent.toper@gmail.com>
2025-07-06 23:15:42 +0200

    Brassbug drop ship

--- modules/php/Cards/AX/AX_Rare_BrassbugDropship.php
+++ modules/php/Cards/AX/AX_Rare_BrassbugDropship.php
@@ -1,28 +1,48 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_BrassbugDropship extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_AX_80_R1',
-            'asset'  => 'ALT_CYCLONE_B_AX_80_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_AX_80_R1',
+      'asset'  => 'ALT_CYCLONE_B_AX_80_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Brassbug Dropship"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Brassbug Dropship"),
       'typeline' => clienttranslate("Landmark_permanent - Construction"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('"Those little pests are tough. Even three miles up, they still manage to follow us." - Subhash'),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('"Those little pests are tough. Even three miles up, they still manage to follow us." - Subhash'),
       'artist' => "Kevin Sidharta",
-			'extension'=>'SO',
-   'subtypes'  => [CONSTRUCTION,LANDMARK],
- 				'effectDesc' => clienttranslate('When I\'m sacrificed — Create a <BRASSBUG> Robot token in each of your Expeditions. (Excess Landmarks are sacrificed at the end of the Day.)'),
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['costHand','costReserve'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('When I\'m sacrificed — Create a <BRASSBUG> Robot token in each of your Expeditions. (Excess Landmarks are sacrificed at the end of the Day.)'),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPassive' => [
+        'Discard' => [
+          'condition' => 'isSacrificed',
+          'output' => FFT::SEQ(
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'AX_Common_Brassbug',
+              'targetLocation' => [STORM_RIGHT],
+            ]),
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'AX_Common_Brassbug',
+              'targetLocation' => [STORM_LEFT],
+            ])
+          ),
+        ],
+      ],
+    ];
   }
 }
