5c9e131dbaf5fb0be453bf3b6733a25d3d9ba858
vtoper <vincent.toper@gmail.com>
2025-07-06 22:30:27 +0200

    Brassbug nursery

--- modules/php/Cards/AX/AX_Rare_BrassbugNursery.php
+++ modules/php/Cards/AX/AX_Rare_BrassbugNursery.php
@@ -1,28 +1,44 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_BrassbugNursery extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_AX_78_R1',
-            'asset'  => 'ALT_CYCLONE_B_AX_78_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_AX_78_R1',
+      'asset'  => 'ALT_CYCLONE_B_AX_78_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Brassbug Nursery"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Brassbug Nursery"),
       'typeline' => clienttranslate("Landmark_permanent - Construction"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('Even far from their Hive, the Brassbugs have developed a way to continue reproducing.'),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('Even far from their Hive, the Brassbugs have developed a way to continue reproducing.'),
       'artist' => "Justice Wong",
-			'extension'=>'SO',
-   'subtypes'  => [CONSTRUCTION,LANDMARK],
- 				'effectDesc' => clienttranslate('{T}, {1}, Sacrifice a Permanent: create a <BRASSBUG> Robot token in target Expedition.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand','costReserve'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('{T}, {1}, Sacrifice a Permanent: create a <BRASSBUG> Robot token in target Expedition.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectTap' =>  FT::ACTION(TARGET, [
+        'targetPlayer' => ME,
+        'targetType' => [PERMANENT],
+        'effect' => FT::SEQ(
+          FT::ACTION(PAY, ['pay' => 1]),
+          FT::ACTION(DISCARD, ['desc' => 'sacrifice']),
+          FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'AX_Common_Brassbug',
+            'targetLocation' => STORMS,
+          ]),
+        )
+      ])
+    ];
   }
 }
--- modules/php/Cards/OD/OD_Rare_BrassbugNursery.php
+++ modules/php/Cards/OD/OD_Rare_BrassbugNursery.php
@@ -1,27 +1,47 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_BrassbugNursery extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_AX_78_R2',
-            'asset'  => 'ALT_CYCLONE_B_AX_78_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_AX_78_R2',
+      'asset'  => 'ALT_CYCLONE_B_AX_78_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Brassbug Nursery"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Brassbug Nursery"),
       'typeline' => clienttranslate("Landmark_permanent - Construction"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('Even far from their Hive, the Brassbugs have developed a way to continue reproducing.'),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('Even far from their Hive, the Brassbugs have developed a way to continue reproducing.'),
       'artist' => "Justice Wong",
-			'extension'=>'SO',
-   'subtypes'  => [CONSTRUCTION,LANDMARK],
- 				'effectDesc' => clienttranslate('{J} Create an <AEROLITH> token in your Landmarks.  {T}, {1}, Sacrifice a Permanent: create a <BRASSBUG> Robot token in target Expedition.'),
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('{J} Create an <AEROLITH> token in your Landmarks.  {T}, {1}, Sacrifice a Permanent: create a <BRASSBUG> Robot token in target Expedition.'),
+      'costHand' => 4,
+      'costReserve' => 4,
+      'effectPlayed' => FT::ACTION(INVOKE_TOKEN, [
+        'tokenType' => 'NE_Common_Aerolith',
+        'targetLocation' => [LANDMARK],
+      ]),
+      'effectTap' =>  FT::ACTION(TARGET, [
+        'targetPlayer' => ME,
+        'targetType' => [PERMANENT],
+        'effect' => FT::SEQ(
+          FT::ACTION(PAY, ['pay' => 1]),
+          FT::ACTION(DISCARD, ['desc' => 'sacrifice']),
+          FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'AX_Common_Brassbug',
+            'targetLocation' => STORMS,
+          ]),
+        )
+      ])
+    ];
   }
 }
