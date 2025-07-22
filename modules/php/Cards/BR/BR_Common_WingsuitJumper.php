06308840d0930ec40b8cac299bd50a16c6e9af55
vtoper <vincent.toper@gmail.com>
2025-07-22 23:05:18 +0200

    Wingsuit jumper

--- modules/php/Cards/BR/BR_Common_WingsuitJumper.php
+++ modules/php/Cards/BR/BR_Common_WingsuitJumper.php
@@ -1,31 +1,37 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Common_WingsuitJumper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_BR_72_C',
-            'asset'  => 'ALT_CYCLONE_B_BR_72_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_BR_72_C',
+      'asset'  => 'ALT_CYCLONE_B_BR_72_C',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Wingsuit Jumper"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Wingsuit Jumper"),
       'typeline' => clienttranslate("Character - Adventurer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('What\'s important is not the landing, but the fall.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('What\'s important is not the landing, but the fall.'),
       'artist' => "Justice Wong",
-			'extension'=>'SO',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('{R} You may <RUSH> to draw a card. (Rush means playing another card immediately.)'),
-     'forest' => 2, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
- 		'rush' => 'todo',
-];
+      'extension' => 'SO',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('{R} You may <RUSH> to draw a card. (Rush means playing another card immediately.)'),
+      'forest' => 2,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectReserve' => FT::SEQ_OPTIONAL(
+        FT::RUSH(),
+        FT::ACTION(DRAW, ['players' => ME])
+      )
+    ];
   }
 }
--- modules/php/Cards/BR/BR_Rare_WingsuitJumper.php
+++ modules/php/Cards/BR/BR_Rare_WingsuitJumper.php
@@ -1,33 +1,40 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_WingsuitJumper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_BR_72_R1',
-            'asset'  => 'ALT_CYCLONE_B_BR_72_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_BR_72_R1',
+      'asset'  => 'ALT_CYCLONE_B_BR_72_R',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Wingsuit Jumper"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Wingsuit Jumper"),
       'typeline' => clienttranslate("Character - Adventurer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('What\'s important is not the landing, but the fall.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('What\'s important is not the landing, but the fall.'),
       'artist' => "Justice Wong",
-			'extension'=>'SO',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('#{H}# You may <RUSH> to draw a card. (Rush means playing another card immediately.)'),
- 				'supportDesc' => clienttranslate('#{D} : The next Character you play this turn gains 1 boost.#'),
- 			     'supportIcon' => 'discard',
-     'forest' => 2, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
- 		'rush' => 'todo',
-];
+      'extension' => 'SO',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('#{H}# You may <RUSH> to draw a card. (Rush means playing another card immediately.)'),
+      'supportDesc' => clienttranslate('#{D} : The next Character you play this turn gains 1 boost.#'),
+      'supportIcon' => 'discard',
+      'forest' => 2,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectHand' => FT::SEQ_OPTIONAL(
+        FT::RUSH(),
+        FT::ACTION(DRAW, ['players' => ME])
+      ),
+      'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterGains1Boost']),
+    ];
   }
 }
