b6fed9f0df831173b90d46c5e2f9570da147271c
vtoper <vincent.toper@gmail.com>
2025-07-24 12:33:43 +0200

    Parliament of ashes

--- modules/php/Cards/LY/LY_Rare_ParliamentofAshes.php
+++ modules/php/Cards/LY/LY_Rare_ParliamentofAshes.php
@@ -1,27 +1,32 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_ParliamentofAshes extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_BR_81_R2',
-            'asset'  => 'ALT_CYCLONE_B_BR_81_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_BR_81_R2',
+      'asset'  => 'ALT_CYCLONE_B_BR_81_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Parliament of Ashes"),
-      'typeline' => clienttranslate("Landmark_permanent - Site"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('A place of eternal rest for the dead, and for Rune\'s Phoenix, the nest of its rebirth.'),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Parliament of Ashes"),
+      'typeline' => clienttranslate("Landmark Permanent - Site"),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('A place of eternal rest for the dead, and for Rune\'s Phoenix, the nest of its rebirth.'),
       'artist' => "Nestor Papatriantafyllou",
-			'extension'=>'SO',
-   'subtypes'  => [SITE,LANDMARK],
- 				'effectDesc' => clienttranslate('{J} #Draw a card.#  Characters you control are <TOUGH_CHA_P_1>. (Opponents must pay {1} to target them.)'),
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('{J} #Draw a card.#  Characters you control are <TOUGH_CHA_P_1>. (Opponents must pay {1} to target them.)'),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::ACTION(DRAW, ['players' => ME]),
+      'tough' => 'universalCharacter1'
+    ];
   }
 }
