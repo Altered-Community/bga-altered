eaed034c9e22560ab92449cd632a0416bb083f3e
vtoper <vincent.toper@gmail.com>
2025-07-24 11:50:50 +0200

    bravos signet

--- modules/php/Cards/YZ/YZ_Rare_BravosSignet.php
+++ modules/php/Cards/YZ/YZ_Rare_BravosSignet.php
@@ -1,28 +1,44 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_BravosSignet extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_BR_79_R2',
-            'asset'  => 'ALT_CYCLONE_B_BR_79_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_BR_79_R2',
+      'asset'  => 'ALT_CYCLONE_B_BR_79_R',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Bravos Signet"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Bravos Signet"),
       'typeline' => clienttranslate("Spell - Conjuration"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('Courage and excellence.'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('Courage and excellence.'),
       'artist' => "Ed Chee, S.Yong & Stephen",
-			'extension'=>'SO',
-   'subtypes'  => [CONJURATION],
- 				'effectDesc' => clienttranslate('#<FLEETING>.#  Choose #two:#  • Target Character gains 3 boosts.  • Return target Character with Hand Cost {3} or less to its owner\'s hand.  • Put the top card of your deck in your Mana zone (as an exhausted Mana Orb).'),
-     'costHand' => 5, 
-     'costReserve' => 5, 
-     'changedStats' => ['costHand','costReserve'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONJURATION],
+      'effectDesc' => clienttranslate('#<FLEETING>.#  Choose #two:#  • Target Character gains 3 boosts.  • Return target Character with Hand Cost {3} or less to its owner\'s hand.  • Put the top card of your deck in your Mana zone (as an exhausted Mana Orb).'),
+      'costHand' => 5,
+      'costReserve' => 5,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN(ME, FLEETING),
+        [
+          'type' => NODE_OR,
+          'args' => ['n' => 2, 'canReuse' => true],
+          'pId' => 'source',
+          'childs' => [
+            FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST, 3])]),
+            FT::ACTION(TARGET, ['maxHandCost' => 3, 'targetType' => [CHARACTER], 'effect' => FT::RETURN_TO_HAND()]),
+            FT::ACTION(DRAW_MANA, [])
+          ]
+        ]
+      )
+    ];
   }
 }
