2ea9bbddc75961d15989388cf81be2f7313e8c8f
vtoper <vincent.toper@gmail.com>
2025-07-28 20:46:07 +0200

    Sticky shroom

--- modules/php/Cards/MU/MU_Rare_StickyShroom.php
+++ modules/php/Cards/MU/MU_Rare_StickyShroom.php
@@ -1,30 +1,43 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_StickyShroom extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_67_R2',
-            'asset'  => 'ALT_CYCLONE_B_LY_67_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_67_R2',
+      'asset'  => 'ALT_CYCLONE_B_LY_67_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Sticky Shroom"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Sticky Shroom"),
       'typeline' => clienttranslate("Character - Plant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('He\'s a fun guy, but he can get into some sticky situations.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('He\'s a fun guy, but he can get into some sticky situations.'),
       'artist' => "Khoa Viet",
-			'extension'=>'SO',
-   'subtypes'  => [PLANT],
- 				'effectDesc' => clienttranslate('{J} I gain <ANCHORED>.  #At Noon — If I\'m in {V}, create an <AEROLITH> token in target player\'s Landmarks.#'),
-     'forest' => 2, 
-     'mountain' => 0, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [PLANT],
+      'effectDesc' => clienttranslate('{J} I gain <ANCHORED>.  #At Noon — If I\'m in {V}, create an <AEROLITH> token in target player\'s Landmarks.#'),
+      'forest' => 2,
+      'mountain' => 0,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::GAIN(ME, ANCHORED),
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'isInBiome:forest',
+          'output' =>  [
+            'action' => INVOKE_TOKEN,
+            'args' => ['tokenType' => 'NE_Common_Aerolith', 'targetLocation' => [LANDMARK], 'allPlayers' => true],
+          ],
+        ]
+      ]
+    ];
   }
 }
