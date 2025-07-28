2a81058a7b70b8cc0f09ce1839f3f9dc09174f81
vtoper <vincent.toper@gmail.com>
2025-07-28 20:30:26 +0200

    mountain guide

--- modules/php/Cards/BR/BR_Rare_MountainGuide.php
+++ modules/php/Cards/BR/BR_Rare_MountainGuide.php
@@ -1,32 +1,39 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_MountainGuide extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_66_R2',
-            'asset'  => 'ALT_CYCLONE_B_LY_66_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_66_R2',
+      'asset'  => 'ALT_CYCLONE_B_LY_66_R',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Mountain Guide"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Mountain Guide"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Who better than a groundhog to wind their way through these hanging rice fields?'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Who better than a groundhog to wind their way through these hanging rice fields?'),
       'artist' => "Ba Vo",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
- 				'supportDesc' => clienttranslate('#{D} : The next card you play this turn costs {1} less.#'),
- 			     'supportIcon' => 'discard',
-     'forest' => 0, 
-     'mountain' => 4, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['mountain','costHand','costReserve'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'supportDesc' => clienttranslate('#{D} : The next card you play this turn costs {1} less.#'),
+      'supportIcon' => 'discard',
+      'forest' => 0,
+      'mountain' => 4,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['mountain', 'costHand', 'costReserve'],
+      'effectSupport' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'costReduction', 'args' => ['type' => ALL, 'reduction' => 1]],
+      ],
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Common_MountainGuide.php
+++ modules/php/Cards/LY/LY_Common_MountainGuide.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_MountainGuide extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_66_C',
-            'asset'  => 'ALT_CYCLONE_B_LY_66_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_66_C',
+      'asset'  => 'ALT_CYCLONE_B_LY_66_C',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Mountain Guide"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Mountain Guide"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Who better than a groundhog to wind their way through these hanging rice fields?'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Who better than a groundhog to wind their way through these hanging rice fields?'),
       'artist' => "Ba Vo",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
-     'forest' => 0, 
-     'mountain' => 2, 
-     'ocean' => 0, 
-     'costHand' => 1, 
-     'costReserve' => 1, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'forest' => 0,
+      'mountain' => 2,
+      'ocean' => 0,
+      'costHand' => 1,
+      'costReserve' => 1,
+    ];
   }
 }
