f0e645602acb77cdb00c419149caef435fe68582
vtoper <vincent.toper@gmail.com>
2025-07-31 21:42:51 +0200

    Mountain Nymph

--- modules/php/Cards/LY/LY_Common_MountainNymph.php
+++ modules/php/Cards/LY/LY_Common_MountainNymph.php
@@ -1,30 +1,41 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_MountainNymph extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_70_C',
-            'asset'  => 'ALT_CYCLONE_B_LY_70_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_70_C',
+      'asset'  => 'ALT_CYCLONE_B_LY_70_C',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Mountain Nymph"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Mountain Nymph"),
       'typeline' => clienttranslate("Character - Fairy"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('From a mere pebble, she creates a sparkling jewel.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('From a mere pebble, she creates a sparkling jewel.'),
       'artist' => "Tristan Bideau",
-			'extension'=>'SO',
-   'subtypes'  => [FAIRY],
- 				'effectDesc' => clienttranslate('{H} If I\'m facing an Expedition in {M}, create an <AEROLITH> token in your Landmarks. (It\'s a Permanent with \"When I\'m sacrificed — Resupply. {T}, {1}: Sacrifice me.\")'),
-     'forest' => 0, 
-     'mountain' => 4, 
-     'ocean' => 0, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [FAIRY],
+      'effectDesc' => clienttranslate('{H} If I\'m facing an Expedition in {M}, create an <AEROLITH> token in your Landmarks. (It\'s a Permanent with \"When I\'m sacrificed — Resupply. {T}, {1}: Sacrifice me.\")'),
+      'forest' => 0,
+      'mountain' => 4,
+      'ocean' => 0,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(CHECK_CONDITION, [
+        'condition' => 'isOpponentExpeditionIn:mountain',
+        'effect' => FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Aerolith',
+          'targetLocation' => [LANDMARK],
+        ]),
+      ])
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Rare_MountainNymph.php
+++ modules/php/Cards/LY/LY_Rare_MountainNymph.php
@@ -1,30 +1,42 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_MountainNymph extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_70_R1',
-            'asset'  => 'ALT_CYCLONE_B_LY_70_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_70_R1',
+      'asset'  => 'ALT_CYCLONE_B_LY_70_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Mountain Nymph"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Mountain Nymph"),
       'typeline' => clienttranslate("Character - Fairy"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('From a mere pebble, she creates a sparkling jewel.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('From a mere pebble, she creates a sparkling jewel.'),
       'artist' => "Tristan Bideau",
-			'extension'=>'SO',
-   'subtypes'  => [FAIRY],
- 				'effectDesc' => clienttranslate('{H} If I\'m facing an Expedition in {M}, create an <AEROLITH> token in #target player\'s# Landmarks.  #{R} You may move a {M} Terrain Marker from any region to my region.#'),
-     'forest' => 0, 
-     'mountain' => 4, 
-     'ocean' => 0, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [FAIRY],
+      'effectDesc' => clienttranslate('{H} If I\'m facing an Expedition in {M}, create an <AEROLITH> token in #target player\'s# Landmarks.  #{R} You may move a {M} Terrain Marker from any region to my region.#'),
+      'forest' => 0,
+      'mountain' => 4,
+      'ocean' => 0,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(CHECK_CONDITION, [
+        'condition' => 'isOpponentExpeditionIn:mountain',
+        'effect' => FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Aerolith',
+          'targetLocation' => [LANDMARK],
+        ]),
+      ]),
+      'effectReserve' => FT::ACTION(MOVE_REGION_MARKER, ['markerType' => MOUNTAIN])
+    ];
   }
 }
