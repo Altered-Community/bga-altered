2436fe08a209e134c7f6dbd48a1845de88a35ef0
vtoper <vincent.toper@gmail.com>
2025-08-26 17:21:32 +0200

    Map makers boon

--- modules/php/Cards/LY/LY_Common_MapmakersBoon.php
+++ modules/php/Cards/LY/LY_Common_MapmakersBoon.php
@@ -1,27 +1,34 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_MapmakersBoon extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_80_C',
-            'asset'  => 'ALT_CYCLONE_B_LY_80_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_80_C',
+      'asset'  => 'ALT_CYCLONE_B_LY_80_C',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Mapmaker's Boon"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Mapmaker's Boon"),
       'typeline' => clienttranslate("Spell - Boon"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('Each of us paints their own reality.'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('Each of us paints their own reality.'),
       'artist' => "Anh Tung",
-			'extension'=>'SO',
-   'subtypes'  => [BOON],
- 				'effectDesc' => clienttranslate('If three or more single-terrain regions are visible, target Character gains 4 boosts. Otherwise, it gains 2 boosts.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [BOON],
+      'effectDesc' => clienttranslate('If three or more single-terrain regions are visible, target Character gains 4 boosts. Otherwise, it gains 2 boosts.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::XOR(
+        FT::ACTION(CHECK_CONDITION, ['condition' => 'countMonoVisibleRegions:3', 'effect' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST, 4)]), 'description' => clienttranslate('4 <BOOST>')]),
+        FT::ACTION(CHECK_CONDITION, ['condition' => 'countMonoVisibleRegions:2:LTE', 'effect' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST, 2)]), 'description' => clienttranslate('2 <BOOST>')]),
+      )
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Rare_MapmakersBoon.php
+++ modules/php/Cards/LY/LY_Rare_MapmakersBoon.php
@@ -1,28 +1,35 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_MapmakersBoon extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_LY_80_R1',
-            'asset'  => 'ALT_CYCLONE_B_LY_80_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_LY_80_R1',
+      'asset'  => 'ALT_CYCLONE_B_LY_80_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Mapmaker's Boon"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Mapmaker's Boon"),
       'typeline' => clienttranslate("Spell - Boon"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('Each of us paints their own reality.'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('Each of us paints their own reality.'),
       'artist' => "Anh Tung",
-			'extension'=>'SO',
-   'subtypes'  => [BOON],
- 				'effectDesc' => clienttranslate('If three or more single-terrain regions are visible, target Character gains 4 boosts. Otherwise, it gains 2 boosts.'),
-     'costHand' => 2, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [BOON],
+      'effectDesc' => clienttranslate('If three or more single-terrain regions are visible, target Character gains 4 boosts. Otherwise, it gains 2 boosts.'),
+      'costHand' => 2,
+      'costReserve' => 3,
+      'changedStats' => ['costHand'],
+      'effectPlayed' => FT::XOR(
+        FT::ACTION(CHECK_CONDITION, ['condition' => 'countMonoVisibleRegions:3', 'effect' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST, 4)]), 'description' => clienttranslate('4 <BOOST>')]),
+        FT::ACTION(CHECK_CONDITION, ['condition' => 'countMonoVisibleRegions:2:LTE', 'effect' => FT::ACTION(TARGET, ['effect' => FT::GAIN(EFFECT, BOOST, 2)]), 'description' => clienttranslate('2 <BOOST>')]),
+      )
+    ];
   }
 }
