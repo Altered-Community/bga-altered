183cdb7896bb85a24db4ea17f678a2ea253970ae
vtoper <vincent.toper@gmail.com>
2025-08-26 22:37:36 +0200

    Drowsy dodo

--- modules/php/Cards/MU/MU_Common_DrowsyDodo.php
+++ modules/php/Cards/MU/MU_Common_DrowsyDodo.php
@@ -1,30 +1,50 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Common_DrowsyDodo extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_MU_72_C',
-            'asset'  => 'ALT_CYCLONE_B_MU_72_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_MU_72_C',
+      'asset'  => 'ALT_CYCLONE_B_MU_72_C',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Drowsy Dodo"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Drowsy Dodo"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('After all that work, it\'s time for the dodo to go night-night.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('After all that work, it\'s time for the dodo to go night-night.'),
       'artist' => "Huo Miao Studio",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{J} I gain <ASLEEP>. (During Dusk, ignore my statistics. During Rest, I don\'t go to Reserve and I lose Asleep.)  When a Character joins the Expedition facing me — You may have me lose <ASLEEP>.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{J} I gain <ASLEEP>. (During Dusk, ignore my statistics. During Rest, I don\'t go to Reserve and I lose Asleep.)  When a Character joins the Expedition facing me — You may have me lose <ASLEEP>.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::GAIN(ME, ASLEEP),
+      'effectPassive' => [
+        'ChooseAssignment' => [
+          'listeningConditions' => ['isAddedCardOpponentEvent:character', 'isPlayedInOpponentExpedition'],
+          'conditions' => ['isSourceSameLocation'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+        'InvokeToken' => [
+          'listeningConditions' => ['isAddedCardOpponentEvent:character', 'isPlayedInOpponentExpedition'],
+          'conditions' => ['isSourceSameLocation'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+        'MoveCard' => [
+          'conditions' => ['isCardOfType:character', 'isPlayedInOpponentExpedition'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+      ],
+    ];
   }
 }
--- modules/php/Cards/MU/MU_Rare_DrowsyDodo.php
+++ modules/php/Cards/MU/MU_Rare_DrowsyDodo.php
@@ -1,31 +1,51 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_DrowsyDodo extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_MU_72_R1',
-            'asset'  => 'ALT_CYCLONE_B_MU_72_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_MU_72_R1',
+      'asset'  => 'ALT_CYCLONE_B_MU_72_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Drowsy Dodo"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Drowsy Dodo"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('After all that work, it\'s time for the dodo to go night-night.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('After all that work, it\'s time for the dodo to go night-night.'),
       'artist' => "Huo Miao Studio",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{J} I gain <ASLEEP>.  When a Character joins the Expedition facing me — #I gain 1 boost.#'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['forest','mountain'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{J} I gain <ASLEEP>.  When a Character joins the Expedition facing me — #I gain 1 boost.#'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['forest', 'mountain'],
+      'effectPlayed' => FT::GAIN(ME, ASLEEP),
+      'effectPassive' => [
+        'ChooseAssignment' => [
+          'listeningConditions' => ['isAddedCardOpponentEvent:character', 'isPlayedInOpponentExpedition'],
+          'conditions' => ['isSourceSameLocation'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+        'InvokeToken' => [
+          'listeningConditions' => ['isAddedCardOpponentEvent:character', 'isPlayedInOpponentExpedition'],
+          'conditions' => ['isSourceSameLocation'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+        'MoveCard' => [
+          'conditions' => ['isCardOfType:character', 'isPlayedInOpponentExpedition'],
+          'output' =>  FT::ACTION(LOOSE, ['type' => ASLEEP, 'cardId' => ME], ['optional' => true]),
+        ],
+      ],
+    ];
   }
 }
