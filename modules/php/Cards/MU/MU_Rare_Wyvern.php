5e51cc363af9b5364fd35380a08d0d11f9040e9b
vtoper <vincent.toper@gmail.com>
2025-08-26 22:39:46 +0200

    Wyvern

--- modules/php/Cards/MU/MU_Rare_Wyvern.php
+++ modules/php/Cards/MU/MU_Rare_Wyvern.php
@@ -1,30 +1,34 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_Wyvern extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_MU_73_R1',
-            'asset'  => 'ALT_CYCLONE_B_MU_73_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_MU_73_R1',
+      'asset'  => 'ALT_CYCLONE_B_MU_73_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Wyvern"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Wyvern"),
       'typeline' => clienttranslate("Character - Dragon"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('With their fluffy texture and cotton-candy flavor, woollybacks are wyverns\' favorite prey.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('With their fluffy texture and cotton-candy flavor, woollybacks are wyverns\' favorite prey.'),
       'artist' => "Matteo Spirito",
-			'extension'=>'SO',
-   'subtypes'  => [DRAGON],
- 				'effectDesc' => clienttranslate('{H} You may send to Reserve #up to two# target Characters with Hand Cost {1} or less.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [DRAGON],
+      'effectDesc' => clienttranslate('{H} You may send to Reserve #up to two# target Characters with Hand Cost {1} or less.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(TARGET, ['maxHandCost' => 1, 'upTo' => true, 'n' => 2, 'effect' => FT::DISCARD_TO_RESERVE()]),
+    ];
   }
 }
