6b9499b6e3de3e793f16250f9b831abb42065849
vtoper <vincent.toper@gmail.com>
2025-08-26 21:47:33 +0200

    Armored Bird

--- modules/php/Cards/MU/MU_Common_ArmoredBird.php
+++ modules/php/Cards/MU/MU_Common_ArmoredBird.php
@@ -1,30 +1,34 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Common_ArmoredBird extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_MU_69_C',
-            'asset'  => 'ALT_CYCLONE_B_MU_69_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_MU_69_C',
+      'asset'  => 'ALT_CYCLONE_B_MU_69_C',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Armored Bird"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Armored Bird"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('He raised a shield of feathers. Lances and harpoons shattered against his steely wings.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('He raised a shield of feathers. Lances and harpoons shattered against his steely wings.'),
       'artist' => "Victor Canton",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('$<DEFENDER>.'),
-     'forest' => 4, 
-     'mountain' => 4, 
-     'ocean' => 5, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('$<DEFENDER>.'),
+      'forest' => 4,
+      'mountain' => 4,
+      'ocean' => 5,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'defender' => true
+    ];
   }
 }
