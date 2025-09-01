94bdde3e7c0daac739e18f51d82641e5a902e05b
vtoper <vincent.toper@gmail.com>
2025-09-02 00:04:34 +0200

    Nike

--- modules/php/Cards/OD/OD_Common_Nike.php
+++ modules/php/Cards/OD/OD_Common_Nike.php
@@ -1,30 +1,40 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Common_Nike extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_OR_76_C',
-            'asset'  => 'ALT_CYCLONE_B_OR_76_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_OR_76_C',
+      'asset'  => 'ALT_CYCLONE_B_OR_76_C',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Nike"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Nike"),
       'typeline' => clienttranslate("Character - Deity"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('When she spreads her wings, her victory shall resonate for all eternity.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('When she spreads her wings, her victory shall resonate for all eternity.'),
       'artist' => "Aaron Ming",
-			'extension'=>'SO',
-   'subtypes'  => [DEITY],
- 				'effectDesc' => clienttranslate('$<ETERNAL_FS>.  At Noon — Target Expedition <ASCENDS>. (Until Rest, it can move forward even if matched in its region\'s terrains by the opponent\'s Expedition.)'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 7, 
-     'costReserve' => 7, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [DEITY],
+      'effectDesc' => clienttranslate('$<ETERNAL_FS>.  At Noon — Target Expedition <ASCENDS>. (Until Rest, it can move forward even if matched in its region\'s terrains by the opponent\'s Expedition.)'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 7,
+      'costReserve' => 7,
+      'eternal' => true,
+      'effectPassive' => [
+        'Noon' => [
+          'conditions' => ['isMe'],
+          'output' => FT::ACTION(TARGET_EXPEDITION, ['effect' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'ascend'])]),
+        ]
+      ]
+    ];
   }
 }
