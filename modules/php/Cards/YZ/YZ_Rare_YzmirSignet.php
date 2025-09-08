8882a6d09fd79e29d369c876c78a3afb2d03d8b9
vtoper <vincent.toper@gmail.com>
2025-09-08 16:42:46 +0200

    YzmirSignet

--- modules/php/Cards/YZ/YZ_Rare_YzmirSignet.php
+++ modules/php/Cards/YZ/YZ_Rare_YzmirSignet.php
@@ -1,28 +1,44 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_YzmirSignet extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_YZ_76_R1',
-            'asset'  => 'ALT_CYCLONE_B_YZ_76_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_YZ_76_R1',
+      'asset'  => 'ALT_CYCLONE_B_YZ_76_R',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Yzmir Signet"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Yzmir Signet"),
       'typeline' => clienttranslate("Spell - Conjuration"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('Secrecy and sacrifice.'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('Secrecy and sacrifice.'),
       'artist' => "HuoMiao Studio",
-			'extension'=>'SO',
-   'subtypes'  => [CONJURATION],
- 				'effectDesc' => clienttranslate('#<FLEETING>.#  Choose #two:#  • Send to Reserve target Character with Hand Cost {2} or less.  • <SABOTAGE>.  • Draw a card.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand','costReserve'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONJURATION],
+      'effectDesc' => clienttranslate('#<FLEETING>.#  Choose #two:#  • Send to Reserve target Character with Hand Cost {2} or less.  • <SABOTAGE>.  • Draw a card.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN(ME, FLEETING),
+        [
+          'type' => NODE_OR,
+          'args' => ['n' => 2],
+          'pId' => 'source',
+          'childs' => [
+            FT::ACTION(TARGET, ['maxHandCost' => 2, 'effect' => FT::DISCARD_TO_RESERVE()]),
+            FT::SABOTAGE(),
+            FT::ACTION(DRAW, ['players' => ME])
+          ]
+        ]
+      )
+    ];
   }
 }
