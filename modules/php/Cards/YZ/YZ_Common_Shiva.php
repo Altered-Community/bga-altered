c089fdeb663c6768502e692b767289e9abfafdc2
vtoper <vincent.toper@gmail.com>
2025-09-08 16:26:13 +0200

    Shiva

--- modules/php/Cards/YZ/YZ_Common_Shiva.php
+++ modules/php/Cards/YZ/YZ_Common_Shiva.php
@@ -1,30 +1,37 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_Shiva extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_YZ_74_C',
-            'asset'  => 'ALT_CYCLONE_B_YZ_74_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_YZ_74_C',
+      'asset'  => 'ALT_CYCLONE_B_YZ_74_C',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Shiva"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Shiva"),
       'typeline' => clienttranslate("Character - Deity"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Any creation begins with an act of destruction.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Any creation begins with an act of destruction.'),
       'artist' => "Taras Susak",
-			'extension'=>'SO',
-   'subtypes'  => [DEITY],
- 				'effectDesc' => clienttranslate('{H} You may discard target Permanent. If you don\'t, I gain 2 boosts.'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 4, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [DEITY],
+      'effectDesc' => clienttranslate('{H} You may discard target Permanent. If you don\'t, I gain 2 boosts.'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 4,
+      'costReserve' => 2,
+      'effectHand' => FT::XOR(
+        FT::ACTION(TARGET, ['targetType' => [PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+        FT::GAIN(ME, BOOST, 2)
+      )
+    ];
   }
 }
