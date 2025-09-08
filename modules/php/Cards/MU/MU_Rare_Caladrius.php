ffd2b5689f1e350f9a19a6b2e9b13fdc7a1c7376
vtoper <vincent.toper@gmail.com>
2025-09-08 12:58:56 +0200

    Caladrius

--- modules/php/Cards/MU/MU_Rare_Caladrius.php
+++ modules/php/Cards/MU/MU_Rare_Caladrius.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_Caladrius extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_YZ_72_R2',
-            'asset'  => 'ALT_CYCLONE_B_YZ_72_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_YZ_72_R2',
+      'asset'  => 'ALT_CYCLONE_B_YZ_72_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Caladrius"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Caladrius"),
       'typeline' => clienttranslate("Character - Animal"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('With a simple glance, it reveals your destination.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('With a simple glance, it reveals your destination.'),
       'artist' => "Fahmi  Fauzi",
-			'extension'=>'SO',
-   'subtypes'  => [ANIMAL],
- 				'effectDesc' => clienttranslate('{H} You may send to Reserve target Character with #Hand Cost {1}# or less.'),
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 1, 
-     'changedStats' => ['costHand'], 
-];
+      'extension' => 'SO',
+      'subtypes'  => [ANIMAL],
+      'effectDesc' => clienttranslate('{H} You may send to Reserve target Character with #Hand Cost {1}# or less.'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 1,
+      'changedStats' => ['costHand'],
+      'effectHand' => FT::ACTION(TARGET, ['maxHandCost' => 1, 'upTo' => true, 'effect' => FT::DISCARD_TO_RESERVE()]),
+    ];
   }
 }
