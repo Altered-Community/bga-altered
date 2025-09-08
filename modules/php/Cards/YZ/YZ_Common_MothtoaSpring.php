e38b6301e7549979ebd46a6d210f1ba519080329
vtoper <vincent.toper@gmail.com>
2025-09-08 17:29:17 +0200

    Moth to a spring

--- modules/php/Cards/YZ/YZ_Common_MothtoaSpring.php
+++ modules/php/Cards/YZ/YZ_Common_MothtoaSpring.php
@@ -1,27 +1,38 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_MothtoaSpring extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_CYCLONE_B_YZ_78_C',
-            'asset'  => 'ALT_CYCLONE_B_YZ_78_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_CYCLONE_B_YZ_78_C',
+      'asset'  => 'ALT_CYCLONE_B_YZ_78_C',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Moth to a Spring"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Moth to a Spring"),
       'typeline' => clienttranslate("Spell - Conjuration Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('A spring that will quench any thirst for magic.'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('A spring that will quench any thirst for magic.'),
       'artist' => "HuoMiao Studio",
-			'extension'=>'SO',
-   'subtypes'  => [CONJURATION,MANEUVER],
- 				'effectDesc' => clienttranslate('Create a <MANA_MOTH> Illusion token in target Expedition.  Draw a card.'),
-     'costHand' => 4, 
-     'costReserve' => 5, 
-];
+      'extension' => 'SO',
+      'subtypes'  => [CONJURATION, MANEUVER],
+      'effectDesc' => clienttranslate('Create a <MANA_MOTH> Illusion token in target Expedition.  Draw a card.'),
+      'costHand' => 4,
+      'costReserve' => 5,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'YZ_Common_ManaMoth',
+          'targetLocation' => STORMS,
+        ]),
+        FT::ACTION(DRAW, ['players' => ME])
+      )
+    ];
   }
 }
