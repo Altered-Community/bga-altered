645ec11b241db4ac63f3f577de329041ac872953
Parn158 <vincent.toper@gmail.com>
2025-10-12 21:58:46 +0200

    #179503: "Unique — Jeanne d'Arc (Ordis) — ALT_COREKS_B_OR_17_U_1949"

--- modules/php/Actions/Target.php
+++ modules/php/Actions/Target.php
@@ -408,7 +408,7 @@ class Target extends \ALT\Models\Action
             $discardSource = $cardFrom . '-' . $card->getPId();
             $node = Utils::updateTree($node, [0 => 'discardedSource'], [$discardSource], ['targetLocation']);
             // if the discarded card is the source, need to update the location
-            if ($cardId == $this->getSourceId() && in_array($card->getUid(), ['ALT_CORE_B_OR_06_U_3184', 'ALT_COREKS_B_AX_07_U_5235', 'ALT_CORE_B_OR_06_U_3184'])) {
+            if ($cardId == $this->getSourceId() && in_array($card->getUid(), ['ALT_CORE_B_OR_06_U_3184', 'ALT_COREKS_B_AX_07_U_5235', 'ALT_CORE_B_OR_06_U_3184', 'ALT_COREKS_B_OR_17_U_1949'])) {
               $node = Utils::updateTree($node, [0 => 'initialSource'], [$discardSource], ['targetLocation']);
             }
           }
