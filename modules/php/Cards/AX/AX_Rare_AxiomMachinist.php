b75f44f0f3a02155d0507a423c3ac8cce7dbde3f
Parn158 <vincent.toper@gmail.com>
2025-11-19 22:19:29 +0100

    Axiom Machinist

--- modules/php/Cards/AX/AX_Rare_AxiomMachinist.php
+++ modules/php/Cards/AX/AX_Rare_AxiomMachinist.php
@@ -1,31 +1,47 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_AxiomMachinist extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_87_R1',
-            'asset'  => 'ALT_DUSTER_B_AX_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_87_R1',
+      'asset'  => 'ALT_DUSTER_B_AX_87_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Axiom Machinist"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Axiom Machinist"),
       'typeline' => clienttranslate("Character - Engineer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('From Kelon to Sap to Manaseed, the Axiom never stop experimenting with alternative energy sources.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('From Kelon to Sap to Manaseed, the Axiom never stop experimenting with alternative energy sources.'),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'SDU',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('#{J}# You may ready target Permanent in play or target card in Reserve.'),
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 1, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ENGINEER],
+      'effectDesc' => clienttranslate('#{J}# You may ready target Permanent in play or target card in Reserve.'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'effectPlayed' => FT::XOR(
+        FT::ACTION(TARGET, [
+          'targetType' => [PERMANENT],
+          'upTo' => true,
+          'effect' => FT::ACTION(READY, ['cardId' => EFFECT])
+        ]),
+        FT::ACTION(TARGET, [
+          'targetType' => [PERMANENT, CHARACTER, SPELL],
+          'targetLocation' => [RESERVE],
+          'upTo' => true,
+          'effect' => FT::ACTION(READY, ['cardId' => EFFECT])
+        ])
+      )
+    ];
   }
 }
