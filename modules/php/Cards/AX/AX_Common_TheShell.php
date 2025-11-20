2675e84898b659f12fb9b39d1a59689abc36ce29
Parn158 <vincent.toper@gmail.com>
2025-11-20 23:13:09 +0100

    The Shell

--- modules/php/Cards/AX/AX_Common_TheShell.php
+++ modules/php/Cards/AX/AX_Common_TheShell.php
@@ -1,30 +1,45 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_TheShell extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_96_C',
-            'asset'  => 'ALT_DUSTER_B_AX_96_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_96_C',
+      'asset'  => 'ALT_DUSTER_B_AX_96_C',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("The Shell"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("The Shell"),
       'typeline' => clienttranslate("Character - Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Does this physical shell define me?"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Does this physical shell define me?"'),
       'artist' => "Taras Susak",
-			'extension'=>'SDU',
-   'subtypes'  => [ROBOT],
- 				'effectDesc' => clienttranslate('{R} You may put a card from your hand in Reserve to draw a card.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 1, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ROBOT],
+      'effectDesc' => clienttranslate('{R} You may put a card from your hand in Reserve to draw a card.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 1,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectReserve' => FT::SEQ_OPTIONAL(
+        FT::ACTION(
+          TARGET,
+          [
+            'targetType' => [CHARACTER, SPELL, PERMANENT],
+            'targetPlayer' => ME,
+            'targetLocation' => [HAND],
+            'effect' => FT::DISCARD_TO_RESERVE(),
+          ]
+        ),
+        FT::ACTION(DRAW, ['players' => ME])
+      ),
+    ];
   }
 }
--- modules/php/Cards/AX/AX_Rare_TheShell.php
+++ modules/php/Cards/AX/AX_Rare_TheShell.php
@@ -1,31 +1,46 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_TheShell extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_96_R1',
-            'asset'  => 'ALT_DUSTER_B_AX_96_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_96_R1',
+      'asset'  => 'ALT_DUSTER_B_AX_96_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("The Shell"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("The Shell"),
       'typeline' => clienttranslate("Character - Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Does this physical shell define me?"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Does this physical shell define me?"'),
       'artist' => "Taras Susak",
-			'extension'=>'SDU',
-   'subtypes'  => [ROBOT],
- 				'effectDesc' => clienttranslate('{R} You may put a card from your hand in Reserve to draw a card.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 1, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ROBOT],
+      'effectDesc' => clienttranslate('{R} You may put a card from your hand in Reserve to draw a card.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'changedStats' => ['costHand'],
+      'effectReserve' => FT::SEQ_OPTIONAL(
+        FT::ACTION(
+          TARGET,
+          [
+            'targetType' => [CHARACTER, SPELL, PERMANENT],
+            'targetPlayer' => ME,
+            'targetLocation' => [HAND],
+            'effect' => FT::DISCARD_TO_RESERVE(),
+          ]
+        ),
+        FT::ACTION(DRAW, ['players' => ME])
+      ),
+    ];
   }
 }
