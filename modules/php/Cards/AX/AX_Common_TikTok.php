6383937f46c59d3b8eed0b31f35569a7ade76960
Parn158 <vincent.toper@gmail.com>
2025-11-20 22:52:07 +0100

    Tiktok

--- modules/php/Cards/AX/AX_Common_TikTok.php
+++ modules/php/Cards/AX/AX_Common_TikTok.php
@@ -1,30 +1,50 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_TikTok extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_86_C',
-            'asset'  => 'ALT_DUSTER_B_AX_86_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_86_C',
+      'asset'  => 'ALT_DUSTER_B_AX_86_C',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Tik-Tok"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Tik-Tok"),
       'typeline' => clienttranslate("Character - Artist Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('HashtagDanceTillUDrop HashtagOzmaLoveLove'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('HashtagDanceTillUDrop HashtagOzmaLoveLove'),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'SDU',
-   'subtypes'  => [ARTIST,ROBOT],
- 				'effectDesc' => clienttranslate('{H} Exhaust ({T}) a Permanent you control or a card in your Reserve. (Exhausted cards can\'t be played and have no Support abilities.)'),
-     'forest' => 3, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ARTIST, ROBOT],
+      'effectDesc' => clienttranslate('{H} Exhaust ({T}) a Permanent you control or a card in your Reserve. (Exhausted cards can\'t be played and have no Support abilities.)'),
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' => FT::XOR(
+        FT::ACTION(TARGET, [
+          'targetType' => [PERMANENT],
+          'targetPlayer' => ME,
+          'effect' => FT::SEQ(
+            FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+          )
+        ]),
+        FT::ACTION(TARGET, [
+          'targetType' => TYPES,
+          'targetPlayer' => ME,
+          'targetLocation' => [RESERVE],
+          'effect' => FT::SEQ(
+            FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+          )
+        ])
+      )
+    ];
   }
 }
--- modules/php/Cards/YZ/YZ_Rare_TikTok.php
+++ modules/php/Cards/YZ/YZ_Rare_TikTok.php
@@ -1,30 +1,73 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_TikTok extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_86_R2',
-            'asset'  => 'ALT_DUSTER_B_AX_86_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_86_R2',
+      'asset'  => 'ALT_DUSTER_B_AX_86_R',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Tik-Tok"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Tik-Tok"),
       'typeline' => clienttranslate("Character - Artist Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('HashtagDanceTillUDrop HashtagOzmaLoveLove'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('HashtagDanceTillUDrop HashtagOzmaLoveLove'),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'SDU',
-   'subtypes'  => [ARTIST,ROBOT],
- 				'effectDesc' => clienttranslate('{H} #Each player# exhausts ({T}) a Permanent they control or a card in their Reserve.'),
-     'forest' => 3, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ARTIST, ROBOT],
+      'effectDesc' => clienttranslate('{H} #Each player# exhausts ({T}) a Permanent they control or a card in their Reserve.'),
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' =>  FT::SEQ(
+        FT::XOR(
+          FT::ACTION(TARGET, [
+            'targetType' => [PERMANENT],
+            'targetPlayer' => ME,
+            'effect' => FT::SEQ(
+              FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+            )
+          ]),
+          FT::ACTION(TARGET, [
+            'targetType' => TYPES,
+            'targetPlayer' => ME,
+            'targetLocation' => [RESERVE],
+            'effect' => FT::SEQ(
+              FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+            )
+          ])
+        ),
+        [
+          'type' => NODE_XOR,
+          'pId' => 'nextPlayer',
+          'childs' => [
+            FT::ACTION(TARGET, [
+              'targetType' => [PERMANENT],
+              'targetPlayer' => ME,
+              'effect' => FT::SEQ(
+                FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+              )
+            ]),
+            FT::ACTION(TARGET, [
+              'targetType' => TYPES,
+              'targetPlayer' => ME,
+              'targetLocation' => [RESERVE],
+              'effect' => FT::SEQ(
+                FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+              )
+            ])
+          ]
+        ]
+      )
+    ];
   }
 }
