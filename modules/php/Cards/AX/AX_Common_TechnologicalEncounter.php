21c709ea4d8d96d8795f2846007327f45c35177e
Parn158 <vincent.toper@gmail.com>
2025-11-24 23:31:37 +0100

    Technological encounter

--- modules/php/Cards/AX/AX_Common_TechnologicalEncounter.php
+++ modules/php/Cards/AX/AX_Common_TechnologicalEncounter.php
@@ -1,28 +1,39 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_TechnologicalEncounter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_98_C',
-            'asset'  => 'ALT_DUSTER_B_AX_98_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_98_C',
+      'asset'  => 'ALT_DUSTER_B_AX_98_C',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Technological Encounter"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Technological Encounter"),
       'typeline' => clienttranslate("Spell - Conjuration Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('"The fruit of our research will lead our peoples to new heights."'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('"The fruit of our research will lead our peoples to new heights."'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [CONJURATION,MANEUVER],
- 				'effectDesc' => clienttranslate('$<COOLDOWN>.  Draw a card, then create a <MANASEED> token in your Landmarks. (It\'s a Permanent with \"{T}, Sacrifice me: Ready a Mana Orb.\")'),
-     'costHand' => 4, 
-     'costReserve' => 2, 
- 		'cooldown' => true,
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CONJURATION, MANEUVER],
+      'effectDesc' => clienttranslate('$<COOLDOWN>.  Draw a card, then create a <MANASEED> token in your Landmarks. (It\'s a Permanent with \"{T}, Sacrifice me: Ready a Mana Orb.\")'),
+      'costHand' => 4,
+      'costReserve' => 2,
+      'cooldown' => true,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(DRAW, ['players' => ME]),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+      )
+    ];
   }
 }
--- modules/php/Cards/AX/AX_Rare_TechnologicalEncounter.php
+++ modules/php/Cards/AX/AX_Rare_TechnologicalEncounter.php
@@ -1,29 +1,40 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_TechnologicalEncounter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_98_R1',
-            'asset'  => 'ALT_DUSTER_B_AX_98_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_98_R1',
+      'asset'  => 'ALT_DUSTER_B_AX_98_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Technological Encounter"),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Technological Encounter"),
       'typeline' => clienttranslate("Spell - Conjuration Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('"The fruit of our research will lead our peoples to new heights."'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('"The fruit of our research will lead our peoples to new heights."'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [CONJURATION,MANEUVER],
- 				'effectDesc' => clienttranslate('<COOLDOWN>.  Draw a card, then create a <MANASEED> token in your Landmarks.'),
-     'costHand' => 3, 
-     'costReserve' => 2, 
-     'changedStats' => ['costHand'], 
- 		'cooldown' => true,
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CONJURATION, MANEUVER],
+      'effectDesc' => clienttranslate('<COOLDOWN>.  Draw a card, then create a <MANASEED> token in your Landmarks.'),
+      'costHand' => 3,
+      'costReserve' => 2,
+      'changedStats' => ['costHand'],
+      'cooldown' => true,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(DRAW, ['players' => ME]),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+      )
+    ];
   }
 }
