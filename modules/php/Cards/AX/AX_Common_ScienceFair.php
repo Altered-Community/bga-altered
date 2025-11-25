1bf74b00d35643556312384b8b6f96f046baae88
Parn158 <vincent.toper@gmail.com>
2025-11-25 21:58:11 +0100

    science fair

--- modules/php/Cards/AX/AX_Common_ScienceFair.php
+++ modules/php/Cards/AX/AX_Common_ScienceFair.php
@@ -1,27 +1,39 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Common_ScienceFair extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_100_C',
-            'asset'  => 'ALT_DUSTER_B_AX_100_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_100_C',
+      'asset'  => 'ALT_DUSTER_B_AX_100_C',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Science Fair"),
-      'typeline' => clienttranslate("Landmark_permanent - Site"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('At this convention, the Reka show off their scientific superiority.'),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Science Fair"),
+      'typeline' => clienttranslate("Landmark Permanent - Site"),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('At this convention, the Reka show off their scientific superiority.'),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'SDU',
-   'subtypes'  => [SITE,LANDMARK],
- 				'effectDesc' => clienttranslate('{T} : Put a card from your hand in Reserve. If you do, draw a card.'),
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('{T} : Put a card from your hand in Reserve. If you do, draw a card.'),
+      'costHand' => 4,
+      'costReserve' => 4,
+      'effectTap' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, PERMANENT],
+        'targetPlayer' => ME,
+        'targetLocation' => [HAND],
+        'effect' => FT::SEQ(
+          FT::DISCARD_TO_RESERVE(),
+          FT::ACTION(DRAW, ['players' => ME])
+        )
+      ]),
+    ];
   }
 }
--- modules/php/Cards/AX/AX_Rare_ScienceFair.php
+++ modules/php/Cards/AX/AX_Rare_ScienceFair.php
@@ -1,28 +1,42 @@
 <?php
+
 namespace ALT\Cards\AX;
+
 use ALT\Helpers\FT;
 
 class AX_Rare_ScienceFair extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_AX_100_R1',
-            'asset'  => 'ALT_DUSTER_B_AX_100_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_AX_100_R1',
+      'asset'  => 'ALT_DUSTER_B_AX_100_R',
 
-    	'faction'  => FACTION_AX,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Science Fair"),
-      'typeline' => clienttranslate("Landmark_permanent - Site"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('At this convention, the Reka show off their scientific superiority.'),
+      'faction'  => FACTION_AX,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Science Fair"),
+      'typeline' => clienttranslate("Landmark Permanent - Site"),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('At this convention, the Reka show off their scientific superiority.'),
       'artist' => "Jean-Baptiste Andrier",
-			'extension'=>'SDU',
-   'subtypes'  => [SITE,LANDMARK],
- 				'effectDesc' => clienttranslate('#{J} Exhaust me.#  {T} : Put a card from your hand in Reserve, #then exhaust it ({T}).# If you do, draw a card.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['costHand','costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('#{J} Exhaust me.#  {T} : Put a card from your hand in Reserve, #then exhaust it ({T}).# If you do, draw a card.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(EXHAUST, ['cardId' => ME]),
+      'effectTap' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, PERMANENT],
+        'targetPlayer' => ME,
+        'targetLocation' => [HAND],
+        'effect' => FT::SEQ(
+          FT::DISCARD_TO_RESERVE(),
+          FT::ACTION(EXHAUST, ['cardId' => EFFECT]),
+          FT::ACTION(DRAW, ['players' => ME])
+        )
+      ]),
+    ];
   }
 }
