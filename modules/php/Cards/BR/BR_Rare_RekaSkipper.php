4ac4fe21226ab290fdcb8adab1f631c771a2d8ac
Parn158 <vincent.toper@gmail.com>
2025-11-25 22:57:15 +0100

    Reka Skipper

--- modules/php/Cards/BR/BR_Rare_RekaSkipper.php
+++ modules/php/Cards/BR/BR_Rare_RekaSkipper.php
@@ -1,30 +1,52 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_RekaSkipper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_BR_87_R1',
-            'asset'  => 'ALT_DUSTER_B_BR_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_BR_87_R1',
+      'asset'  => 'ALT_DUSTER_B_BR_87_R',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Skipper"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Skipper"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Into the great wide open.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Into the great wide open.'),
       'artist' => "Jefrey Yonathan",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('#{R} I gain 1 boost.#  When my Expedition moves forward — Create a <MANASEED> token in your Landmarks.'),
-     'forest' => 0, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('#{R} I gain 1 boost.#  When my Expedition moves forward — Create a <MANASEED> token in your Landmarks.'),
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'effectReserve' => FT::GAIN(ME, BOOST),
+      'effectPassive' => [
+        'AfterDusk' => [
+          'condition' => 'myExpeditionHasMoved',
+          'output' =>  FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Manaseed',
+            'targetLocation' => [LANDMARK],
+          ]),
+        ],
+        'MoveExpedition' => [
+          'condition' => 'myExpeditionHasMoved',
+          'output' =>  FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Manaseed',
+            'targetLocation' => [LANDMARK],
+          ]),
+        ],
+      ],
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Rare_RekaSkipper.php
+++ modules/php/Cards/LY/LY_Rare_RekaSkipper.php
@@ -1,30 +1,51 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_RekaSkipper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_BR_87_R2',
-            'asset'  => 'ALT_DUSTER_B_BR_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_BR_87_R2',
+      'asset'  => 'ALT_DUSTER_B_BR_87_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Skipper"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Skipper"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Into the great wide open.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Into the great wide open.'),
       'artist' => "Jefrey Yonathan",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('When my Expedition moves forward — Create a <MANASEED> token in your Landmarks.'),
-     'forest' => 0, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('When my Expedition moves forward — Create a <MANASEED> token in your Landmarks.'),
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'effectPassive' => [
+        'AfterDusk' => [
+          'condition' => 'myExpeditionHasMoved',
+          'output' =>  FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Manaseed',
+            'targetLocation' => [LANDMARK],
+          ]),
+        ],
+        'MoveExpedition' => [
+          'condition' => 'myExpeditionHasMoved',
+          'output' =>  FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Manaseed',
+            'targetLocation' => [LANDMARK],
+          ]),
+        ],
+      ],
+    ];
   }
 }
