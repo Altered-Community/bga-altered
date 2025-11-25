8a6610f7a3e478072a40086efcc2ac4413cdbe09
Parn158 <vincent.toper@gmail.com>
2025-11-25 22:53:44 +0100

    Isabel Letham

--- modules/php/Cards/LY/LY_Rare_IsabelLetham.php
+++ modules/php/Cards/LY/LY_Rare_IsabelLetham.php
@@ -1,32 +1,38 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_IsabelLetham extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_BR_90_R2',
-            'asset'  => 'ALT_DUSTER_B_BR_90_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_BR_90_R2',
+      'asset'  => 'ALT_DUSTER_B_BR_90_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Isabel Letham"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Isabel Letham"),
       'typeline' => clienttranslate("Character - Adventurer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The lowest point of the wave is where she draws all her strength.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The lowest point of the wave is where she draws all her strength.'),
       'artist' => "HuoMiao Studio",
-			'extension'=>'SDU',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('{R} #<RESUPPLY>, then# you may <RUSH>.'),
-     'forest' => 2, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['forest','costReserve'], 
- 		'rush' => 'todo',
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('{R} #<RESUPPLY>, then# you may <RUSH>.'),
+      'forest' => 2,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['forest', 'costReserve'],
+      'effectReserve' => FT::SEQ(
+        FT::ACTION(RESUPPLY, []),
+        FT::SEQ_OPTIONAL_MANUAL(FT::RUSH())
+      )
+    ];
   }
 }
