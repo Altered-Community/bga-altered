c43e064765f928f80d8479e046b24e632a377b5d
Parn158 <vincent.toper@gmail.com>
2025-11-26 18:42:37 +0100

    Reka Peddler

--- modules/php/Cards/BR/BR_Rare_RekaPeddler.php
+++ modules/php/Cards/BR/BR_Rare_RekaPeddler.php
@@ -1,31 +1,43 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_RekaPeddler extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_BR_92_R1',
-            'asset'  => 'ALT_DUSTER_B_BR_92_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_BR_92_R1',
+      'asset'  => 'ALT_DUSTER_B_BR_92_R',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Peddler"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Peddler"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Keep the change!"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Keep the change!"'),
       'artist' => "Gamon Studio",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{R} #You may# create a <MANASEED> token in target opponent\'s Landmarks. #If you do,# I lose <FLEETING>.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{R} #You may# create a <MANASEED> token in target opponent\'s Landmarks. #If you do,# I lose <FLEETING>.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'changedStats' => ['mountain'],
+      'effectReserve' => FT::SEQ_OPTIONAL(
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'targetPlayer' => OPPONENT,
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+        FT::LOOSE(ME, FLEETING),
+      )
+    ];
   }
 }
