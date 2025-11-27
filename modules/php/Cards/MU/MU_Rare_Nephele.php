0e2a7a35cc8999069f013b34acb12786496acf2e
Parn158 <vincent.toper@gmail.com>
2025-11-27 22:14:41 +0100

    Nephele

--- modules/php/Cards/MU/MU_Rare_Nephele.php
+++ modules/php/Cards/MU/MU_Rare_Nephele.php
@@ -1,31 +1,43 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_Nephele extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_BR_96_R2',
-            'asset'  => 'ALT_DUSTER_B_BR_96_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_BR_96_R2',
+      'asset'  => 'ALT_DUSTER_B_BR_96_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Nephele"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Nephele"),
       'typeline' => clienttranslate("Character - Fairy"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Even the brightest day of sunshine has its clouds.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Even the brightest day of sunshine has its clouds.'),
       'artist' => "Taras Susak",
-			'extension'=>'SDU',
-   'subtypes'  => [FAIRY],
- 				'effectDesc' => clienttranslate('{J} You may pass. #If you do,# create a <MANASEED> token in #another target player\'s# Landmarks.'),
-     'forest' => 5, 
-     'mountain' => 5, 
-     'ocean' => 5, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [FAIRY],
+      'effectDesc' => clienttranslate('{J} You may pass. #If you do,# create a <MANASEED> token in #another target player\'s# Landmarks.'),
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 5,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['mountain'],
+      'effectPlayed' => FT::SEQ_OPTIONAL_MANUAL(
+        FT::ACTION(END_AFTERNOON, []),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'allPlayers' => true,
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+      )
+    ];
   }
 }
