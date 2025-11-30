aa5eb9980f67307d254d97a1e7f799d8303f0a63
Parn158 <vincent.toper@gmail.com>
2025-11-30 23:11:55 +0100

    Nyala night reveler

--- modules/php/Cards/LY/LY_Common_NyalaNightReveler.php
+++ modules/php/Cards/LY/LY_Common_NyalaNightReveler.php
@@ -1,30 +1,34 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_NyalaNightReveler extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_90_C',
-            'asset'  => 'ALT_DUSTER_B_LY_90_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_90_C',
+      'asset'  => 'ALT_DUSTER_B_LY_90_C',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Nyala, Night Reveler"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Nyala, Night Reveler"),
       'typeline' => clienttranslate("Character - Mage"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"I can get in anywhere, VIP list or not."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"I can get in anywhere, VIP list or not."'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [MAGE],
- 				'effectDesc' => clienttranslate('{H} If I\'m <IN_CONTACT>, you may exchange a card from your Reserve with a card from your hand. (I\'m In Contact if another player\'s Expedition is in my region.)'),
-     'forest' => 0, 
-     'mountain' => 2, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MAGE],
+      'effectDesc' => clienttranslate('{H} If I\'m <IN_CONTACT>, you may exchange a card from your Reserve with a card from your hand. (I\'m In Contact if another player\'s Expedition is in my region.)'),
+      'forest' => 0,
+      'mountain' => 2,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(CHECK_CONDITION, ['condition' => 'isInContact', 'effect' =>  FT::ACTION(EXCHANGE, ['targetType' => [PERMANENT, SPELL, CHARACTER]], ['optional' => true]),])
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Rare_NyalaNightReveler.php
+++ modules/php/Cards/LY/LY_Rare_NyalaNightReveler.php
@@ -1,33 +1,38 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_NyalaNightReveler extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_90_R1',
-            'asset'  => 'ALT_DUSTER_B_LY_90_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_90_R1',
+      'asset'  => 'ALT_DUSTER_B_LY_90_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Nyala, Night Reveler"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Nyala, Night Reveler"),
       'typeline' => clienttranslate("Character - Mage"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"I can get in anywhere, VIP list or not."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"I can get in anywhere, VIP list or not."'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [MAGE],
- 				'effectDesc' => clienttranslate('{H} If I\'m <IN_CONTACT>, you may exchange a card from your Reserve with a card from your hand.'),
- 				'supportDesc' => clienttranslate('#{D} : Exchange another card from your Reserve with a card from your hand.#'),
- 			     'supportIcon' => 'discard',
-     'forest' => 0, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MAGE],
+      'effectDesc' => clienttranslate('{H} If I\'m <IN_CONTACT>, you may exchange a card from your Reserve with a card from your hand.'),
+      'supportDesc' => clienttranslate('#{D} : Exchange another card from your Reserve with a card from your hand.#'),
+      'supportIcon' => 'discard',
+      'forest' => 0,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['mountain'],
+      'effectHand' => FT::ACTION(CHECK_CONDITION, ['condition' => 'isInContact', 'effect' =>  FT::ACTION(EXCHANGE, ['targetType' => [PERMANENT, SPELL, CHARACTER]], ['optional' => true]),]),
+      'effectSupport' => FT::ACTION(EXCHANGE, ['targetType' => [PERMANENT, SPELL, CHARACTER]])
+    ];
   }
 }
