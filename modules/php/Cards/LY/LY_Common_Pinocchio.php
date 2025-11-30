25c4ab511b52589a24344477bb4d8987552be9a9
Parn158 <vincent.toper@gmail.com>
2025-11-30 22:27:14 +0100

    Pinocchio

--- modules/php/Cards/LY/LY_Common_Pinocchio.php
+++ modules/php/Cards/LY/LY_Common_Pinocchio.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Common_Pinocchio extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_86_C',
-            'asset'  => 'ALT_DUSTER_B_LY_86_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_86_C',
+      'asset'  => 'ALT_DUSTER_B_LY_86_C',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Pinocchio"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Pinocchio"),
       'typeline' => clienttranslate("Character - Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('After that, he\'ll sleep like a log, and that\'s no lie.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('After that, he\'ll sleep like a log, and that\'s no lie.'),
       'artist' => "Victor Canton",
-			'extension'=>'SDU',
-   'subtypes'  => [ROBOT],
-     'forest' => 0, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 1, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ROBOT],
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 1,
+    ];
   }
 }
--- modules/php/Cards/LY/LY_Rare_Pinocchio.php
+++ modules/php/Cards/LY/LY_Rare_Pinocchio.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_Pinocchio extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_86_R1',
-            'asset'  => 'ALT_DUSTER_B_LY_86_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_86_R1',
+      'asset'  => 'ALT_DUSTER_B_LY_86_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Pinocchio"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Pinocchio"),
       'typeline' => clienttranslate("Character - Robot"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('After that, he\'ll sleep like a log, and that\'s no lie.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('After that, he\'ll sleep like a log, and that\'s no lie.'),
       'artist' => "Victor Canton",
-			'extension'=>'SDU',
-   'subtypes'  => [ROBOT],
- 				'effectDesc' => clienttranslate('#{R} If there are three or more base statistics of 0 among Characters you control, draw a card.#'),
-     'forest' => 0, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ROBOT],
+      'effectDesc' => clienttranslate('#{R} If there are three or more base statistics of 0 among Characters you control, draw a card.#'),
+      'forest' => 0,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costReserve'],
+      'effectReserve' => FT::ACTION(CHECK_CONDITION, ['condition' => 'has3WithZeroStat', 'effect' => FT::ACTION(DRAW, ['players' => ME])])
+    ];
   }
 }
