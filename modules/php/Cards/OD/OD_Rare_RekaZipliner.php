a8257a2dc208ca7c512bfa7f5af71e1e9bc8a2c6
Parn158 <vincent.toper@gmail.com>
2025-11-30 22:44:44 +0100

    Reka Zipliner

--- modules/php/Cards/OD/OD_Rare_RekaZipliner.php
+++ modules/php/Cards/OD/OD_Rare_RekaZipliner.php
@@ -1,31 +1,43 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_RekaZipliner extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_88_R2',
-            'asset'  => 'ALT_DUSTER_B_LY_88_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_88_R2',
+      'asset'  => 'ALT_DUSTER_B_LY_88_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Zipliner"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Zipliner"),
       'typeline' => clienttranslate("Character - Adventurer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The shortest distance between two points is a straight line.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The shortest distance between two points is a straight line.'),
       'artist' => "Victor Canton",
-			'extension'=>'SDU',
-   'subtypes'  => [ADVENTURER],
- 				'effectDesc' => clienttranslate('#{H} If I\'m in {O}, create a <MANASEED> token in your Landmarks.#'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-     'changedStats' => ['forest','mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ADVENTURER],
+      'effectDesc' => clienttranslate('#{H} If I\'m in {O}, create a <MANASEED> token in your Landmarks.#'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 0,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['forest', 'mountain'],
+      'effectHand' => FT::ACTION(CHECK_CONDITION, [
+        'condition' => 'isInBiome:ocean',
+        'description' => clienttranslate('Create a Manaseed'),
+        'effect' => FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+      ]),
+    ];
   }
 }
