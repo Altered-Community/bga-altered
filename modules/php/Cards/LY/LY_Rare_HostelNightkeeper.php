2d50bd1856dec2269dd68e514641ba414befdb35
Parn158 <vincent.toper@gmail.com>
2025-12-03 22:17:37 +0100

    Hostel Nightkeeper

--- modules/php/Cards/LY/LY_Rare_HostelNightkeeper.php
+++ modules/php/Cards/LY/LY_Rare_HostelNightkeeper.php
@@ -1,31 +1,51 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_HostelNightkeeper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_95_R1',
-            'asset'  => 'ALT_DUSTER_B_LY_95_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_95_R1',
+      'asset'  => 'ALT_DUSTER_B_LY_95_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Hostel Nightkeeper"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Hostel Nightkeeper"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Floor -2, room 217. Welcome to the Underlook Hostel."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Floor -2, room 217. Welcome to the Underlook Hostel."'),
       'artist' => "Anh Tung",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{H} You may target a Character, it gains <ASLEEP>. #If you control it,# create a <MANASEED> token in your Landmarks.'),
-     'forest' => 4, 
-     'mountain' => 2, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 3, 
-     'changedStats' => ['mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{H} You may target a Character, it gains <ASLEEP>. #If you control it,# create a <MANASEED> token in your Landmarks.'),
+      'forest' => 4,
+      'mountain' => 2,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 3,
+      'changedStats' => ['mountain'],
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER],
+        'upTo' => true,
+        'effect' => FT::SEQ(
+          FT::GAIN(EFFECT, ASLEEP),
+          FT::ACTION(CHECK_CONDITION, [
+            'condition' => 'hasSameOwner',
+            'effect' =>
+            FT::ACTION(INVOKE_TOKEN, [
+              'pId' => 'source',
+              'tokenType' => 'NE_Common_Manaseed',
+              'targetLocation' => ['discardedSource'],
+              'forcedLocation' => LANDMARK
+            ]),
+          ])
+        )
+      ]),
+    ];
   }
 }
--- modules/php/Cards/MU/MU_Rare_HostelNightkeeper.php
+++ modules/php/Cards/MU/MU_Rare_HostelNightkeeper.php
@@ -1,30 +1,46 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_HostelNightkeeper extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_LY_95_R2',
-            'asset'  => 'ALT_DUSTER_B_LY_95_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_LY_95_R2',
+      'asset'  => 'ALT_DUSTER_B_LY_95_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Hostel Nightkeeper"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Hostel Nightkeeper"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Floor -2, room 217. Welcome to the Underlook Hostel."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Floor -2, room 217. Welcome to the Underlook Hostel."'),
       'artist' => "Anh Tung",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{H} You may target a Character. If you do, it gains <ASLEEP>, then create a <MANASEED> token in its controller\'s Landmarks.'),
-     'forest' => 4, 
-     'mountain' => 0, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{H} You may target a Character. If you do, it gains <ASLEEP>, then create a <MANASEED> token in its controller\'s Landmarks.'),
+      'forest' => 4,
+      'mountain' => 0,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER],
+        'upTo' => true,
+        'effect' => FT::SEQ(
+          FT::GAIN(EFFECT, ASLEEP),
+          FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'NE_Common_Manaseed',
+            'targetLocation' => ['discardedSource'],
+            'forcedLocation' => LANDMARK
+          ]),
+        )
+      ]),
+    ];
   }
 }
