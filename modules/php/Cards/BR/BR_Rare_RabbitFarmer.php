da4b45b001a3cc63598d6510d95ed98bc06efdfc
Parn158 <vincent.toper@gmail.com>
2025-12-07 23:30:41 +0100

    Rabbit Farmer

--- modules/php/Cards/BR/BR_Rare_RabbitFarmer.php
+++ modules/php/Cards/BR/BR_Rare_RabbitFarmer.php
@@ -1,31 +1,48 @@
 <?php
+
 namespace ALT\Cards\BR;
+
 use ALT\Helpers\FT;
 
 class BR_Rare_RabbitFarmer extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_MU_86_R2',
-            'asset'  => 'ALT_DUSTER_B_MU_86_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_MU_86_R2',
+      'asset'  => 'ALT_DUSTER_B_MU_86_R',
 
-    	'faction'  => FACTION_BR,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Rabbit Farmer"),
+      'faction'  => FACTION_BR,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Rabbit Farmer"),
       'typeline' => clienttranslate("Character - Animal Druid"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The pleasure of giving, the joy of receiving.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The pleasure of giving, the joy of receiving.'),
       'artist' => "Ba Vo",
-			'extension'=>'SDU',
-   'subtypes'  => [ANIMAL,DRUID],
- 				'effectDesc' => clienttranslate('When you make a <GIFT> — If I have no boosts, I gain 1 boost. (A Gift is when on your turn, an opponent draws a card, Resupplies or receives a token.)'),
-     'forest' => 1, 
-     'mountain' => 1, 
-     'ocean' => 0, 
-     'costHand' => 1, 
-     'costReserve' => 1, 
-     'changedStats' => ['forest','mountain','ocean','costHand','costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ANIMAL, DRUID],
+      'effectDesc' => clienttranslate('When you make a <GIFT> — If I have no boosts, I gain 1 boost. (A Gift is when on your turn, an opponent draws a card, Resupplies or receives a token.)'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 0,
+      'costHand' => 1,
+      'costReserve' => 1,
+      'changedStats' => ['forest', 'mountain', 'ocean', 'costHand', 'costReserve'],
+      'effectPassive' => [
+        'InvokeToken' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMeInvoke'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ],
+        'Draw' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ],
+        'Resupply' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ]
+      ]
+    ];
   }
 }
--- modules/php/Cards/MU/MU_Common_RabbitFarmer.php
+++ modules/php/Cards/MU/MU_Common_RabbitFarmer.php
@@ -1,30 +1,47 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Common_RabbitFarmer extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_MU_86_C',
-            'asset'  => 'ALT_DUSTER_B_MU_86_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_MU_86_C',
+      'asset'  => 'ALT_DUSTER_B_MU_86_C',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Rabbit Farmer"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Rabbit Farmer"),
       'typeline' => clienttranslate("Character - Animal Druid"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The pleasure of giving, the joy of receiving.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The pleasure of giving, the joy of receiving.'),
       'artist' => "Ba Vo",
-			'extension'=>'SDU',
-   'subtypes'  => [ANIMAL,DRUID],
- 				'effectDesc' => clienttranslate('When you make a <GIFT> — If I have no boosts, I gain 1 boost. (A Gift is when on your turn, an opponent draws a card, Resupplies or receives a token.)'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 1, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ANIMAL, DRUID],
+      'effectDesc' => clienttranslate('When you make a <GIFT> — If I have no boosts, I gain 1 boost. (A Gift is when on your turn, an opponent draws a card, Resupplies or receives a token.)'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPassive' => [
+        'InvokeToken' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMeInvoke'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ],
+        'Draw' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ],
+        'Resupply' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::GAIN(ME, BOOST, 1, 1)
+        ]
+      ]
+    ];
   }
 }
