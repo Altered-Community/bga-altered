7a67ae47e64d95ddf0bcb56d9fad63e3496f8df6
Parn158 <vincent.toper@gmail.com>
2025-12-07 23:38:47 +0100

    Reka Tradesman

--- modules/php/Cards/OD/OD_Rare_RekaTradesman.php
+++ modules/php/Cards/OD/OD_Rare_RekaTradesman.php
@@ -1,31 +1,43 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_RekaTradesman extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_MU_87_R2',
-            'asset'  => 'ALT_DUSTER_B_MU_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_MU_87_R2',
+      'asset'  => 'ALT_DUSTER_B_MU_87_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Tradesman"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Tradesman"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Here\'s one for the road."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Here\'s one for the road."'),
       'artist' => "Jefrey Yonathan",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{R} Put the top card of your deck #in your Mana zone# (as an exhausted Mana Orb), then create a <MANASEED> token in target opponent\'s Landmarks.'),
-     'forest' => 0, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 3, 
-     'changedStats' => ['costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{R} Put the top card of your deck #in your Mana zone# (as an exhausted Mana Orb), then create a <MANASEED> token in target opponent\'s Landmarks.'),
+      'forest' => 0,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 3,
+      'changedStats' => ['costReserve'],
+      'effectReserve' => FT::SEQ(
+        FT::ACTION(DRAW_MANA, []),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+          'targetPlayer' => OPPONENT
+        ]),
+      )
+    ];
   }
 }
