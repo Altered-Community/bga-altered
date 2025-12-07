9b94693cbc5ce3b6858834c7062f5538a283f5cc
Parn158 <vincent.toper@gmail.com>
2025-12-07 23:23:07 +0100

    Charitable Reka

--- modules/php/Cards/MU/MU_Rare_CharitableReka.php
+++ modules/php/Cards/MU/MU_Rare_CharitableReka.php
@@ -1,30 +1,46 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_CharitableReka extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_MU_89_R1',
-            'asset'  => 'ALT_DUSTER_B_MU_89_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_MU_89_R1',
+      'asset'  => 'ALT_DUSTER_B_MU_89_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Charitable Reka"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Charitable Reka"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Opulence is a state of grace that forever teeters on the precipice.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Opulence is a state of grace that forever teeters on the precipice.'),
       'artist' => "Ba Vo",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('#{J} Create a <MANASEED> token in each player\'s Landmarks.#'),
-     'forest' => 3, 
-     'mountain' => 0, 
-     'ocean' => 3, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('#{J} Create a <MANASEED> token in each player\'s Landmarks.#'),
+      'forest' => 3,
+      'mountain' => 0,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+          'targetPlayer' => OPPONENT
+        ]),
+      )
+    ];
   }
 }
