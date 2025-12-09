56253e2a74c19e321ab51b163bc84bb8e883b5ea
Parn158 <vincent.toper@gmail.com>
2025-12-09 23:44:36 +0100

    Ploutos

--- modules/php/Cards/MU/MU_Exalted_PloutosRekaHexarch.php
+++ modules/php/Cards/MU/MU_Exalted_PloutosRekaHexarch.php
@@ -1,30 +1,48 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Exalted_PloutosRekaHexarch extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_MU_96_E',
-            'asset'  => 'ALT_DUSTER_B_MU_96_E',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_MU_96_E',
+      'asset'  => 'ALT_DUSTER_B_MU_96_E',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_EXALTED,
-    	'name'  => clienttranslate("Ploutos, Reka Hexarch"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_EXALTED,
+      'name'  => clienttranslate("Ploutos, Reka Hexarch"),
       'typeline' => clienttranslate("Character - Druid Noble"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"My worker bees will increase our harvests!"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"My worker bees will increase our harvests!"'),
       'artist' => "Kevin Sidharta",
-			'extension'=>'SDU',
-   'subtypes'  => [DRUID,NOBLE],
- 				'effectDesc' => clienttranslate('{J} Target player <EXHAUSTED_RESUPPLIES>.  When you make a <GIFT> — Target Character gains <ANCHORED>.'),
-     'forest' => 5, 
-     'mountain' => 5, 
-     'ocean' => 5, 
-     'costHand' => 7, 
-     'costReserve' => 7, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [DRUID, NOBLE],
+      'effectDesc' => clienttranslate('{J} Target player <EXHAUSTED_RESUPPLIES>.  When you make a <GIFT> — Target Character gains <ANCHORED>.'),
+      'forest' => 5,
+      'mountain' => 5,
+      'ocean' => 5,
+      'costHand' => 7,
+      'costReserve' => 7,
+      'effectPlayed' => FT::ACTION(TARGET_PLAYER, ['opponentsOnly' => false, 'effect' => FT::ACTION(RESUPPLY, [])]),
+      'effectPassive' => [
+        'InvokeToken' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMeInvoke'],
+          'output' => FT::ACTION(TARGET, ['effect' => FT::GAIN($this, ANCHORED)]),
+        ],
+        'Draw' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::ACTION(TARGET, ['effect' => FT::GAIN($this, ANCHORED)]),
+        ],
+        'Resupply' => [
+          'conditions' => ['isMyTurn', 'isNotFirstTurn', 'isAfternoon', 'isNotMe'],
+          'output' => FT::ACTION(TARGET, ['effect' => FT::GAIN($this, ANCHORED)]),
+        ]
+      ]
+    ];
   }
 }
