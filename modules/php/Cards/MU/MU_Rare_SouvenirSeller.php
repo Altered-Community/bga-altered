91ca8d9a04dec84cf0330756dd8448f902e2c769
Parn158 <vincent.toper@gmail.com>
2025-12-11 15:47:42 +0100

    Souvenir Seller

--- modules/php/Cards/MU/MU_Rare_SouvenirSeller.php
+++ modules/php/Cards/MU/MU_Rare_SouvenirSeller.php
@@ -1,30 +1,37 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_SouvenirSeller extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_87_R2',
-            'asset'  => 'ALT_DUSTER_B_OR_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_87_R2',
+      'asset'  => 'ALT_DUSTER_B_OR_87_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Souvenir Seller"),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Souvenir Seller"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Souvenirs are memories incarnate. Treasure them."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Souvenirs are memories incarnate. Treasure them."'),
       'artist' => "Zero Wen",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{J} You may give me 1 boost. If you do, target opponent <RESUPPLIES>. (They put the top card of their deck in Reserve.)'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{J} You may give me 1 boost. If you do, target opponent <RESUPPLIES>. (They put the top card of their deck in Reserve.)'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::SEQ_OPTIONAL(
+        FT::GAIN(ME, BOOST),
+        FT::ACTION(RESUPPLY, ['player' => 'nextPlayer'])
+      )
+    ];
   }
 }
--- modules/php/Cards/OD/OD_Rare_SouvenirSeller.php
+++ modules/php/Cards/OD/OD_Rare_SouvenirSeller.php
@@ -1,31 +1,42 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_SouvenirSeller extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_87_R1',
-            'asset'  => 'ALT_DUSTER_B_OR_87_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_87_R1',
+      'asset'  => 'ALT_DUSTER_B_OR_87_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Souvenir Seller"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Souvenir Seller"),
       'typeline' => clienttranslate("Character - Merchant"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Souvenirs are memories incarnate. Treasure them."'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Souvenirs are memories incarnate. Treasure them."'),
       'artist' => "Zero Wen",
-			'extension'=>'SDU',
-   'subtypes'  => [MERCHANT],
- 				'effectDesc' => clienttranslate('{J} You may #create a <MANASEED> token in your Landmarks.# If you do, target opponent <RESUPPLIES>.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['forest','mountain','ocean','costHand','costReserve'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [MERCHANT],
+      'effectDesc' => clienttranslate('{J} You may #create a <MANASEED> token in your Landmarks.# If you do, target opponent <RESUPPLIES>.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['forest', 'mountain', 'ocean', 'costHand', 'costReserve'],
+      'effectPlayed' => FT::SEQ_OPTIONAL(
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'tokenType' => 'NE_Common_Manaseed',
+          'targetLocation' => [LANDMARK],
+        ]),
+        FT::ACTION(RESUPPLY, ['player' => 'nextPlayer'])
+      )
+    ];
   }
 }
