c3bb2d6edcd082c753c8a8c37cca2bb151574e1a
Parn158 <vincent.toper@gmail.com>
2025-12-11 16:41:17 +0100

    Sullivan

--- modules/php/Cards/OD/OD_Common_LouisSullivan.php
+++ modules/php/Cards/OD/OD_Common_LouisSullivan.php
@@ -1,30 +1,34 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Common_LouisSullivan extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_86_C',
-            'asset'  => 'ALT_DUSTER_B_OR_86_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_86_C',
+      'asset'  => 'ALT_DUSTER_B_OR_86_C',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Louis Sullivan"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Louis Sullivan"),
       'typeline' => clienttranslate("Character - Engineer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Form ever follows function. This is the law." — Louis Sullivan'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Form ever follows function. This is the law." — Louis Sullivan'),
       'artist' => "Kevin Sidharta",
-			'extension'=>'SDU',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('{H} If there are two or more cards in your Landmarks, draw a card.'),
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ENGINEER],
+      'effectDesc' => clienttranslate('{H} If there are two or more cards in your Landmarks, draw a card.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectHand' => FT::ACTION(CHECK_CONDITION, ['condition' => 'hasControl:landmark:2', 'effect' => FT::ACTION(DRAW, ['players' => ME])])
+    ];
   }
 }
--- modules/php/Cards/OD/OD_Rare_LouisSullivan.php
+++ modules/php/Cards/OD/OD_Rare_LouisSullivan.php
@@ -1,31 +1,35 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_LouisSullivan extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_86_R1',
-            'asset'  => 'ALT_DUSTER_B_OR_86_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_86_R1',
+      'asset'  => 'ALT_DUSTER_B_OR_86_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Louis Sullivan"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Louis Sullivan"),
       'typeline' => clienttranslate("Character - Engineer"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Form ever follows function. This is the law." — Louis Sullivan'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Form ever follows function. This is the law." — Louis Sullivan'),
       'artist' => "Kevin Sidharta",
-			'extension'=>'SDU',
-   'subtypes'  => [ENGINEER],
- 				'effectDesc' => clienttranslate('#{J}# If there are #three or more# cards in your Landmarks, draw a card.'),
-     'forest' => 2, 
-     'mountain' => 3, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 3, 
-     'changedStats' => ['forest'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [ENGINEER],
+      'effectDesc' => clienttranslate('#{J}# If there are #three or more# cards in your Landmarks, draw a card.'),
+      'forest' => 2,
+      'mountain' => 3,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 3,
+      'changedStats' => ['forest'],
+      'effectPlayed' => FT::ACTION(CHECK_CONDITION, ['condition' => 'hasControl:landmark:3', 'effect' => FT::ACTION(DRAW, ['players' => ME])])
+    ];
   }
 }
