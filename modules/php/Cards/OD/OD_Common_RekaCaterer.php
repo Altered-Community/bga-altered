a864fe370377a9eeb1c74e179e96ec81d4c2c121
Parn158 <vincent.toper@gmail.com>
2025-12-11 16:53:55 +0100

    Reka Caterer

--- modules/php/Cards/OD/OD_Common_RekaCaterer.php
+++ modules/php/Cards/OD/OD_Common_RekaCaterer.php
@@ -1,30 +1,39 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Common_RekaCaterer extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_94_C',
-            'asset'  => 'ALT_DUSTER_B_OR_94_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_94_C',
+      'asset'  => 'ALT_DUSTER_B_OR_94_C',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Reka Caterer"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Reka Caterer"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Happiness is always within reach, if you know how to savor it. Care for a smoothie?"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Happiness is always within reach, if you know how to savor it. Care for a smoothie?"'),
       'artist' => "Damian Audino",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('{J} Create a <MANASEED> token in your Landmarks. (It\'s a Permanent token with \"{T}, Sacrifice me: Ready a Mana Orb.\")'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('{J} Create a <MANASEED> token in your Landmarks. (It\'s a Permanent token with \"{T}, Sacrifice me: Ready a Mana Orb.\")'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'effectPlayed' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'NE_Common_Manaseed',
+        'targetLocation' => [LANDMARK],
+      ]),
+
+    ];
   }
 }
--- modules/php/Cards/OD/OD_Rare_RekaCaterer.php
+++ modules/php/Cards/OD/OD_Rare_RekaCaterer.php
@@ -1,33 +1,45 @@
 <?php
+
 namespace ALT\Cards\OD;
+
 use ALT\Helpers\FT;
 
 class OD_Rare_RekaCaterer extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_OR_94_R1',
-            'asset'  => 'ALT_DUSTER_B_OR_94_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_OR_94_R1',
+      'asset'  => 'ALT_DUSTER_B_OR_94_R',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Caterer"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Caterer"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('"Happiness is always within reach, if you know how to savor it. Care for a smoothie?"'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('"Happiness is always within reach, if you know how to savor it. Care for a smoothie?"'),
       'artist' => "Damian Audino",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('{J} Create a <MANASEED> token in your Landmarks.'),
- 				'supportDesc' => clienttranslate('#{D} : Create a <ORDIS_RECRUIT> Soldier token in target Expedition.#'),
- 			     'supportIcon' => 'discard',
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 4, 
-     'costHand' => 4, 
-     'costReserve' => 4, 
-     'changedStats' => ['forest','mountain'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('{J} Create a <MANASEED> token in your Landmarks.'),
+      'supportDesc' => clienttranslate('#{D} : Create a <ORDIS_RECRUIT> Soldier token in target Expedition.#'),
+      'supportIcon' => 'discard',
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 4,
+      'costHand' => 4,
+      'costReserve' => 4,
+      'changedStats' => ['forest', 'mountain'],
+      'effectPlayed' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'NE_Common_Manaseed',
+        'targetLocation' => [LANDMARK],
+      ]),
+      'effectSupport' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'OD_Common_OrdisRecruit',
+      ]),
+    ];
   }
 }
