b44e7836258541b8da9a8f4217350f03469ce8af
Parn158 <vincent.toper@gmail.com>
2025-12-18 16:12:36 +0100

    Reka Supporter

--- modules/php/Cards/YZ/YZ_Common_RekaSupporter.php
+++ modules/php/Cards/YZ/YZ_Common_RekaSupporter.php
@@ -1,30 +1,38 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_RekaSupporter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_91_C',
-            'asset'  => 'ALT_DUSTER_B_YZ_91_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_91_C',
+      'asset'  => 'ALT_DUSTER_B_YZ_91_C',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Reka Supporter"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Reka Supporter"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The energy of the crowd is what drives the greatest victories.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The energy of the crowd is what drives the greatest victories.'),
       'artist' => "Zero Wen",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('{H} Create a <MANASEED> token in your Landmarks. (It\'s a Permanent with \"{T}, Sacrifice me: Ready a Mana orb.\")'),
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 2, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('{H} Create a <MANASEED> token in your Landmarks. (It\'s a Permanent with \"{T}, Sacrifice me: Ready a Mana orb.\")'),
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 2,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'NE_Common_Manaseed',
+        'targetLocation' => [LANDMARK],
+      ]),
+    ];
   }
 }
--- modules/php/Cards/YZ/YZ_Rare_RekaSupporter.php
+++ modules/php/Cards/YZ/YZ_Rare_RekaSupporter.php
@@ -1,33 +1,42 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_RekaSupporter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_91_R1',
-            'asset'  => 'ALT_DUSTER_B_YZ_91_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_91_R1',
+      'asset'  => 'ALT_DUSTER_B_YZ_91_R',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Reka Supporter"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Reka Supporter"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('The energy of the crowd is what drives the greatest victories.'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('The energy of the crowd is what drives the greatest victories.'),
       'artist' => "Zero Wen",
-			'extension'=>'SDU',
-   'subtypes'  => [CITIZEN],
- 				'effectDesc' => clienttranslate('{H} Create a <MANASEED> token in your Landmarks.'),
- 				'supportDesc' => clienttranslate('#{D} : <AFTER_YOU>.#'),
- 			     'supportIcon' => 'discard',
-     'forest' => 2, 
-     'mountain' => 2, 
-     'ocean' => 3, 
-     'costHand' => 3, 
-     'costReserve' => 2, 
-     'changedStats' => ['ocean'], 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [CITIZEN],
+      'effectDesc' => clienttranslate('{H} Create a <MANASEED> token in your Landmarks.'),
+      'supportDesc' => clienttranslate('#{D} : <AFTER_YOU>.#'),
+      'supportIcon' => 'discard',
+      'forest' => 2,
+      'mountain' => 2,
+      'ocean' => 3,
+      'costHand' => 3,
+      'costReserve' => 2,
+      'changedStats' => ['ocean'],
+      'effectHand' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'NE_Common_Manaseed',
+        'targetLocation' => [LANDMARK],
+      ]),
+      'effectSupport' => FT::ACTION(AFTER_YOU, [])
+    ];
   }
 }
