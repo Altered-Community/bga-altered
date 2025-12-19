32b28b752f5eec602974bb003365962872feaa56
Parn158 <vincent.toper@gmail.com>
2025-12-19 23:38:53 +0100

    The infirmary

--- modules/php/Cards/MU/MU_Rare_TheInfirmary.php
+++ modules/php/Cards/MU/MU_Rare_TheInfirmary.php
@@ -1,27 +1,41 @@
 <?php
+
 namespace ALT\Cards\MU;
+
 use ALT\Helpers\FT;
 
 class MU_Rare_TheInfirmary extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_102_R2',
-            'asset'  => 'ALT_DUSTER_B_YZ_102_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_102_R2',
+      'asset'  => 'ALT_DUSTER_B_YZ_102_R',
 
-    	'faction'  => FACTION_MU,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("The Infirmary"),
-      'typeline' => clienttranslate("Landmark_permanent - Site"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('"I told you it was a bad idea to drink all that Sap Cola…"'),
+      'faction'  => FACTION_MU,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("The Infirmary"),
+      'typeline' => clienttranslate("Landmark Permanent - Site"),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('"I told you it was a bad idea to drink all that Sap Cola…"'),
       'artist' => "HuoMiao Studio",
-			'extension'=>'SDU',
-   'subtypes'  => [SITE,LANDMARK],
- 				'effectDesc' => clienttranslate('#{J} <RESUPPLY>.#  {T}, Exhaust a card in your Reserve: <AFTER_YOU>.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('#{J} <RESUPPLY>.#  {T}, Exhaust a card in your Reserve: <AFTER_YOU>.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectTap' => FT::ACTION(TARGET, [
+        'targetLocation' => [RESERVE],
+        'targetPlayer' => ME,
+        'isNotTapped' => true,
+        'targetType' => [PERMANENT, SPELL, CHARACTER],
+        'effect' => FT::SEQ(
+          FT::ACTION(EXHAUST, []),
+          FT::ACTION(AFTER_YOU, []),
+        )
+      ]),
+      'effectPlayed' => FT::ACTION(RESUPPLY, [])
+    ];
   }
 }
--- modules/php/Cards/YZ/YZ_Common_TheInfirmary.php
+++ modules/php/Cards/YZ/YZ_Common_TheInfirmary.php
@@ -1,27 +1,40 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_TheInfirmary extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_102_C',
-            'asset'  => 'ALT_DUSTER_B_YZ_102_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_102_C',
+      'asset'  => 'ALT_DUSTER_B_YZ_102_C',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("The Infirmary"),
-      'typeline' => clienttranslate("Landmark_permanent - Site"),
-    	'type'  => PERMANENT,
-    	'flavorText'  => clienttranslate('"I told you it was a bad idea to drink all that Sap Cola…"'),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("The Infirmary"),
+      'typeline' => clienttranslate("Landmark Permanent - Site"),
+      'type'  => PERMANENT,
+      'flavorText'  => clienttranslate('"I told you it was a bad idea to drink all that Sap Cola…"'),
       'artist' => "HuoMiao Studio",
-			'extension'=>'SDU',
-   'subtypes'  => [SITE,LANDMARK],
- 				'effectDesc' => clienttranslate('{T}, Exhaust a card in your Reserve: $<AFTER_YOU>.'),
-     'costHand' => 3, 
-     'costReserve' => 3, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('{T}, Exhaust a card in your Reserve: $<AFTER_YOU>.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectTap' => FT::ACTION(TARGET, [
+        'targetLocation' => [RESERVE],
+        'targetPlayer' => ME,
+        'isNotTapped' => true,
+        'targetType' => [PERMANENT, SPELL, CHARACTER],
+        'effect' => FT::SEQ(
+          FT::ACTION(EXHAUST, []),
+          FT::ACTION(AFTER_YOU, []),
+        )
+      ])
+    ];
   }
 }
