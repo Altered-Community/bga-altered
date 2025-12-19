1422c4d693111c7c7886340bfe3986f7d5027523
Parn158 <vincent.toper@gmail.com>
2025-12-19 17:09:21 +0100

    Sports encounter

--- modules/php/Cards/YZ/YZ_Rare_SportsEncounter.php
+++ modules/php/Cards/YZ/YZ_Rare_SportsEncounter.php
@@ -1,31 +1,39 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Rare_SportsEncounter extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_97_R1',
-            'asset'  => 'ALT_DUSTER_B_YZ_97_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_97_R1',
+      'asset'  => 'ALT_DUSTER_B_YZ_97_R',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Sports Encounter"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Sports Encounter"),
       'typeline' => clienttranslate("Spell - Boon Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('"Your victory is a victory for both our peoples!"'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('"Your victory is a victory for both our peoples!"'),
       'artist' => "Victor Canton",
-			'extension'=>'SDU',
-   'subtypes'  => [BOON,MANEUVER],
- 				'effectDesc' => clienttranslate('#$<COOLDOWN>.#  Distribute 2 boosts among any target Characters in play #or in Reserve.#'),
- 				'supportDesc' => clienttranslate('{D} : <AFTER_YOU>.'),
- 			     'supportIcon' => 'discard',
-     'costHand' => 2, 
-     'costReserve' => 1, 
-     'changedStats' => ['costReserve'], 
- 		'cooldown' => true,
-];
+      'extension' => 'SDU',
+      'subtypes'  => [BOON, MANEUVER],
+      'effectDesc' => clienttranslate('#$<COOLDOWN>.#  Distribute 2 boosts among any target Characters in play #or in Reserve.#'),
+      'supportDesc' => clienttranslate('{D} : <AFTER_YOU>.'),
+      'supportIcon' => 'discard',
+      'costHand' => 2,
+      'costReserve' => 1,
+      'changedStats' => ['costReserve'],
+      'cooldown' => true,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST])]),
+        FT::ACTION(TARGET, ['effect' => FT::ACTION(GAIN, ['type' => BOOST])]),
+      ),
+      'effectSupport' => FT::ACTION(AFTER_YOU, []),
+    ];
   }
 }
