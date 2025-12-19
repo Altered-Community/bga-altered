bc805da44e1d2987c7c6282ba40d8928f374c6a5
Parn158 <vincent.toper@gmail.com>
2025-12-19 18:37:11 +0100

    Finisher

--- modules/php/Cards/LY/LY_Rare_Finisher.php
+++ modules/php/Cards/LY/LY_Rare_Finisher.php
@@ -1,27 +1,44 @@
 <?php
+
 namespace ALT\Cards\LY;
+
 use ALT\Helpers\FT;
 
 class LY_Rare_Finisher extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_98_R2',
-            'asset'  => 'ALT_DUSTER_B_YZ_98_R',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_98_R2',
+      'asset'  => 'ALT_DUSTER_B_YZ_98_R',
 
-    	'faction'  => FACTION_LY,
-    	'rarity'  => RARITY_RARE,
-    	'name'  => clienttranslate("Finisher"),
+      'faction'  => FACTION_LY,
+      'rarity'  => RARITY_RARE,
+      'name'  => clienttranslate("Finisher"),
       'typeline' => clienttranslate("Spell - Disruption Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('"Maaaaaleros!"'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('"Maaaaaleros!"'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [DISRUPTION,MANEUVER],
- 				'effectDesc' => clienttranslate('<FLEETING>.  Choose up to three, #you may choose the same option multiple times:#  • Discard target Character.  • Discard target Permanent.  • <SABOTAGE>.'),
-     'costHand' => 7, 
-     'costReserve' => 7, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [DISRUPTION, MANEUVER],
+      'effectDesc' => clienttranslate('<FLEETING>.  Choose up to three, #you may choose the same option multiple times:#  • Discard target Character.  • Discard target Permanent.  • <SABOTAGE>.'),
+      'costHand' => 7,
+      'costReserve' => 7,
+      'effecPlayed' => FT::SEQ(
+        FT::GAIN(ME, FLEETING),
+        [
+          'type' => NODE_OR,
+          'args' => ['n' => 3, 'canReuse' => true],
+          'pId' => 'source',
+          'option' => true,
+          'childs' => [
+            FT::SABOTAGE(),
+            FT::ACTION(TARGET, ['effect' => FT::ACTION(DISCARD, [])]),
+            FT::ACTION(TARGET, ['targetType' => [CHARACTER, PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+          ]
+        ]
+      )
+    ];
   }
 }
--- modules/php/Cards/YZ/YZ_Common_Finisher.php
+++ modules/php/Cards/YZ/YZ_Common_Finisher.php
@@ -1,27 +1,44 @@
 <?php
+
 namespace ALT\Cards\YZ;
+
 use ALT\Helpers\FT;
 
 class YZ_Common_Finisher extends \ALT\Models\Card
 {
-  public function __construct($row){
-		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_DUSTER_B_YZ_98_C',
-            'asset'  => 'ALT_DUSTER_B_YZ_98_C',
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_DUSTER_B_YZ_98_C',
+      'asset'  => 'ALT_DUSTER_B_YZ_98_C',
 
-    	'faction'  => FACTION_YZ,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Finisher"),
+      'faction'  => FACTION_YZ,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Finisher"),
       'typeline' => clienttranslate("Spell - Disruption Maneuver"),
-    	'type'  => SPELL,
-    	'flavorText'  => clienttranslate('"Maaaaaleros!"'),
+      'type'  => SPELL,
+      'flavorText'  => clienttranslate('"Maaaaaleros!"'),
       'artist' => "DOBA",
-			'extension'=>'SDU',
-   'subtypes'  => [DISRUPTION,MANEUVER],
- 				'effectDesc' => clienttranslate('<FLEETING>.  Choose up to three:  • Discard target Character.  • Discard target Permanent.  • <SABOTAGE>.'),
-     'costHand' => 7, 
-     'costReserve' => 7, 
-];
+      'extension' => 'SDU',
+      'subtypes'  => [DISRUPTION, MANEUVER],
+      'effectDesc' => clienttranslate('<FLEETING>.  Choose up to three:  • Discard target Character.  • Discard target Permanent.  • <SABOTAGE>.'),
+      'costHand' => 7,
+      'costReserve' => 7,
+      'effecPlayed' => FT::SEQ(
+        FT::GAIN(ME, FLEETING),
+        [
+          'type' => NODE_OR,
+          'args' => ['n' => 3],
+          'pId' => 'source',
+          'option' => true,
+          'childs' => [
+            FT::SABOTAGE(),
+            FT::ACTION(TARGET, ['effect' => FT::ACTION(DISCARD, [])]),
+            FT::ACTION(TARGET, ['targetType' => [CHARACTER, PERMANENT], 'effect' => FT::ACTION(DISCARD, [])]),
+          ]
+        ]
+      )
+    ];
   }
 }
