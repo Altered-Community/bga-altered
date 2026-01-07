9389d8f54d3a4bf93c262f5b400891312874aa4f
Parn158 <vincent.toper@gmail.com>
2026-01-08 00:00:24 +0100

    #197056: "Tough Cost // Force te be payed"

--- modules/php/Actions/Target.php
+++ modules/php/Actions/Target.php
@@ -168,7 +168,8 @@ class Target extends \ALT\Models\Action
     return $this->getArg('upTo') ||
       count($this->getTargetableCards(Players::getActive(), true)) == 0 ||
       !$this->isDoable($player) ||
-      $this->getCtx()->getOptional();
+      $this->getCtx()->getOptional() ||
+      (count($this->getTargetableCards($player)) == count($this->getTargetCosts($player)) && $this->getTargetCosts($player) > 0);
   }
 
   public function getTargetableCards($player, $checkTough = false)
