5afffff00f8e9579a166498149508c844c87dace
Parn158 <vincent.toper@gmail.com>
2026-01-28 17:57:41 +0100

    API pass progression

--- modules/php/States/EndGameTrait.php
+++ modules/php/States/EndGameTrait.php
@@ -31,9 +31,24 @@ trait EndGameTrait
       }
     }
 
-    // $valid = self::getGenericGameInfos('push_adventure_pass', $request);
+    $valid = self::getGenericGameInfos('push_adventure_pass', $request);
+    if ($valid['success'] == 1 && isset($valid['winner_bga_adventure_pass_progress']) && !is_null($valid['winner_bga_adventure_pass_progress'])) {
+      Notifications::message(
+        clienttranslate('${player_name} increased the BGA Adventure pass to ${pass}'),
+        [
+          'player' => Players::get($request['winner']['id']),
+          'pass' => $valid['winner_bga_adventure_pass_progress']
+        ]
+      );
+    }
     // throw new \feException(print_r($valid));
     // TODO remove in alpha
+    // [success] => 1
+    // [transaction_id] => 01KG2N443JATCX4XYH55Q30PGT
+    // [winner_adventure_pass_point] => 0
+    // [winner_bga_adventure_pass_progress] => 3/15
+    // [loser_adventure_pass_point] => 
+    // [loser_bga_adventure_pass_progress] => 
     if ($this->getBgaEnvironment() == 'studio') {
       // throw new \feException(print_r(debug_print_backtrace()));
       throw new \feException('winner');
