8c231909fa37aafe0ec046c18784ad0f1e3db28b
Nyliar <eirel@hotmail.fr>
2026-04-30 13:36:50 -0400

    Added support for Yeong-Gi (Lyra Common) and Lakshmi's Owl card family

--- modules/php/Cards/BR/BR_Rare_LakshmisOwl.php
+++ modules/php/Cards/BR/BR_Rare_LakshmisOwl.php
@@ -1,45 +1,45 @@
 <?php
 
-namespace ALT\Cards\BR;
+namespace ALT\Cards\LY;
 
 use ALT\Helpers\FT;
 
-class BR_Rare_LakshmisOwl extends \ALT\Models\Card
+class LY_Rare_LakshmisOwl extends \ALT\Models\Card
 {
-  public function __construct($row)
-  {
-    parent::__construct($row);
-    $this->properties = [
-      'uid' => 'ALT_EOLE_B_LY_119_R2',
-      'asset'  => 'ALT_EOLE_B_LY_119_R',
+    public function __construct($row)
+    {
+        parent::__construct($row);
+        $this->properties = [
+            'uid' => 'ALT_EOLE_B_LY_119_R1',
+            'asset'  => 'ALT_EOLE_B_LY_119_R1',
 
-      'faction'  => FACTION_BR,
-      'rarity'  => RARITY_RARE,
-      'name'  => clienttranslate("Lakshmi's Owl"),
-      'typeline' => clienttranslate("Character - Animal Spirit"),
-      'type'  => CHARACTER,
-      'flavorText'  => clienttranslate(''),
-      'artist' => "Fahmi Fauzi",
-      'extension' => 'ROC',
-      'subtypes'  => [ANIMAL, SPIRIT],
-      'effectDesc' => clienttranslate('When #a Character gains 1 or more boosts# — If I have no boosts, #I gain 1 boost.#'),
-      'supportDesc' => clienttranslate('#{D} : Pay {1} less for the next Character you play this turn, down to a minimum of {1}.#'),
-      'supportIcon' => 'discard',
-      'forest' => 0,
-      'mountain' => 1,
-      'ocean' => 0,
-      //VTO support
-      'costHand' => 1,
-      'costReserve' => 1,
-      'effectPassive' => [
-        'Gain' => [
-          [
-            'conditions' => ['isGain:boost', 'hasNoBoost'],
-            'output' => FT::GAIN(ME, BOOST),
-          ],
-        ],
-      ],
-      'effectSupport' => FT::ACTION(SPECIAL_EFFECT, ['effect' => 'nextCharacterCostReduction1']),
-    ];
-  }
+            'faction'  => FACTION_LY,
+            'rarity'  => RARITY_RARE,
+            'name'  => clienttranslate("Lakshmi's Owl"),
+            'typeline' => clienttranslate("Character - Animal, Spirit"),
+            'type'  => CHARACTER,
+            'flavorText'  => clienttranslate('Even in the blackest night, I can see light.'),
+            'artist' => "Fahmi Fauzi",
+            'extension' => 'ROC',
+            'subtypes'  => [ANIMAL, SPIRIT],
+            'effectDesc' => clienttranslate('When you roll one or more dice — If I have no boosts, #I gain 1 boost#. $<BB>'), 
+            'supportDesc' => clienttranslate('{D} : Pay {1} less for the next Character you play this turn, down to a minimum of {1}. (Discard me from Reserve to do this.)'),
+            'forest' => 0,
+            'mountain' => 1,
+            'ocean' => 0,
+            'costHand' => 1,
+            'costReserve' => 1,
+            'supportIcon' => 'discard',
+            'effectPassive' => [
+                'RollDie' => [
+                    'conditions' => ['isMe', 'hasBoost:0:LTE', 'isInStorms'],
+                    'output' => FT::GAIN(ME, BOOST),
+                ],
+            ],
+            'effectSupport' => [
+              'action' => SPECIAL_EFFECT,
+              'args' => ['effect' => 'costReduction', 'args' => ['type' => CHARACTER, 'reduction' => 1]],
+            ],
+        ];
+    }
 }
