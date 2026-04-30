eb15f07568f811b40d57fb00db3b62f2e4737588
noobiwow <valentin.millet@efrei.net>
2026-04-30 22:22:12 +0900

    Add common, rare and oof for Thunderbird ALT_EOLE_B_OR_110, add a new condition to check moving ascended

--- modules/php/Helpers/Conditions.php
+++ modules/php/Helpers/Conditions.php
@@ -233,6 +233,26 @@ abstract class Conditions
     return false;
   }
 
+    public static function movesStormsWithAscended($card, $event)
+  {
+    $stormMoves = Globals::getStormMoves();
+    $storm = $event['expedition'] ?? $card->getLocation();
+    if (
+      !isset($stormMoves[$card->getPId()]) ||
+      $card->getPId() != $event['pId'] ||
+      !isset($stormMoves[$card->getPId()][$storm])
+    ) {
+      return false;
+    }
+
+    $move = $stormMoves[$card->getPId()][$storm];
+    if ($card->getPlayer()->isAscended($storm) && $move['moves'] >= 1) {
+      return true;
+    }
+
+    return false;
+  }
+
   public static function movesAscendedAnyExpeditions($card, $event)
   {
     $stormMoves = Globals::getStormMoves();
