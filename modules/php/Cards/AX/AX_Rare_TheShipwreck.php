077d61bf0f701a035e32c9dda773eb3163ebf65e
Nyliar <eirel@hotmail.fr>
2026-06-05 18:08:52 -0400

    Added 3 card families - Fane of Lotus Eater (AX/AX/OD) - The Shipwreck (AX/AX/LY) - Fane of Circe (AX/AX/OD)

---
+++ modules/php/Cards/AX/AX_Rare_TheShipwreck.php
@@ -0,0 +1,41 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Rare_TheShipwreck extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_145_R1',
+      'asset' => 'ALT_FUGUE_B_AX_145_R',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('The Shipwreck'),
+      'typeline' => clienttranslate('Landmark Permanent - Construction'),
+      'type' => PERMANENT,
+      'subtypes' => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('{J} I gain 3 Kelon counters. At Noon — You may spend 1 of my Kelon counters to $<RESUPPLY>. #When I\'m sacrificed — Draw a card.#'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'gainCounter', 'args' => ['counter' => 3, 'counterName' => clienttranslate('Kelon counter')]],
+      ],
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'isNoon',
+          'output' =>  FT::SEQ_OPTIONAL_MANUAL(
+            FT::ACTION(USE_COUNTER, ['consume' => 1], ['sourceId' => $this->id]),
+            FT::ACTION(RESUPPLY, [])
+          )
+        ],
+        'Discard' => [
+          'condition' => 'isSacrificed',
+          'output' => FT::ACTION(DRAW, ['players' => ME]),
+        ],
+      ],
+    ];
+  }
+}
---
+++ modules/php/Cards/OD/OD_Rare_FaneofCirce.php
@@ -0,0 +1,37 @@
+<?php
+namespace ALT\Cards\OD;
+use ALT\Helpers\FT;
+
+class OD_Rare_FaneofCirce extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_147_R2',
+      'asset' => 'ALT_FUGUE_B_AX_147_R',
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Fane of Circe'),
+      'typeline' => clienttranslate('Landmark Permanent - Construction'),
+      'type' => PERMANENT,
+      'artist' => 'Jean-Baptiste Andrier',
+      'extension' => 'NEJ',
+      'subtypes' => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('When you play a Character with Base Cost {3} or more — You may exhaust me to give it 1 boost.'),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(RESUPPLY, []),
+      'effectPassive' => [
+        'ChooseAssignment' => [
+          'conditions' => ['notTapped', 'isCardPlayed:character', 'cardPlayedCostCheck:2'],
+          'output' => FT::SEQ_OPTIONAL(
+            FT::ACTION(TAP, []),
+            FT::GAIN(EFFECT, BOOST)
+          )
+        ],
+      ]
+    ];
+  }
+}
