8c90c37161632dc4a6bcc2837265c1fd70ff6f03
Nyliar <eirel@hotmail.fr>
2026-06-15 13:10:30 -0400

    Added NEJ OD cards families - Eyota / Echo hero - Majestic Eagle (OD/OD/YZ) - Paris (OD/OD/YZ)

---
+++ modules/php/Cards/OD/OD_Rare_MajesticEagle.php
@@ -0,0 +1,41 @@
+<?php
+namespace ALT\Cards\OD;
+use ALT\Helpers\FT;
+
+class OD_Rare_MajesticEagle extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_OR_131_R1',
+      'asset' => 'ALT_FUGUE_B_OR_131_R',
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Majestic Eagle'),
+      'typeline' => clienttranslate('Character - Soldier Animal'),
+      'type' => CHARACTER,
+      'flavorText' => clienttranslate('It\'s time to spread your wings, take flight, and sail towards the horizon.'),
+      'artist' => 'Kevin Sidharta',
+      'extension' => 'NEJ',
+      'subtypes' => [SOLDIER, ANIMAL],
+      'effectDesc' => clienttranslate('#When I go to your Reserve from your Expeditions — Create an Ordis Recruit 1/1/1 Soldier token in my Expedition.#'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 0,
+      'costHand' => 1,
+      'costReserve' => 1,
+      'effectPassive' => [
+        'LeaveExpedition' => [
+          'pId' => CONTROLLER,
+          'condition' => 'isToReserve',
+          'output' => FT::ACTION(INVOKE_TOKEN, [
+            'pId' => 'source',
+            'tokenType' => 'OD_Common_OrdisRecruit',
+            'targetLocation' => ['source'],
+          ]),
+        ],
+      ]
+    ];
+  }
+}
---
+++ modules/php/Cards/OD/OD_Rare_Paris.php
@@ -0,0 +1,35 @@
+<?php
+namespace ALT\Cards\OD;
+use ALT\Helpers\FT;
+
+class OD_Rare_Paris extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_OR_133_R1',
+      'asset' => 'ALT_FUGUE_B_OR_133_R',
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Paris'),
+      'typeline' => clienttranslate('Character - Soldier'),
+      'type' => CHARACTER,
+      'artist' => 'Justice Wong',
+      'extension' => 'NEJ',
+      'subtypes' => [SOLDIER],
+      'effectDesc' => clienttranslate('{R} Create an Ordis Recruit 1/1/1 Soldier token in my Expedition.'),
+      'forest' => 2,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 1,
+      'costReserve' => 2,
+      'changedStats' => ['forest'],
+      'effectReserve' => FT::ACTION(INVOKE_TOKEN, [
+        'pId' => 'source',
+        'tokenType' => 'OD_Common_OrdisRecruit',
+        'targetLocation' => ['source'],
+      ]),
+    ];
+  }
+}
