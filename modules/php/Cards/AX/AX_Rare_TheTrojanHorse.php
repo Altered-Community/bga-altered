3266a5c74cb9834ccf57f07e072fd647e8b892fb
Nyliar <eirel@hotmail.fr>
2026-06-15 13:22:54 -0400

    Added NEJ OD card families - Helen of Troy (OD/OD/YZ) -Menelaus Bringer of War (OD/OD/MU) -Trojan Horse (OD/OD/AX)

---
+++ modules/php/Cards/AX/AX_Rare_TheTrojanHorse.php
@@ -0,0 +1,41 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Rare_TheTrojanHorse extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_OR_145_R2',
+      'asset' => 'ALT_FUGUE_B_OR_145_R',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('The Trojan Horse'),
+      'typeline' => clienttranslate('Character - Construction'),
+      'type' => CHARACTER,
+      'artist' => 'Saeed Jalabi',
+      'extension' => 'NEJ',
+      'subtypes' => [CONSTRUCTION],
+      'effectDesc' => clienttranslate('Defender.  {J} I #defect#.  When I leave the Expedition zone — My owner creates an Ordis Recruit 1/1/1 Soldier token in each of their Expeditions.'),
+      'forest' => 3,
+      'mountain' => 3,
+      'ocean' => 3,
+      'costHand' => 5,
+      'costReserve' => 5,
+      'defender' => true,
+      'effectPlayed' => FT::SEQ(
+        FT::ACTION(SPECIAL_EFFECT,
+          ['effect' => 'defect']
+        ),
+        FT::ACTION(INVOKE_TOKEN, [
+          'pId' => 'source',
+          'targetPlayer' => 'owner',
+          'tokenType' => 'OD_Common_OrdisRecruit',
+          'targetLocation' => [STORM_RIGHT, STORM_LEFT],
+        ]),
+      ),
+    ];
+  }
+}
