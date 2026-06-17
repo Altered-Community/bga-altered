440547e6e71e05f94fa2e49fd6517b4ab01fc540
Nyliar <eirel@hotmail.fr>
2026-06-17 09:23:12 -0400

    Added NEJ Exalted Cards Hermes and Treacherous Loop

---
+++ modules/php/Cards/YZ/YZ_Exalted_TreacherousLoop.php
@@ -0,0 +1,30 @@
+<?php
+namespace ALT\Cards\YZ;
+use ALT\Helpers\FT;
+
+class YZ_Exalted_TreacherousLoop extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_YZ_147_E',
+      'asset' => 'ALT_FUGUE_B_YZ_147_E',
+      'faction' => FACTION_YZ,
+      'rarity' => RARITY_EXALTED,
+      'name' => clienttranslate('Treacherous Loop'),
+      'typeline' => clienttranslate('Spell - Maneuver'),
+      'type' => SPELL,
+      'artist' => 'Andy Joffrit',
+      'extension' => 'NEJ',
+      'subtypes' => [MANEUVER],
+      'effectDesc' => clienttranslate('$<FLEETING>. Ready up to eight Mana Orbs.'),
+      'costHand' => 7,
+      'costReserve' => 7,
+      'effectPlayed' => FT::SEQ(
+        FT::GAIN(ME, FLEETING),
+        FT::ACTION(READY, ['upTo' => 8]),
+      ),
+    ];
+  }
+}
