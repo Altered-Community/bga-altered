41dd7b88cff75cc0bea33c57b7e331a288a15e18
Nyliar <eirel@hotmail.fr>
2026-08-13 15:29:35 -0400

    Added Fane of Lotus Eaters C/R1/R2

---
+++ modules/php/Cards/AX/AX_Common_FaneofLotusEaters.php
@@ -0,0 +1,43 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Common_FaneofLotusEaters extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_146_C',
+      'asset' => 'ALT_FUGUE_B_AX_146_C',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Fane of Lotus-Eaters'),
+      'typeline' => clienttranslate('Landmark Permanent - Site'),
+      'type' => PERMANENT,
+      'artist' => 'DOBA',
+      'extension' => 'NEJ',
+      'subtypes' => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('{J} Sabotage (Discard up to one card from a Reserve).  At Noon — Target Character in your Reserve gains 1 boost.'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+      'effectPassive' => [
+        'Noon' => [
+          'condition' => 'isNoon',
+          'output' => FT::ACTION(TARGET, [
+            'targetPlayer' => ME,
+            'targetType' => [CHARACTER],
+            'targetLocation' => [RESERVE],
+            'effect' => FT::ACTION(GAIN, ['type' => BOOST]),
+          ]),
+        ],
+      ],
+    ];
+  }
+}
---
+++ modules/php/Cards/AX/AX_Rare_FaneofLotusEaters.php
@@ -0,0 +1,44 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Rare_FaneofLotusEaters extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_146_R1',
+      'asset' => 'ALT_FUGUE_B_AX_146_R',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Fane of Lotus-Eaters'),
+      'typeline' => clienttranslate('Landmark Permanent - Site'),
+      'type' => PERMANENT,
+      'artist' => 'DOBA',
+      'extension' => 'NEJ',
+      'subtypes' => [SITE, LANDMARK],
+      'effectDesc' => clienttranslate('{J} Sabotage.  At Noon — #If you\'re first player#, target Character in your Reserve gains 1 boost.'),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'changedStats' => ['costHand', 'costReserve'],
+      'effectPlayed' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, SPELL, TOKEN, PERMANENT],
+        'targetLocation' => [RESERVE],
+        'upTo' => true,
+        'effect' => FT::ACTION(DISCARD, []),
+      ]),
+      'effectPassive' => [
+        'Noon' => [
+          'conditions' => ['isNoon', 'isFirstPlayer'],
+          'output' => FT::ACTION(TARGET, [
+            'targetPlayer' => ME,
+            'targetType' => [CHARACTER],
+            'targetLocation' => [RESERVE],
+            'effect' => FT::ACTION(GAIN, ['type' => BOOST]),
+          ]),
+        ],
+      ],
+    ];
+  }
+}
