f07db944f1c9347c3c90515a9f6938d29ff2e87b
Nyliar <eirel@hotmail.fr>
2026-08-13 08:45:18 -0400

    Added Ordis General C/R1/R2

---
+++ modules/php/Cards/OD/OD_Common_OrdisGeneral.php
@@ -0,0 +1,35 @@
+<?php
+namespace ALT\Cards\OD;
+use ALT\Helpers\FT;
+
+class OD_Common_OrdisGeneral extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_OR_136_C',
+      'asset' => 'ALT_FUGUE_B_OR_136_C',
+      'faction' => FACTION_OD,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Ordis General'),
+      'typeline' => clienttranslate('Character - Soldier'),
+      'type' => CHARACTER,
+      'artist' => 'Tristan Bideau',
+      'extension' => 'NEJ',
+      'subtypes' => [SOLDIER],
+      'effectDesc' => clienttranslate('{J} Target Soldier in play or in Reserve gains 1 boost.'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::ACTION(TARGET, [
+        'targetType' => [CHARACTER, TOKEN],
+        'subType' => SOLDIER,
+        'targetLocation' => [STORM_LEFT, STORM_RIGHT, RESERVE],
+        'effect' => FT::GAIN(TARGET, BOOST, 1),
+      ]),
+    ];
+  }
+}
