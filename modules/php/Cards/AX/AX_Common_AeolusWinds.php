d08273d2bf5458ccd93aeea4a5c457ebcc2986d0
Nyliar <eirel@hotmail.fr>
2026-08-13 15:41:40 -0400

    Added Aeolus Wind C/R1/R2 card family

---
+++ modules/php/Cards/AX/AX_Common_AeolusWinds.php
@@ -0,0 +1,32 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Common_AeolusWinds extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_142_C',
+      'asset' => 'ALT_FUGUE_B_AX_142_C',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Aeolus\' Winds'),
+      'typeline' => clienttranslate('Spell'),
+      'type' => SPELL,
+      'artist' => 'Gamon Studio',
+      'extension' => 'NEJ',
+      'effectDesc' => clienttranslate('$<FLEETING>.  Send to Reserve target Character with Base Cost less than or equal to the number of cards in your Landmarks.'),
+      'costHand' => 2,
+      'costReserve' => 2,
+      'fleeting' => true,
+      'effectPlayed' => FT::SEQ(FT::GAIN(ME, FLEETING),
+        FT::ACTION(TARGET, [
+          'targetType' => [CHARACTER],
+          'maxBaseCost' => 'landmarks',
+          'effect' => FT::DISCARD_TO_RESERVE(),
+        ])),
+    ];
+  }
+}
