7afcf7915a3c8b27b61a2a4672957f36578d42af
Nyliar <eirel@hotmail.fr>
2026-08-13 12:30:04 -0400

    Added Penelope Eteral Weaver C/R1/R2

---
+++ modules/php/Cards/AX/AX_Common_PenelopeEternalWeaver.php
@@ -0,0 +1,48 @@
+<?php
+namespace ALT\Cards\AX;
+use ALT\Helpers\FT;
+
+class AX_Common_PenelopeEternalWeaver extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_137_C',
+      'asset' => 'ALT_FUGUE_B_AX_137_C',
+      'faction' => FACTION_AX,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Penelope, Eternal Weaver'),
+      'typeline' => clienttranslate('Character - Noble'),
+      'artist' => 'Damian Audino',
+      'extension' => 'NEJ',
+      'type' => CHARACTER,
+      'subtypes' => [NOBLE],
+      'effectDesc' => clienttranslate('{H} Pay {1} less for the next Permanent you play this Afternoon, down to a minimum of {1}.  At Dusk — Sacrifice a Permanent with Base Cost {2} or less.'),
+      'forest' => 3,
+      'mountain' => 2,
+      'ocean' => 3,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectHand' => FT::ACTION(SPECIAL_EFFECT, 
+        ['effect' => 'costReduction', 'args' => ['type' => PERMANENT, 'reduction' => 1, 'minimum' => 1, 'permanent' => true]]
+      ),
+      'effectPassive' => [
+        'Dusk' => [
+          'condition' => 'isMe',
+          'output' => FT::ACTION(
+            TARGET,
+            [
+              'targetPlayer' => ME,
+              'targetType' => [PERMANENT],
+              'upTo' => true,
+              'minCost' => 2,
+              'effect' =>
+              FT::ACTION(DISCARD, ['desc' => 'sacrifice']),
+            ]
+          ),
+        ],
+      ],
+    ];
+  }
+}
