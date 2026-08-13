2d558269b725952aedf58f808652ac56a804010e
Nyliar <eirel@hotmail.fr>
2026-08-13 15:30:59 -0400

    Âdded The Shipwreck C/R1/R2 card family

---
+++ modules/php/Cards/LY/LY_Rare_TheShipwreck.php
@@ -0,0 +1,42 @@
+<?php
+namespace ALT\Cards\LY;
+use ALT\Helpers\FT;
+
+class LY_Rare_TheShipwreck extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_AX_145_R2',
+      'asset' => 'ALT_FUGUE_B_AX_145_R',
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('The Shipwreck'),
+      'typeline' => clienttranslate('Landmark Permanent - Construction'),
+      'type' => PERMANENT,
+      'extension' => 'NEJ',
+      'subtypes' => [CONSTRUCTION, LANDMARK],
+      'effectDesc' => clienttranslate('{J} I gain 3 Kelon counters.  At Noon — You may spend 1 of my Kelon counters to $<RESUPPLY>.  #When I\'m sacrificed — Draw a card.#'),
+      'costHand' => 3,
+      'costReserve' => 3,
+      'effectPlayed' => [
+        'action' => SPECIAL_EFFECT,
+        'args' => ['effect' => 'gainCounter', 'args' => ['counter' => 3, 'counterName' => clienttranslate('Kelon counter')]],
+      ],
+      'effectPassive' => [
+        'Noon' => [
+          'conditions' => ['isMe', 'hasCounterOnCard'],
+          'output' =>  FT::SEQ_OPTIONAL_MANUAL(
+            FT::ACTION(USE_COUNTER, ['consume' => 1], ['sourceId' => $this->id]),
+            FT::ACTION(RESUPPLY, [])
+          )
+        ],
+        'Discard' => [
+          'condition' => 'isSacrificed',
+          'output' => FT::ACTION(DRAW, ['players' => ME]),
+        ],
+      ],
+    ];
+  }
+}
