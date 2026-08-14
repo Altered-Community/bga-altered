dce777a7abc9353e2c6728bcec814ab101c0fe38
Nyliar <eirel@hotmail.fr>
2026-08-14 09:02:38 -0400

    Added Unstudious Pig C/R1/R2 card family

---
+++ modules/php/Cards/MU/MU_Rare_UnstudiousPig.php
@@ -0,0 +1,29 @@
+<?php
+namespace ALT\Cards\MU;
+
+class MU_Rare_UnstudiousPig extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_LY_131_R2',
+      'asset' => 'ALT_FUGUE_B_LY_131_R',
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_RARE,
+      'name' => clienttranslate('Unstudious Pig'),
+      'typeline' => clienttranslate('Character - Animal'),
+      'type' => CHARACTER,
+      'flavorText' => clienttranslate('"Fasano loves his new form...but life as a pig is anything but easy." - Nif-Nif'),
+      'artist' => 'Khoa Viet',
+      'extension' => 'NEJ',
+      'subtypes' => [ANIMAL],
+      'forest' => 1,
+      'mountain' => 2,
+      'ocean' => 1,
+      'costHand' => 1,
+      'costReserve' => 1,
+      'changedStats' => ['mountain'],
+    ];
+  }
+}
