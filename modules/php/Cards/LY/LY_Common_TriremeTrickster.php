ca10fd3a2a42e1ad241e0fa48ebd07cf3ae3f3fc
Nyliar <eirel@hotmail.fr>
2026-08-21 11:04:54 -0400

    Added Trireme Trickster C/R1/R2 card family

---
+++ modules/php/Cards/LY/LY_Common_TriremeTrickster.php
@@ -0,0 +1,35 @@
+<?php
+namespace ALT\Cards\LY;
+use ALT\Helpers\FT;
+
+class LY_Common_TriremeTrickster extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_LY_147_C',
+      'asset' => 'ALT_FUGUE_B_LY_147_C',
+      'faction' => FACTION_LY,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Trireme Trickster'),
+      'typeline' => clienttranslate('Character - Artist'),
+      'type' => CHARACTER,
+      'artist' => 'Victor Canton',
+      'extension' => 'NEJ',
+      'subtypes' => [ARTIST],
+      'effectDesc' => clienttranslate('When you roll one or more dice — If I have no boosts, I gain 1 boost.'),
+      'forest' => 2,
+      'mountain' => 1,
+      'ocean' => 1,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPassive' => [
+          'RollDie' => [
+              'conditions' => ['isMe', 'hasBoost:0:LTE', 'isInStorms'],
+              'output' => FT::GAIN(ME, BOOST),
+          ],
+      ],
+    ];
+  }
+}
