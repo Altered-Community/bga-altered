9d6080690b60dd04c42c230b6378605540fb75f7
Nyliar <eirel@hotmail.fr>
2026-08-26 13:14:23 -0400

    Added Stocky Shroom C/R1/R2 card family

---
+++ modules/php/Cards/MU/MU_Common_StockyShroom.php
@@ -0,0 +1,34 @@
+<?php
+namespace ALT\Cards\MU;
+use ALT\Helpers\FT;
+
+class MU_Common_StockyShroom extends \ALT\Models\Card
+{
+  public function __construct($row)
+  {
+    parent::__construct($row);
+    $this->properties = [
+      'uid' => 'ALT_FUGUE_B_MU_134_C',
+      'asset' => 'ALT_FUGUE_B_MU_134_C',
+      'faction' => FACTION_MU,
+      'rarity' => RARITY_COMMON,
+      'name' => clienttranslate('Stocky Shroom'),
+      'typeline' => clienttranslate('Character - Soldier Plant'),
+      'type' => CHARACTER,
+      'flavorText' => clienttranslate('Such a bad spore !'),
+      'artist' => 'Khoa Viet',
+      'extension' => 'NEJ',
+      'subtypes' => [SOLDIER, PLANT],
+      'effectDesc' => clienttranslate('{J} If you control a <COMPANION>, I gain $<ANCHORED>.'),
+      'forest' => 1,
+      'mountain' => 1,
+      'ocean' => 2,
+      'costHand' => 2,
+      'costReserve' => 2,
+      'effectPlayed' => FT::ACTION(CHECK_CONDITION, [
+        'condition' => 'hasCompanionsInExpeditions',
+        'effect' => FT::GAIN(ME, ANCHORED),
+      ]),
+    ];
+  }
+}
