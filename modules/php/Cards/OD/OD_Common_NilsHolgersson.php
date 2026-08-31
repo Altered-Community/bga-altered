9456dba1551ec9c6ec56a461a8659c7bf08c81db
noobiwow <valentin.millet@efrei.net>
2026-08-31 12:45:28 +0900

    Fix Nils Hoggersson C stats

--- modules/php/Cards/OD/OD_Common_NilsHolgersson.php
+++ modules/php/Cards/OD/OD_Common_NilsHolgersson.php
@@ -6,24 +6,24 @@ class OD_Common_NilsHolgersson extends \ALT\Models\Card
 {
   public function __construct($row){
 		parent::__construct($row);
-        $this->properties = [
-            'uid' => 'ALT_EOLE_B_OR_106_C',
-            'asset'  => 'ALT_EOLE_B_OR_106_C',
+    $this->properties = [
+      'uid' => 'ALT_EOLE_B_OR_106_C',
+      'asset'  => 'ALT_EOLE_B_OR_106_C',
 
-    	'faction'  => FACTION_OD,
-    	'rarity'  => RARITY_COMMON,
-    	'name'  => clienttranslate("Nils Holgersson"),
+      'faction'  => FACTION_OD,
+      'rarity'  => RARITY_COMMON,
+      'name'  => clienttranslate("Nils Holgersson"),
       'typeline' => clienttranslate("Character - Citizen"),
-    	'type'  => CHARACTER,
-    	'flavorText'  => clienttranslate('Come on, saddle up! We need to fly in tight formation to evacuate as many as possible!'),
+      'type'  => CHARACTER,
+      'flavorText'  => clienttranslate('Come on, saddle up! We need to fly in tight formation to evacuate as many as possible!'),
       'artist' => "Ba Vo",
-			'extension'=>'ROC',
-   'subtypes'  => [CITIZEN],
-     'forest' => 3, 
-     'mountain' => 3, 
-     'ocean' => 0, 
-     'costHand' => 2, 
-     'costReserve' => 2, 
-];
+      'extension'=>'ROC',
+      'subtypes'  => [CITIZEN],
+      'forest' => 0, 
+      'mountain' => 3, 
+      'ocean' => 3, 
+      'costHand' => 2, 
+      'costReserve' => 2, 
+    ];
   }
 }
